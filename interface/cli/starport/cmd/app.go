23dc3917229ce411dc18c9d93a8bc34d1789b8fd
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2020-07-28 11:23:15 +0300

    interface/cli/starport/cmd: made app command newable
    * reconstructed organization of app command.

--- interface/cli/starport/cmd/app.go
+++ interface/cli/starport/cmd/app.go
@@ -11,28 +11,37 @@ import (
 	"github.com/tendermint/starport/templates/app"
 )
 
-var appCmd = &cobra.Command{
-	Use:   "app [github.com/org/repo]",
-	Short: "Generates an empty application",
-	Args:  cobra.ExactArgs(1),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		path, err := gomodulepath.Parse(args[0])
-		if err != nil {
-			return err
-		}
-		denom, _ := cmd.Flags().GetString("denom")
-		g, _ := app.New(&app.Options{
-			ModulePath:       path.RawPath,
-			AppName:          path.Package,
-			BinaryNamePrefix: path.Root,
-			Denom:            denom,
-		})
-		run := genny.WetRunner(context.Background())
-		run.With(g)
-		pwd, _ := os.Getwd()
-		run.Root = pwd + "/" + path.Root
-		run.Run()
-		message := `
+// NewApp creates new command named `app` to create Cosmos scaffolds customized
+// by the user given options.
+func NewApp() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "app [github.com/org/repo]",
+		Short: "Generates an empty application",
+		Args:  cobra.ExactArgs(1),
+		RunE:  appHandler,
+	}
+	c.Flags().StringP("denom", "d", "token", "Token denomination")
+	return c
+}
+
+func appHandler(cmd *cobra.Command, args []string) error {
+	path, err := gomodulepath.Parse(args[0])
+	if err != nil {
+		return err
+	}
+	denom, _ := cmd.Flags().GetString("denom")
+	g, _ := app.New(&app.Options{
+		ModulePath:       path.RawPath,
+		AppName:          path.Package,
+		BinaryNamePrefix: path.Root,
+		Denom:            denom,
+	})
+	run := genny.WetRunner(context.Background())
+	run.With(g)
+	pwd, _ := os.Getwd()
+	run.Root = pwd + "/" + path.Root
+	run.Run()
+	message := `
 ⭐️ Successfully created a Cosmos app '%[1]v'.
 👉 Get started with the following commands:
 
@@ -41,7 +50,6 @@ var appCmd = &cobra.Command{
 
 NOTE: add -v flag for advanced use.
 `
-		fmt.Printf(message, path.Root)
-		return nil
-	},
+	fmt.Printf(message, path.Root)
+	return nil
 }
