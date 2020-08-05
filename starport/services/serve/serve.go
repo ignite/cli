f365cc0674da806f0e7d34b7d80b2d17a47f6ad7
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2020-08-05 15:26:10 +0300

    improve logs

--- starport/services/serve/serve.go
+++ starport/services/serve/serve.go
@@ -192,10 +192,6 @@ func (s *starportServe) buildSteps() (steps step.Steps) {
 	))
 	steps.Add(step.New(
 		step.Exec("go", "mod", "verify"),
-		step.PreExec(func() error {
-			fmt.Println("🛠️  Building the app...")
-			return nil
-		}),
 		step.PostExec(captureBuildErr),
 		step.Stderr(buildErr),
 	))
@@ -204,6 +200,10 @@ func (s *starportServe) buildSteps() (steps step.Steps) {
 
 	steps.Add(step.New(
 		step.Exec("go", "install", "-mod", "readonly", "-ldflags", ldflags, filepath.Join(cwd, "cmd", appd)),
+		step.PreExec(func() error {
+			fmt.Println("🛠️  Building the app...")
+			return nil
+		}),
 		step.PostExec(captureBuildErr),
 		step.Stderr(buildErr),
 	))
