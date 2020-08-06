48d4d41ec98fc95e38b78ba82b3cc93923c1091b
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2020-08-06 14:30:52 +0300

    services/serve: add prefixes to verbose logs
    * added new pkg/lineprefixer.
    * added new pkg/prefixgen.
    * added options builder to pkg/cmdrunner/step.
    * cosmetics.

---
+++ starport/pkg/lineprefixer/lineprefixer_test.go
@@ -0,0 +1,27 @@
+package lineprefixer
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWriter(t *testing.T) {
+	logs := `hello,
+this
+is
+Starport!`
+	buf := bytes.Buffer{}
+	w := NewWriter(&buf, "[TENDERMINT] ")
+	_, err := io.Copy(w, strings.NewReader(logs))
+	require.NoError(t, err)
+	require.Equal(t, `[TENDERMINT] hello,
+[TENDERMINT] this
+[TENDERMINT] is
+[TENDERMINT] Starport!`,
+		buf.String(),
+	)
+}
