3594e25399585caf3f5c8dd1d54fe5008a98f556
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2020-09-05 12:19:12 +0300

    improve docs

--- starport/services/scaffolder/scaffolder.go
+++ starport/services/scaffolder/scaffolder.go
@@ -1,13 +1,16 @@
-// Package scaffolder, initializes Starports and modifies to add
-// more features in a later time.
+// Package scaffolder initializes Starport apps and modifies existing ones
+// to add more features in a later time.
 package scaffolder
 
+// Scaffolder is Starport app scaffolder.
 type Scaffolder struct {
 	// name is app's name
 	name string
 }
 
 // New creates a new Scaffolder for given app name.
+// app name can be belong to an existent app or the one wished
+// to be initialized.
 func New(name string) *Scaffolder {
 	return &Scaffolder{
 		name: name,
