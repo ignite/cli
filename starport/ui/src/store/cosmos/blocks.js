3143c542f0a5eaeea3a5f1402fc4a72dcba451ae
vms <vince19972@gmail.com>
2020-09-21 15:35:58 -0400

    [blocks] move initConnection into vuex

--- starport/ui/src/store/cosmos/blocks.js
+++ starport/ui/src/store/cosmos/blocks.js
@@ -1,3 +1,6 @@
+import ReconnectingWebSocket from 'reconnecting-websocket'
+import blockHelpers from '@/mixins/blocks/helpers'
+
 export default {
   namespaced: true,
   state: {
@@ -79,6 +82,62 @@ export default {
   actions: {
     addBlockEntry({ commit }, block) {
       commit('addBlockEntry', block)
+    },
+    initBlockConnection({ commit, getters }, { localEnv }) {
+      const ws = new ReconnectingWebSocket(`ws://${localEnv.COSMOS_RPC}/websocket`)
+      // const ws = new ReconnectingWebSocket(`wss://${this.localEnv.COSMOS_RPC}:443/websocket`, [], { WebSocket: WebSocket })    
+  
+      ws.onopen = function() {
+        ws.send(
+          JSON.stringify({
+            jsonrpc: "2.0",
+            method: "subscribe",
+            id: "1",
+            params: ["tm.event = 'NewBlock'"]
+          })
+        )
+      }
+      
+      ws.onmessage = (msg) => {
+        const { result } = JSON.parse(msg.data)
+  
+        if (result.data && result.events) {
+          const { data } = result        
+          const { data: txsData, header } = data.value.block
+  
+          const { fetchBlockMeta, fetchDecodedTx } = blockHelpers
+          const blockFormatter = blockHelpers.blockFormatter()
+          const blockHolder = blockFormatter.setNewBlock(header, txsData)
+  
+          const blockErrCallback = (errLog) => commit('addErrorBlock', {
+            blockHeight: header.height,
+            errLog
+          })
+          const txErrCallback = (txEncoded, errLog) => commit('addErrorTx', {
+            blockHeight: header.height,
+            txEncoded,
+            errLog
+          })
+  
+          fetchBlockMeta(localEnv.COSMOS_RPC, header.height, blockErrCallback)
+            .then(blockMeta => {
+              blockHolder.setBlockMeta(blockMeta)
+  
+              if (txsData.txs && txsData.txs.length > 0) {
+                const txsDecoded = txsData.txs
+                  .map(txEncoded => fetchDecodedTx(localEnv.LCD, txEncoded, txErrCallback))
+                
+                txsDecoded.forEach(txRes => txRes.then(txResolved => {
+                  blockHolder.setBlockTxsDecoded(txResolved)
+                }))
+              }    
+              // this guards duplicated block pushed into blockEntries
+              if (getters.blockByHeight(blockHolder.block.height).length<=0) {
+                commit('addBlockEntry', blockHolder.block)
+              }
+            })
+        }         
+      }      
     }
   }
 }
