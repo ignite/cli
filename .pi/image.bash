97443df8e13f6d9ffe8ba286d18062c67bc7752d
Jacob Gadikian <jacobgadikian@gmail.com>
2020-10-03 02:17:14 +0700

    Configured starport pi to use ubuntu.  Both starport Pi and Downstream Pi now connect to ZT by default.

--- .pi/image.bash
+++ .pi/image.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This install script runs inside the chroot of your image builder.
+# After it runs, a second shell session installs nodejs with nvm
 
 # Connect to the internet by configuring DNS
 mv /etc/resolv.conf /etc/resolv.conf.bk
@@ -14,19 +15,19 @@ wget http://download.zerotier.com/debian/buster/pool/main/z/zerotier-one/zerotie
 dpkg -i zerotier-one_1.4.6_arm64.deb
 apt install -y
 
-# Set Up <%= AppName %>
-chmod +x /usr/bin/<%= AppName %>d /usr/bin/<%= AppName %>cli
-mkdir -p /<%= AppName %>/config
-systemctl enable <%= AppName %>d
+# Set Up Starport
+chmod +x /usr/bin/starport
+mkdir -p /starport/config
+systemctl enable starportd
 
 
 # Set up Nodejs without NVM
 wget https://nodejs.org/dist/v12.18.4/node-v12.18.4-linux-arm64.tar.xz
 sudo mkdir -p /usr/local/lib/nodejs
 sudo tar -xJvf node-v12.18.4-linux-arm64.tar.xz -C /usr/local/lib/nodejs
-echo "export PATH=/usr/local/lib/nodejs/node-v12.18.4-linux-arm64/bin:$PATH" >> ~/.profile
+echo "export PATH=/usr/local/lib/nodejs/node-v12.18.4-linux-arm64/bin:$PATH" >> /root/.profile
 echo "export PATH=/usr/local/lib/nodejs/node-v12.18.4-linux-arm64/bin:$PATH" >> /home/ubuntu/.profile
-. ~/.profile
+. /root/.profile
 node -v
 npm version
 npx -v
