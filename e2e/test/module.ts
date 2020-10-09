63bcf1c9048358a5eda623e787efb8ad1c3d4dc4
ltacker <lucas.bertrand.22@gmail.com>
2020-10-09 09:27:19 +0200

    Test module create with stargate

--- e2e/test/module.ts
+++ e2e/test/module.ts
@@ -20,3 +20,20 @@ test('generate an app with a empty module and verify', async t => {
   console.log(gotest.stdout.toString())
   t.falsy(gotest.error,  "app cannot be build or tests aren't passing")
 });
+
+test('generate an app with stargate version with a empty module and verify', async t => {
+    const workdir = tmp.dirSync();
+    const appPath = path.join(workdir.name, "e2e");
+  
+    // generate the app. 
+    await exec('starport app github.com/e2e/e2e --sdk-version stargate', { cwd: workdir.name });
+  
+    // add a type. 
+    await exec('starport module create example', { cwd: appPath });
+  
+    // check if app builds and tests passes.
+    const gotest = await exec('go test ./...', { cwd: appPath })
+    console.log(gotest.stdout.toString())
+    t.falsy(gotest.error,  "app cannot be build or tests aren't passing")
+  });
+  
