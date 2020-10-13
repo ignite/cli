7f62033242413362a931fa26fc39d95964415302
ltacker <lucas.bertrand.22@gmail.com>
2020-10-13 10:40:53 +0200

    Add tests types with module

--- e2e/test/app.ts
+++ e2e/test/app.ts
@@ -1,22 +1,22 @@
-import test from "ava";
-import * as tmp from "tmp";
-import * as path from "path";
-import * as fs from "fs";
-import * as util from "util";
-import { ChildProcess, spawn } from 'child_process';
-import * as retry from'async-retry';
-import * as fetch from 'node-fetch';
-const exec = util.promisify(require('child_process').exec);
+import test from "ava"
+import * as tmp from "tmp"
+import * as path from "path"
+import * as fs from "fs"
+import * as util from "util"
+import { ChildProcess, spawn } from 'child_process'
+import * as retry from'async-retry'
+import * as fetch from 'node-fetch'
+const exec = util.promisify(require('child_process').exec)
 
 const cosmosAddr = "http://localhost:1317"
 
 test('generate an app and verify', async t => {
-  const workdir = tmp.dirSync();
+  const workdir = tmp.dirSync()
 
   // check if app can be generated.
   await exec('starport app github.com/e2e/e2e', {
     cwd: workdir.name,
-  });
+  })
   t.true(fs.existsSync(path.join(workdir.name, "e2e/config.yml")), "cannot locate a config.yml")
 
   // check if app builds and tests passes.
@@ -25,21 +25,21 @@ test('generate an app and verify', async t => {
   })
   console.log(gotest.stdout.toString())
   t.falsy(gotest.error,  "app cannot be build or tests aren't passing")
-});
+})
 
 test('generate an app with CosmWasm and verify', async t => {
-  const workdir = tmp.dirSync();
-  const appPath = path.join(workdir.name, "e2e");
+  const workdir = tmp.dirSync()
+  const appPath = path.join(workdir.name, "e2e")
 
   // generate the app. 
-  await exec('starport app github.com/e2e/e2e', { cwd: workdir.name });
+  await exec('starport app github.com/e2e/e2e', { cwd: workdir.name })
 
   // add CosmWasm. 
-  await exec('starport module import wasm', { cwd: appPath });
+  await exec('starport module import wasm', { cwd: appPath })
 
   // cannot add CosmWasm again. 
   try {
-    await exec('starport module import wasm', { cwd: appPath });
+    await exec('starport module import wasm', { cwd: appPath })
     t.fail("cannot add wasm twice")
   } catch(e) {
   }
@@ -48,34 +48,34 @@ test('generate an app with CosmWasm and verify', async t => {
   const gotest = await exec('go test ./...', { cwd: appPath })
   console.log(gotest.stdout.toString())
   t.falsy(gotest.error,  "app cannot be build or tests aren't passing")
-});
+})
 
 test('serve app with CosmWasm', async t => {
-  const workdir = tmp.dirSync();
-  const appPath = path.join(workdir.name, "e2e");
+  const workdir = tmp.dirSync()
+  const appPath = path.join(workdir.name, "e2e")
 
   // generate the app. 
-  await exec('starport app github.com/e2e/e2e', { cwd: workdir.name });
+  await exec('starport app github.com/e2e/e2e', { cwd: workdir.name })
 
   // add CosmWasm. 
-  await exec('starport module import wasm', { cwd: appPath });
+  await exec('starport module import wasm', { cwd: appPath })
 
   // serve should be OK and there should not be any runtime errors during serve.
   await new Promise(async (resolve, reject) => {
     let stderr = "";
-    const serveproc = spawn("starport", ["serve", "--verbose"], { cwd: appPath });
+    const serveproc = spawn("starport", ["serve", "--verbose"], { cwd: appPath })
 
     // collect logs from the error stream.
     serveproc.stderr.on('data', (data) => {
-      stderr += data.toString();
-    });
+      stderr += data.toString()
+    })
 
     // watch serve exit.
     serveproc.on("exit", (code) => {
       if (code > 0) {
-        reject(`cannot serve:\n${stderr}`);
+        reject(`cannot serve:\n${stderr}`)
       }
-    });
+    })
 
     // try to reach to App through API to see if it works properly. 
     await retry(async (bail, no) => {
@@ -91,4 +91,4 @@ test('serve app with CosmWasm', async t => {
   })
 
   t.pass("app is reachable");
-});
+})
--- e2e/test/type.ts
+++ e2e/test/type.ts
@@ -1,22 +1,76 @@
-import test from "ava";
-import * as tmp from "tmp";
-import * as path from "path";
-import * as util from "util";
+import test from "ava"
+import * as tmp from "tmp"
+import * as path from "path"
+import * as util from "util"
 
-const exec = util.promisify(require('child_process').exec);
+const exec = util.promisify(require('child_process').exec)
 
 test('generate an app with a type and verify', async t => {
-  const workdir = tmp.dirSync();
-  const appPath = path.join(workdir.name, "e2e");
+  const workdir = tmp.dirSync()
+  const appPath = path.join(workdir.name, "e2e")
 
   // generate the app. 
-  await exec('starport app github.com/e2e/e2e', { cwd: workdir.name });
+  await exec('starport app github.com/e2e/e2e', { cwd: workdir.name })
 
   // add a type. 
-  await exec('starport type user name email', { cwd: appPath });
+  await exec('starport type user name email', { cwd: appPath })
 
   // check if app builds and tests passes.
   const gotest = await exec('go test ./...', { cwd: appPath })
   console.log(gotest.stdout.toString())
   t.falsy(gotest.error,  "app cannot be build or tests aren't passing")
-});
+})
+
+test('generate an app using stargate version with a type and verify', async t => {
+  const workdir = tmp.dirSync()
+  const appPath = path.join(workdir.name, "e2e")
+
+  // generate the app. 
+  await exec('starport app --sdk-version stargate github.com/e2e/e2e', { cwd: workdir.name })
+
+  // add a type. 
+  await exec('starport type user name email', { cwd: appPath })
+
+  // check if app builds and tests passes.
+  const gotest = await exec('go test ./...', { cwd: appPath })
+  console.log(gotest.stdout.toString())
+  t.falsy(gotest.error,  "app cannot be build or tests aren't passing")
+})
+
+test('creating types in a custom module', async t => {
+  const workdir = tmp.dirSync();
+  const appPath = path.join(workdir.name, "e2e")
+
+  // generate the app. 
+  await exec('starport app github.com/e2e/e2e', { cwd: workdir.name })
+
+  // create a module
+  await exec('starport module create example', { cwd: appPath })
+
+  // add a type. 
+  await exec('starport type user name email --module example', { cwd: appPath })
+
+  // check if app builds and tests passes.
+  const gotest = await exec('go test ./...', { cwd: appPath })
+  console.log(gotest.stdout.toString())
+  t.falsy(gotest.error,  "app cannot be build or tests aren't passing")
+})
+
+test('creating types in a custom module using stargate version ', async t => {
+  const workdir = tmp.dirSync();
+  const appPath = path.join(workdir.name, "e2e")
+
+  // generate the app. 
+  await exec('starport app --sdk-version stargate github.com/e2e/e2e', { cwd: workdir.name })
+
+  // create a module
+  await exec('starport module create example', { cwd: appPath })
+
+  // add a type. 
+  await exec('starport type user name email --module example', { cwd: appPath })
+
+  // check if app builds and tests passes.
+  const gotest = await exec('go test ./...', { cwd: appPath })
+  console.log(gotest.stdout.toString())
+  t.falsy(gotest.error,  "app cannot be build or tests aren't passing")
+})
