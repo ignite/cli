1de524cc24b45ca52e2f346a74e5fda20dead1cc
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2020-10-14 12:59:35 +0200

    Feat: Adding the --module option for the type command (#329)
    * Add module flag
    
    * Changed options
    
    * Rename AppName into ModuleName in templates
    
    * New placeholders
    
    * Update packages
    
    * Add placeholders in proto querier
    
    * Add tests types with module
    
    * Improve tests
    
    * Packages update
    
    * Created types detection issue
    
    * Add type tests in Go integration tests

--- integration/cmd_type_test.go
+++ integration/cmd_type_test.go
@@ -11,14 +11,144 @@ func TestGenerateAnAppWithTypeAndVerify(t *testing.T) {
 
 	var (
 		env  = newEnv(t)
-		path = env.Scaffold("blog", Stargate)
+		path = env.Scaffold("blog", Launchpad)
 	)
 
-	env.Exec("add CosmWasm module",
+	env.Exec("create a type",
 		step.New(
 			step.Exec("starport", "type", "user", "email"),
 			step.Workdir(path),
 		),
+	)
+
+	env.Exec("should prevent creating an existing type",
+		step.New(
+			step.Exec("starport", "type", "user", "email"),
+			step.Workdir(path),
+		),
+		ExecShouldError(),
+	)
+
+	env.EnsureAppIsSteady(path)
+}
+
+func TestGenerateAnAppWithStargateWithTypeAndVerify(t *testing.T) {
+	t.Parallel()
+
+	var (
+		env  = newEnv(t)
+		path = env.Scaffold("blog", Stargate)
+	)
+
+	env.Exec("create a type",
+		step.New(
+			step.Exec("starport", "type", "user", "email"),
+			step.Workdir(path),
+		),
+	)
+
+	env.Exec("should prevent creating an existing type",
+		step.New(
+			step.Exec("starport", "type", "user", "email"),
+			step.Workdir(path),
+		),
+		ExecShouldError(),
+	)
+
+	env.EnsureAppIsSteady(path)
+}
+
+func TestCreateTypeInCustomModule(t *testing.T) {
+	t.Parallel()
+
+	var (
+		env  = newEnv(t)
+		path = env.Scaffold("blog", Launchpad)
+	)
+
+	env.Exec("create a module",
+		step.New(
+			step.Exec("starport", "module", "create", "example"),
+			step.Workdir(path),
+		),
+	)
+
+	env.Exec("create a type",
+		step.New(
+			step.Exec("starport", "type", "user", "email", "--module", "example"),
+			step.Workdir(path),
+		),
+	)
+
+	env.Exec("create a type in the app's module",
+		step.New(
+			step.Exec("starport", "type", "user", "email"),
+			step.Workdir(path),
+		),
+	)
+
+	env.Exec("should prevent creating a type in a non existant module",
+		step.New(
+			step.Exec("starport", "type", "user", "email", "--module", "idontexist"),
+			step.Workdir(path),
+		),
+		ExecShouldError(),
+	)
+
+	env.Exec("should prevent creating an existing type",
+		step.New(
+			step.Exec("starport", "type", "user", "email", "--module", "example"),
+			step.Workdir(path),
+		),
+		ExecShouldError(),
+	)
+
+	env.EnsureAppIsSteady(path)
+}
+
+func TestCreateTypeInCustomModuleWithStargate(t *testing.T) {
+	t.Parallel()
+
+	var (
+		env  = newEnv(t)
+		path = env.Scaffold("blog", Launchpad)
+	)
+
+	env.Exec("create a module",
+		step.New(
+			step.Exec("starport", "module", "create", "example"),
+			step.Workdir(path),
+		),
+	)
+
+	env.Exec("create a type",
+		step.New(
+			step.Exec("starport", "type", "user", "email", "--module", "example"),
+			step.Workdir(path),
+		),
+	)
+
+	env.Exec("create a type in the app's module",
+		step.New(
+			step.Exec("starport", "type", "user", "email"),
+			step.Workdir(path),
+		),
+	)
+
+	env.Exec("should prevent creating a type in a non existant module",
+		step.New(
+			step.Exec("starport", "type", "user", "email", "--module", "idontexist"),
+			step.Workdir(path),
+		),
+		ExecShouldError(),
+	)
+
+	env.Exec("should prevent creating an existing type",
+		step.New(
+			step.Exec("starport", "type", "user", "email", "--module", "example"),
+			step.Workdir(path),
+		),
+		ExecShouldError(),
 	)
 
 	env.EnsureAppIsSteady(path)
