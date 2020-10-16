e62eb6f0e1bbe1da2c3637347c6e8bd8ce66b406
ltacker <lucas.bertrand.22@gmail.com>
2020-10-16 14:42:29 +0200

    Prevent duplicated types

--- integration/cmd_type_test.go
+++ integration/cmd_type_test.go
@@ -21,6 +21,14 @@ func TestGenerateAnAppWithTypeAndVerify(t *testing.T) {
 		),
 	)
 
+	env.Exec("should prevent creating a type with duplicated fields",
+		step.New(
+			step.Exec("starport", "type", "company", "name", "name"),
+			step.Workdir(path),
+		),
+		ExecShouldError(),
+	)
+
 	env.Exec("should prevent creating an existing type",
 		step.New(
 			step.Exec("starport", "type", "user", "email"),
@@ -45,6 +53,14 @@ func TestGenerateAnAppWithStargateWithTypeAndVerify(t *testing.T) {
 			step.Exec("starport", "type", "user", "email"),
 			step.Workdir(path),
 		),
+	)
+
+	env.Exec("should prevent creating a type with duplicated fields",
+		step.New(
+			step.Exec("starport", "type", "company", "name", "name"),
+			step.Workdir(path),
+		),
+		ExecShouldError(),
 	)
 
 	env.Exec("should prevent creating an existing type",
