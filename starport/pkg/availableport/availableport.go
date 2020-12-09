99deba9a43774686614cd288542f57e2e0625afe
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2020-12-09 18:22:12 +0300

    fix(pkg/availableport): change finding available port logic
    we were letting OS to find an available port previosuly but that has a side
    effect of sometimes ports not being released back in time by the OS.

--- starport/pkg/availableport/availableport.go
+++ starport/pkg/availableport/availableport.go
@@ -1,22 +1,33 @@
 package availableport
 
 import (
+	"fmt"
+	"math/rand"
 	"net"
+	"time"
 )
 
 // Find finds n number of unused ports.
 // it is not guaranteed that these ports will not be allocated to
 // another program in the time of calling Find().
 func Find(n int) (ports []int, err error) {
+	min := 44000
+	max := 55000
+
 	for i := 0; i < n; i++ {
-		ln, err := net.Listen("tcp", ":0")
-		if err != nil {
-			return nil, err
+		for {
+			rand.Seed(time.Now().UnixNano())
+			port := rand.Intn(max-min+1) + min
+
+			conn, err := net.Dial("tcp", fmt.Sprintf("%d", port))
+			// if there is an error, this might mean that no one is listening from this port
+			// which is what we need.
+			if err != nil {
+				conn.Close()
+				continue
+			}
+			ports = append(ports, port)
 		}
-		if err := ln.Close(); err != nil {
-			return nil, err
-		}
-		ports = append(ports, ln.Addr().(*net.TCPAddr).Port)
 	}
 	return ports, nil
 }
