6b1ac6644849fb2f7f41e152bfca6f2bc8ceaeef
Brian <45702419+lukitsbrian@users.noreply.github.com>
2020-12-10 01:24:48 -0800

    feat(cmd/version): set version and build date for development (#498)
    * Add version and build date to version command
    
    * Pass version, build, and HEAD object information via Makefile
    
    * Update date format
    
    * Remove git object output for prod
    
    * Remove package-lock.json
    
    * Lazy variable eval + fetch tags
    
    * organize dependencies
    
    Co-authored-by: İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>

--- starport/internal/version/version.go
+++ starport/internal/version/version.go
@@ -7,17 +7,25 @@ import (
 
 var (
 	// Version is the semantic version of Starport.
-	Version = "dev"
+	Version = ""
 
 	// Date is the build date of Starport.
 	Date = ""
+
+	// Head is the HEAD of the current branch.
+	Head = ""
 )
 
 // Long generates a detailed version info.
 func Long() string {
-	return fmt.Sprintf("starport version %s %s/%s -build date: %s",
+	output := fmt.Sprintf("starport version %s %s/%s -build date: %s",
 		Version,
 		runtime.GOOS,
 		runtime.GOARCH,
 		Date)
+
+	if Head != "" {
+		output = fmt.Sprintf("%s\ngit object hash: %s", output, Head)
+	}
+	return output
 }
