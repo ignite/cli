c66ffb697db4f3f0d893f364e649f5b817a3d92f
ltacker <lucas.bertrand.22@gmail.com>
2020-12-11 17:53:28 +0100

    add test

--- integration/cmd_serve_test.go
+++ integration/cmd_serve_test.go
@@ -38,3 +38,25 @@ func TestServeAppWithCosmWasm(t *testing.T) {
 
 	require.NoError(t, isBackendAliveErr, "app cannot get online in time")
 }
+
+func TestServeAppStargate(t *testing.T) {
+	t.Parallel()
+
+	var (
+		env     = newEnv(t)
+		apath   = env.Scaffold("sgblog", Stargate)
+		servers = env.RandomizeServerPorts(apath)
+	)
+
+	var (
+		ctx, cancel       = context.WithTimeout(env.Ctx(), serveTimeout)
+		isBackendAliveErr error
+	)
+	go func() {
+		defer cancel()
+		isBackendAliveErr = env.IsAppServed(ctx, servers)
+	}()
+	env.Must(env.Serve("should serve with Stargate version", apath, ExecCtx(ctx)))
+
+	require.NoError(t, isBackendAliveErr, "app cannot get online in time")
+}
