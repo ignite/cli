f5100c00e01598ca5e55655c9f8e0d0dd601c32b
ltacker <lucas.bertrand.22@gmail.com>
2021-01-13 17:27:32 +0100

    Launchpad create module

---
+++ starport/templates/module/new_create_launchpad.go
@@ -0,0 +1,99 @@
+package module
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gobuffalo/genny"
+	"github.com/gobuffalo/plush"
+	"github.com/gobuffalo/plushgen"
+	"github.com/tendermint/starport/starport/pkg/cosmosver"
+)
+
+// New ...
+func NewCreateLaunchpad(opts *CreateOptions) (*genny.Generator, error) {
+	g := genny.New()
+
+	g.RunFn(createAppModifyLaunchpad(opts))
+
+	if err := g.Box(templates[cosmosver.Launchpad]); err != nil {
+		return g, err
+	}
+	ctx := plush.NewContext()
+	ctx.Set("moduleName", opts.ModuleName)
+	ctx.Set("modulePath", opts.ModulePath)
+	ctx.Set("title", strings.Title)
+	g.Transformer(plushgen.Transformer(ctx))
+	g.Transformer(genny.Replace("{{moduleName}}", opts.ModuleName))
+	return g, nil
+}
+
+// app.go modification on Launchpad when creating a module
+func createAppModifyLaunchpad(opts *CreateOptions) genny.RunFn {
+	return func(r *genny.Runner) error {
+		path := PathAppGo
+		f, err := r.Disk.Find(path)
+		if err != nil {
+			return err
+		}
+
+		// Import
+		template := `%[1]v
+		"%[3]v/x/%[2]v"
+		%[2]vkeeper "%[3]v/x/%[2]v/keeper"
+		%[2]vtypes "%[3]v/x/%[2]v/types"`
+		replacement := fmt.Sprintf(template, placeholder, opts.ModuleName, opts.ModulePath)
+		content := strings.Replace(f.String(), placeholder, replacement, 1)
+
+		// ModuleBasic
+		template = `%[1]v
+		%[2]v.AppModuleBasic{},`
+		replacement = fmt.Sprintf(template, placeholder2, opts.ModuleName)
+		content = strings.Replace(content, placeholder2, replacement, 1)
+
+		// Keeper declaration
+		template = `%[1]v
+		%[2]vKeeper %[2]vkeeper.Keeper`
+		replacement = fmt.Sprintf(template, placeholder3, opts.ModuleName)
+		content = strings.Replace(content, placeholder3, replacement, 1)
+
+		// Store key
+		template = `%[1]v
+		%[2]vtypes.StoreKey,`
+		replacement = fmt.Sprintf(template, placeholder5, opts.ModuleName)
+		content = strings.Replace(content, placeholder5, replacement, 1)
+
+		// Param subspace
+		template = `%[1]v
+		app.subspaces[%[2]vtypes.ModuleName] = app.paramsKeeper.Subspace(%[2]vtypes.DefaultParamspace)`
+		replacement = fmt.Sprintf(template, placeholder5_1, opts.ModuleName)
+		content = strings.Replace(content, placeholder5_1, replacement, 1)
+
+		// Keeper definition
+		template = `%[1]v
+		app.%[2]vKeeper = %[2]vkeeper.NewKeeper(
+			app.cdc,
+			keys[%[2]vtypes.StoreKey],
+			app.subspaces[%[2]vtypes.ModuleName],
+		)`
+		replacement = fmt.Sprintf(template, placeholder5_2, opts.ModuleName)
+		content = strings.Replace(content, placeholder5_2, replacement, 1)
+
+		// Module manager
+		template = `%[1]v
+		%[2]v.NewAppModule(app.%[2]vKeeper),`
+		replacement = fmt.Sprintf(template, placeholder6, opts.ModuleName)
+		content = strings.Replace(content, placeholder6, replacement, 1)
+
+		// Genesis
+		template = `%[1]v
+		%[2]vtypes.ModuleName,`
+		replacement = fmt.Sprintf(template, placeholder7, opts.ModuleName)
+		content = strings.Replace(content, placeholder7, replacement, 1)
+
+		newFile := genny.NewFileS(path, content)
+		return r.File(newFile)
+	}
+}
+
+
