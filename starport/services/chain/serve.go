bb126f93f010ed2fd485a20faaf2736407186548
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2021-01-21 15:00:07 +0100

    feat(serve): save state as genesis when the app is stopped (#666)

--- starport/services/chain/serve.go
+++ starport/services/chain/serve.go
@@ -13,6 +13,9 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/tendermint/starport/starport/services"
+
+	"github.com/otiai10/copy"
 	"github.com/pkg/errors"
 	conf "github.com/tendermint/starport/starport/chainconf"
 	secretconf "github.com/tendermint/starport/starport/chainconf/secret"
@@ -32,6 +35,12 @@ import (
 var (
 	// ignoredExts holds a list of ignored files from watching.
 	ignoredExts = []string{"pb.go", "pb.gw.go"}
+
+	// chainSavePath is the place where chain exported genesis are saved
+	chainSavePath = filepath.Join(services.StarportConfDir, "local-chains")
+
+	// exportedGenesis is the name of the exported genesis file for a chain
+	exportedGenesis = "exported_genesis.json"
 )
 
 // Serve serves an app.
@@ -56,13 +65,18 @@ func (c *Chain) Serve(ctx context.Context) error {
 
 	// start serving components.
 	g, ctx := errgroup.WithContext(ctx)
+
+	// routine to watch front-end
 	g.Go(func() error {
 		return c.watchAppFrontend(ctx)
 	})
+
+	// development server routine
 	g.Go(func() error {
 		return c.runDevServer(ctx)
 	})
 
+	// blockchain node routine
 	g.Go(func() error {
 		c.refreshServe()
 		for {
@@ -85,6 +99,25 @@ func (c *Chain) Serve(ctx context.Context) error {
 				switch {
 				case err == nil:
 				case errors.Is(err, context.Canceled):
+					// If the app has been served, we save the genesis state
+					if c.served {
+						c.served = false
+
+						fmt.Fprintf(c.stdLog(logStarport).out, "\nSaving genesis state...\n")
+
+						// If serve has been stopped, save the genesis state
+						if err := c.saveChainState(context.TODO()); err != nil {
+							fmt.Fprint(c.stdLog(logStarport).err, err.Error())
+							return err
+						}
+
+						genesisPath, err := c.exportedGenesisPath()
+						if err != nil {
+							fmt.Fprint(c.stdLog(logStarport).err, err.Error())
+							return err
+						}
+						fmt.Fprintf(c.stdLog(logStarport).out, "\nGenesis state saved in %s\n", genesisPath)
+					}
 				case errors.As(err, &buildErr):
 					fmt.Fprintf(c.stdLog(logStarport).err, "%s\n", errorColor(err.Error()))
 
@@ -100,9 +133,12 @@ func (c *Chain) Serve(ctx context.Context) error {
 			}
 		}
 	})
+
+	// routine to watch back-end
 	g.Go(func() error {
 		return c.watchAppBackend(ctx)
 	})
+
 	return g.Wait()
 }
 
@@ -263,6 +299,9 @@ func (c *Chain) start(ctx context.Context, conf conf.Config) error {
 		})
 	}
 
+	// set the app as being served
+	c.served = true
+
 	// print the server addresses.
 	fmt.Fprintf(c.stdLog(logStarport).out, "🌍 Running a Cosmos '%[1]v' app with Tendermint at %s.\n", c.app.Name, xurl.HTTP(conf.Servers.RPCAddr))
 	fmt.Fprintf(c.stdLog(logStarport).out, "🌍 Running a server at %s (LCD)\n", xurl.HTTP(conf.Servers.APIAddr))
@@ -394,6 +433,48 @@ func (c *Chain) runFaucetServer(ctx context.Context) error {
 		Addr:    fmt.Sprintf("0.0.0.0:%d", config.Faucet.Port),
 		Handler: faucet,
 	})
+}
+
+// saveChainState runs the export command of the chain and store the exported genesis in the chain saved config
+func (c *Chain) saveChainState(ctx context.Context) error {
+	genesisPath, err := c.exportedGenesisPath()
+	if err != nil {
+		return err
+	}
+
+	return c.cmd.Export(ctx, genesisPath)
+}
+
+// importChainState imports the saved genesis in chain config to use it as the genesis
+// nolint:unused
+func (c *Chain) importChainState() error {
+	exportGenesisPath, err := c.exportedGenesisPath()
+	if err != nil {
+		return err
+	}
+	genesisPath, err := c.GenesisPath()
+	if err != nil {
+		return err
+	}
+
+	return copy.Copy(exportGenesisPath, genesisPath)
+}
+
+// exportedGenesisPath gets the path of the exported genesis file
+func (c *Chain) exportedGenesisPath() (string, error) {
+	chainID, err := c.ID()
+	if err != nil {
+		return "", err
+	}
+
+	savePath := filepath.Join(chainSavePath, chainID)
+
+	// ensure the path exists
+	if err := os.MkdirAll(savePath, 0700); err != nil && !os.IsExist(err) {
+		return "", err
+	}
+
+	return filepath.Join(savePath, exportedGenesis), nil
 }
 
 type CannotBuildAppError struct {
