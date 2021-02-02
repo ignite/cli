38edd101ab6e2cbe0fa859d09f44fd035418dd6e
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2021-02-02 06:07:30 -0800

    fix tests

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -2,17 +2,12 @@
 package integration_test
 
 import (
-	"bytes"
-	"context"
 	"errors"
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 	"testing"
 
-	"github.com/tendermint/starport/starport/pkg/cmdrunner"
-	"github.com/tendermint/starport/starport/pkg/cmdrunner/step"
 	"github.com/tendermint/starport/starport/pkg/xexec"
 )
 
@@ -29,23 +24,5 @@ func checkSystemRequirements() error {
 	if !xexec.IsCommandAvailable("starport") {
 		return errors.New("starport needs to be installed")
 	}
-	if !xexec.IsCommandAvailable("rly") {
-		return errors.New("relayer needs to be installed")
-	}
-	version := &bytes.Buffer{}
-	return cmdrunner.
-		New().
-		Run(context.Background(), step.New(
-			step.Exec("rly", "version"),
-			step.PostExec(func(execErr error) error {
-				if execErr != nil {
-					return execErr
-				}
-				if !strings.Contains(version.String(), relayerVersion) {
-					return fmt.Errorf("relayer is not at the required version %q", relayerVersion)
-				}
-				return nil
-			}),
-			step.Stdout(version),
-		))
+	return nil
 }
