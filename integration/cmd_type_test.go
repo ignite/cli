716bd8eb9d3c4a856cc6d74f392536321e1040a7
ltacker <lucas.bertrand.22@gmail.com>
2021-02-08 15:25:34 +0100

    Add test type in an IBC module

--- integration/cmd_type_test.go
+++ integration/cmd_type_test.go
@@ -259,5 +259,19 @@ func TestCreateTypeInCustomModuleWithStargate(t *testing.T) {
 		ExecShouldError(),
 	))
 
+	env.Must(env.Exec("create an IBC module",
+		step.NewSteps(step.New(
+			step.Exec("starport", "module", "create", "--ibc", "foo"),
+			step.Workdir(path),
+		)),
+	))
+
+	env.Must(env.Exec("create a type in an IBC modu;e",
+		step.NewSteps(step.New(
+			step.Exec("starport", "type", "user", "email", "--module", "foo"),
+			step.Workdir(path),
+		)),
+	))
+
 	env.EnsureAppIsSteady(path)
 }
