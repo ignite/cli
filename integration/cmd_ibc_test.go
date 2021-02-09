c37e8f163292e32e67438c23e8d30906182616ed
ltacker <lucas.bertrand.22@gmail.com>
2021-02-09 13:23:47 +0100

    create specific test package for ibc module

---
+++ integration/cmd_ibc_test.go
@@ -0,0 +1,48 @@
+// +build !relayer
+
+package integration_test
+
+import (
+	"testing"
+
+	"github.com/tendermint/starport/starport/pkg/cmdrunner/step"
+)
+
+func TestCreateModuleWithIBC(t *testing.T) {
+	t.Parallel()
+
+	var (
+		env  = newEnv(t)
+		path = env.Scaffold("ibcblog", Stargate)
+	)
+
+	env.Must(env.Exec("create an IBC module",
+		step.NewSteps(step.New(
+			step.Exec("starport", "module", "create", "--ibc", "foo"),
+			step.Workdir(path),
+		)),
+	))
+
+	env.Must(env.Exec("create a type in an IBC module",
+		step.NewSteps(step.New(
+			step.Exec("starport", "type", "user", "email", "--module", "foo"),
+			step.Workdir(path),
+		)),
+	))
+
+	env.Must(env.Exec("create an IBC module with an ordered channel",
+		step.NewSteps(step.New(
+			step.Exec("starport", "module", "create", "--ibc", "foo", "--ibc-ordering", "ordered"),
+			step.Workdir(path),
+		)),
+	))
+
+	env.Must(env.Exec("create an IBC module with an unordered channel",
+		step.NewSteps(step.New(
+			step.Exec("starport", "module", "create", "--ibc", "foo", "--ibc-unordered", "ordered"),
+			step.Workdir(path),
+		)),
+	))
+
+	env.EnsureAppIsSteady(path)
+}
