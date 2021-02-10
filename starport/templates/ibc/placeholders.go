656a559683111f5743cee46ffdef7ec9718c4974
ltacker <lucas.bertrand.22@gmail.com>
2021-02-10 08:06:48 +0100

    Lint

--- starport/templates/ibc/placeholders.go
+++ starport/templates/ibc/placeholders.go
@@ -2,12 +2,12 @@ package ibc
 
 const (
 	// Placeholders IBC packets
-	PlaceholderIBCPacketProtoField         = "// this line is used by starport scaffolding # ibc/packet/proto/field"
-	PlaceholderIBCPacketProtoFieldNumber         = "// this line is used by starport scaffolding # ibc/packet/proto/field/number"
-	PlaceholderIBCPacketProtoMessage         = "// this line is used by starport scaffolding # ibc/packet/proto/message"
-	PlaceholderIBCPacketType          = "// this line is used by starport scaffolding # ibc/packet/type"
-	PlaceholderIBCPacketEvent         = "// this line is used by starport scaffolding # ibc/packet/event"
-	PlaceholderIBCPacketModuleRecv    = "// this line is used by starport scaffolding # ibc/packet/module/recv"
-	PlaceholderIBCPacketModuleAck     = "// this line is used by starport scaffolding # ibc/packet/module/ack"
-	PlaceholderIBCPacketModuleTimeout = "// this line is used by starport scaffolding # ibc/packet/module/timeout"
+	PlaceholderIBCPacketProtoField       = "// this line is used by starport scaffolding # ibc/packet/proto/field"
+	PlaceholderIBCPacketProtoFieldNumber = "// this line is used by starport scaffolding # ibc/packet/proto/field/number"
+	PlaceholderIBCPacketProtoMessage     = "// this line is used by starport scaffolding # ibc/packet/proto/message"
+	PlaceholderIBCPacketType             = "// this line is used by starport scaffolding # ibc/packet/type"
+	PlaceholderIBCPacketEvent            = "// this line is used by starport scaffolding # ibc/packet/event"
+	PlaceholderIBCPacketModuleRecv       = "// this line is used by starport scaffolding # ibc/packet/module/recv"
+	PlaceholderIBCPacketModuleAck        = "// this line is used by starport scaffolding # ibc/packet/module/ack"
+	PlaceholderIBCPacketModuleTimeout    = "// this line is used by starport scaffolding # ibc/packet/module/timeout"
 )
