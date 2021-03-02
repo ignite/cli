a46e4742e5fa04ec1f09c7f714663803433e7a4b
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2021-03-02 01:49:51 -0800

    sync stargate scaffolding for new js gen path

---
+++ starport/templates/app/stargate/vue/src/store/chain/cosmos/cosmos-sdk/cosmos.bank.v1beta1/module/index.d.ts
@@ -0,0 +1,21 @@
+import { StdFee } from "@cosmjs/launchpad";
+import { OfflineSigner, EncodeObject } from "@cosmjs/proto-signing";
+import { Api } from "./rest";
+import { MsgSend } from "./types/cosmos/bank/v1beta1/tx";
+import { MsgMultiSend } from "./types/cosmos/bank/v1beta1/tx";
+interface TxClientOptions {
+    addr: string;
+}
+interface SignAndBroadcastOptions {
+    fee: StdFee;
+}
+declare const txClient: (wallet: OfflineSigner, { addr: addr }?: TxClientOptions) => Promise<{
+    signAndBroadcast: (msgs: EncodeObject[], { fee: fee }?: SignAndBroadcastOptions) => Promise<import("@cosmjs/stargate").BroadcastTxResponse>;
+    msgSend: (data: MsgSend) => EncodeObject;
+    msgMultiSend: (data: MsgMultiSend) => EncodeObject;
+}>;
+interface QueryClientOptions {
+    addr: string;
+}
+declare const queryClient: ({ addr: addr }?: QueryClientOptions) => Promise<Api<unknown>>;
+export { txClient, queryClient, };
---
+++ starport/templates/app/stargate/vue/src/store/chain/cosmos/cosmos-sdk/cosmos.distribution.v1beta1/module/index.d.ts
@@ -0,0 +1,25 @@
+import { StdFee } from "@cosmjs/launchpad";
+import { OfflineSigner, EncodeObject } from "@cosmjs/proto-signing";
+import { Api } from "./rest";
+import { MsgSetWithdrawAddress } from "./types/cosmos/distribution/v1beta1/tx";
+import { MsgFundCommunityPool } from "./types/cosmos/distribution/v1beta1/tx";
+import { MsgWithdrawValidatorCommission } from "./types/cosmos/distribution/v1beta1/tx";
+import { MsgWithdrawDelegatorReward } from "./types/cosmos/distribution/v1beta1/tx";
+interface TxClientOptions {
+    addr: string;
+}
+interface SignAndBroadcastOptions {
+    fee: StdFee;
+}
+declare const txClient: (wallet: OfflineSigner, { addr: addr }?: TxClientOptions) => Promise<{
+    signAndBroadcast: (msgs: EncodeObject[], { fee: fee }?: SignAndBroadcastOptions) => Promise<import("@cosmjs/stargate").BroadcastTxResponse>;
+    msgSetWithdrawAddress: (data: MsgSetWithdrawAddress) => EncodeObject;
+    msgFundCommunityPool: (data: MsgFundCommunityPool) => EncodeObject;
+    msgWithdrawValidatorCommission: (data: MsgWithdrawValidatorCommission) => EncodeObject;
+    msgWithdrawDelegatorReward: (data: MsgWithdrawDelegatorReward) => EncodeObject;
+}>;
+interface QueryClientOptions {
+    addr: string;
+}
+declare const queryClient: ({ addr: addr }?: QueryClientOptions) => Promise<Api<unknown>>;
+export { txClient, queryClient, };
