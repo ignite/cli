9644f104383e9f8333b7424a664ba5d26dbe15c2
İlker G. Öztürk <ilkergoktugozturk@gmail.com>
2021-04-09 10:30:10 +0300

    refactor: rm unused & cleanup (#973)
    * refactor: rm unused & cleanup
    
    * cleanup
    
    * cap license
    
    Co-authored-by: Lucas Bertrand <lucas.bertrand.22@gmail.com>

--- .npm/lib/conf.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const home = require('home-path');
-
-exports.starportDir = path.join(home(), ".starport");
-exports.starportPath = path.join(exports.starportDir, "starport");
