b81c3c43ea1142f9628f295753e0dd2711bd8a1f
ltacker <lucas.bertrand.22@gmail.com>
2021-04-13 21:36:34 +0200

    docs

--- starport/pkg/xfilepath/xfilepath.go
+++ starport/pkg/xfilepath/xfilepath.go
@@ -6,18 +6,24 @@ import (
 	"path/filepath"
 )
 
+// PathRetriever is a function that retrieve the contained path or an error
 type PathRetriever func() (path string, err error)
 
+// PathsRetriever is a function that retrieve the contained list of paths or an error
 type PathsRetriever func() (path []string, err error)
 
+// Path returns a path retriever from the provided path
 func Path(path string) PathRetriever {
 	return func() (string, error) { return path, nil }
 }
 
+// Path returns a path retriever from the provided path and error
 func PathWithError(path string, err error) PathRetriever {
 	return func() (string, error) { return path, err }
 }
 
+// Join returns a path retriever from the join of the provided path retrievers
+// The returned path retriever eventually returns the error from the first provided path retrievers that returns a non-nil error
 func Join(paths ...PathRetriever) PathRetriever {
 	var components []string
 	var err error
@@ -36,10 +42,14 @@ func Join(paths ...PathRetriever) PathRetriever {
 	}
 }
 
+// JoinFromHome returns a path retriever from the join of the user home and the provided path retrievers
+// The returned path retriever eventually returns the error from the first provided path retrievers that returns a non-nil error
 func JoinFromHome(paths ...PathRetriever) PathRetriever {
 	return Join(append([]PathRetriever{os.UserHomeDir}, paths...)...)
 }
 
+// List returns a paths retriever from a list of path retrievers
+// The returned paths retriever eventually returns the error from the first provided path retrievers that returns a non-nil error
 func List(paths ...PathRetriever) PathsRetriever {
 	var list []string
 	var err error
