e6893186b11a639485e33697b515144346468bee
ltacker <lucas.bertrand.22@gmail.com>
2021-04-15 11:49:13 +0200

    Add import for msgservice

--- starport/templates/module/create/msgserver.go
+++ starport/templates/module/create/msgserver.go
@@ -12,6 +12,8 @@ import (
 	"github.com/tendermint/starport/starport/templates/typed"
 )
 
+const msgServiceImport = "github.com/cosmos/cosmos-sdk/types/msgservice"
+
 // AddMsgServerConventionToLegacyModule add the files and the necessary modifications to an existing module that doesn't support MsgServer convention
 // https://github.com/cosmos/cosmos-sdk/blob/master/docs/architecture/adr-031-msg-service.md
 func AddMsgServerConventionToLegacyModule(opts *MsgServerOptions) (*genny.Generator, error) {
@@ -49,7 +51,6 @@ func handlerPatch(moduleName string) genny.RunFn {
 		old := "func NewHandler(k keeper.Keeper) sdk.Handler {"
 		new := fmt.Sprintf(`%v
 %v`, old, typed.PlaceholderHandlerMsgServer)
-
 		content := strings.Replace(f.String(), old, new, 1)
 
 		newFile := genny.NewFileS(path, content)
@@ -64,11 +65,19 @@ func codecPath(moduleName string) genny.RunFn {
 		if err != nil {
 			return err
 		}
+
+		// Add msgservice import
+		old := "import("
+		new := fmt.Sprintf(`%v
+%v`, old, msgServiceImport)
+		content := strings.Replace(f.String(), old, new, 1)
+
+		// Add RegisterMsgServiceDesc method call
 		template := `%[1]v
 
 msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)`
 		replacement := fmt.Sprintf(template, module.Placeholder3)
-		content := strings.Replace(f.String(), module.Placeholder3, replacement, 1)
+		content = strings.Replace(content, module.Placeholder3, replacement, 1)
 		newFile := genny.NewFileS(path, content)
 		return r.File(newFile)
 	}
