127912db395914b21985307838fb87889cd81823
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2021-04-19 13:42:46 +0200

    fix: patch module when msgserver convention is not supported (#1000)

--- starport/services/scaffolder/component.go
+++ starport/services/scaffolder/component.go
@@ -1,9 +1,13 @@
 package scaffolder
 
 import (
+	"context"
 	"go/token"
 	"os"
 	"path/filepath"
+
+	"github.com/gobuffalo/genny"
+	modulecreate "github.com/tendermint/starport/starport/templates/module/create"
 )
 
 // isComponentCreated checks if the component has been already created with Starport in the project
@@ -24,6 +28,31 @@ func isComponentCreated(appPath, moduleName, compName string) (bool, error) {
 		return created, nil
 	}
 	return isMsgCreated(appPath, moduleName, compName)
+}
+
+// supportMsgServer checks if the module supports the MsgServer convention
+// if not, the module codebase is modified to support it
+// https://github.com/cosmos/cosmos-sdk/blob/master/docs/architecture/adr-031-msg-service.md
+func supportMsgServer(
+	appPath string,
+	opts *modulecreate.MsgServerOptions,
+) error {
+	// Check if convention used
+	msgServerDefined, err := isMsgServerDefined(appPath, opts.ModuleName)
+	if err != nil {
+		return err
+	}
+	if !msgServerDefined {
+		// Patch the module to support the convention
+		g, err := modulecreate.AddMsgServerConventionToLegacyModule(opts)
+		if err != nil {
+			return err
+		}
+		run := genny.WetRunner(context.Background())
+		run.With(g)
+		return run.Run()
+	}
+	return nil
 }
 
 // isMsgServerDefined checks if the module uses the MsgServer convention for transactions
@@ -56,6 +85,7 @@ func isForbiddenComponentName(name string) bool {
 	return isGoReservedWord(name)
 }
 
+// isGoReservedWord checks if the name can't be used because it is a go reserved keyword
 func isGoReservedWord(name string) bool {
 	// Check keyword or literal
 	if token.Lookup(name).IsKeyword() {
--- starport/templates/module/create/options.go
+++ starport/templates/module/create/options.go
@@ -14,6 +14,14 @@ type CreateOptions struct {
 	IBCOrdering string
 }
 
+// CreateOptions defines options to add MsgServer
+type MsgServerOptions struct {
+	ModuleName string
+	ModulePath string
+	AppName    string
+	OwnerName  string
+}
+
 // Validate that options are usable
 func (opts *CreateOptions) Validate() error {
 	return nil
