cc77229d287a0a99333e5426d0f64a25c8d99a16
ltacker <lucas.bertrand.22@gmail.com>
2021-04-22 14:06:12 +0200

    Add comments

--- starport/pkg/cmdrunner/cmdrunner.go
+++ starport/pkg/cmdrunner/cmdrunner.go
@@ -27,71 +27,71 @@ type Option func(*Runner)
 
 // DefaultStdout provides the default stdout for the commands to run
 func DefaultStdout(writer io.Writer) Option {
-	return func(r *Runner) {
-		r.stdout = writer
+	return func(runner *Runner) {
+		runner.stdout = writer
 	}
 }
 
 // DefaultStderr provides the default stderr for the commands to run
 func DefaultStderr(writer io.Writer) Option {
-	return func(r *Runner) {
-		r.stderr = writer
+	return func(runner *Runner) {
+		runner.stderr = writer
 	}
 }
 
 // DefaultStdin provides the default stdin for the commands to run
 func DefaultStdin(reader io.Reader) Option {
-	return func(r *Runner) {
-		r.stdin = reader
+	return func(runner *Runner) {
+		runner.stdin = reader
 	}
 }
 
 // DefaultWorkdir provides the default working directory for the commands to run
 func DefaultWorkdir(path string) Option {
-	return func(r *Runner) {
-		r.workdir = path
+	return func(runner *Runner) {
+		runner.workdir = path
 	}
 }
 
 // RunParallel allows the commands to run concurrently
 func RunParallel() Option {
-	return func(r *Runner) {
-		r.runParallel = true
+	return func(runner *Runner) {
+		runner.runParallel = true
 	}
 }
 
 // EndSignal configures s to be signaled to the processes to end them.
 func EndSignal(s os.Signal) Option {
-	return func(r *Runner) {
-		r.endSignal = s
+	return func(runner *Runner) {
+		runner.endSignal = s
 	}
 }
 
 // New returns a new commands runner
 func New(options ...Option) *Runner {
-	r := &Runner{
+	runner := &Runner{
 		endSignal: os.Interrupt,
 	}
-	for _, o := range options {
-		o(r)
+	for _, option := range options {
+		option(runner)
 	}
-	return r
+	return runner
 }
 
 // Run blocks until all steps have completed their executions.
-func (r *Runner) Run(ctx context.Context, steps ...*step.Step) error {
+func (runner *Runner) Run(ctx context.Context, steps ...*step.Step) error {
 	if len(steps) == 0 {
 		return nil
 	}
 	g, ctx := errgroup.WithContext(ctx)
-	for _, s := range steps {
+	for _, step := range steps {
 		// copy s to a new variable to allocate a new address
 		// so we can safely use it inside goroutines spawned in this loop.
-		s := s
+		step := step
 		if err := ctx.Err(); err != nil {
 			return err
 		}
-		if err := s.PreExec(); err != nil {
+		if err := step.PreExec(); err != nil {
 			return err
 		}
 		runPostExecs := func(processErr error) error {
@@ -104,18 +104,18 @@ func (r *Runner) Run(ctx context.Context, steps ...*step.Step) error {
 			} else {
 				err = processErr
 			}
-			for _, exec := range s.PostExecs {
+			for _, exec := range step.PostExecs {
 				if err := exec(err); err != nil {
 					return err
 				}
 			}
-			if len(s.PostExecs) > 0 {
+			if len(step.PostExecs) > 0 {
 				return nil
 			}
 			return err
 		}
-		c := r.newCommand(s)
-		startErr := c.Start()
+		command := runner.newCommand(step)
+		startErr := command.Start()
 		if startErr != nil {
 			if err := runPostExecs(startErr); err != nil {
 				return err
@@ -124,28 +124,28 @@ func (r *Runner) Run(ctx context.Context, steps ...*step.Step) error {
 		}
 		go func() {
 			<-ctx.Done()
-			c.Signal(r.endSignal)
+			command.Signal(runner.endSignal)
 		}()
-		if err := s.InExec(); err != nil {
+		if err := step.InExec(); err != nil {
 			return err
 		}
-		if len(s.WriteData) > 0 {
-			if _, err := c.Write(s.WriteData); err != nil {
+		if len(step.WriteData) > 0 {
+			if _, err := command.Write(step.WriteData); err != nil {
 				return err
 			}
 		}
-		if r.runParallel {
+		if runner.runParallel {
 			g.Go(func() error {
-				return runPostExecs(c.Wait())
+				return runPostExecs(command.Wait())
 			})
-		} else if err := runPostExecs(c.Wait()); err != nil {
+		} else if err := runPostExecs(command.Wait()); err != nil {
 			return err
 		}
 	}
 	return g.Wait()
 }
 
-// Executor is a command to execute
+// Executor represents a command to execute
 type Executor interface {
 	Wait() error
 	Start() error
@@ -153,71 +153,87 @@ type Executor interface {
 	Write(data []byte) (n int, err error)
 }
 
+// dummyExecutor is an executor that does nothing
 type dummyExecutor struct{}
 
-func (s *dummyExecutor) Start() error { return nil }
+func (exec *dummyExecutor) Start() error { return nil }
 
-func (s *dummyExecutor) Wait() error { return nil }
+func (exec *dummyExecutor) Wait() error { return nil }
 
-func (s *dummyExecutor) Signal(os.Signal) {}
+func (exec *dummyExecutor) Signal(os.Signal) {}
 
-func (s *dummyExecutor) Write([]byte) (int, error) { return 0, nil }
+func (exec *dummyExecutor) Write([]byte) (int, error) { return 0, nil }
 
+// cmdSignal is an executor with signal processing
 type cmdSignal struct {
+	*exec.Cmd
+}
+
+func (exec *cmdSignal) Signal(s os.Signal) { exec.Cmd.Process.Signal(s) }
+
+func (exec *cmdSignal) Write(data []byte) (n int, err error) { return 0, nil }
+
+// cmdSignalWithWriter is an executor with signal processing and that can write into stdin
+type cmdSignalWithWriter struct {
 	*exec.Cmd
 	w io.WriteCloser
 }
 
-func (c *cmdSignal) Signal(s os.Signal) { c.Cmd.Process.Signal(s) }
+func (exec *cmdSignalWithWriter) Signal(s os.Signal) { exec.Cmd.Process.Signal(s) }
 
-func (c *cmdSignal) Write(data []byte) (n int, err error) {
-	defer c.w.Close()
-	return c.w.Write(data)
+func (exec *cmdSignalWithWriter) Write(data []byte) (n int, err error) {
+	defer exec.w.Close()
+	return exec.w.Write(data)
 }
-
-type cmdSignalNoWriter struct {
-	*exec.Cmd
-}
-
-func (c *cmdSignalNoWriter) Signal(s os.Signal) { c.Cmd.Process.Signal(s) }
-
-func (c *cmdSignalNoWriter) Write(data []byte) (n int, err error) { return 0, nil }
 
 // newCommand returns a new command to execute
-func (r *Runner) newCommand(s *step.Step) Executor {
-	if s.Exec.Command == "" {
+func (runner *Runner) newCommand(step *step.Step) Executor {
+	// Return a dummy executor in case of an empty command
+	if step.Exec.Command == "" {
 		return &dummyExecutor{}
 	}
 	var (
-		stdout = s.Stdout
-		stderr = s.Stderr
-		dir    = s.Workdir
+		stdout = step.Stdout
+		stderr = step.Stderr
+		stdin  = step.Stdin
+		dir    = step.Workdir
 	)
+
+	// Define standard input and outputs
 	if stdout == nil {
-		stdout = r.stdout
+		stdout = runner.stdout
 	}
 	if stderr == nil {
-		stderr = r.stderr
+		stderr = runner.stderr
+	}
+	if stdin == nil {
+		stdin = runner.stdin
 	}
 	if dir == "" {
-		dir = r.workdir
-	}
-	c := exec.Command(s.Exec.Command, s.Exec.Args...)
-	c.Stdout = stdout
-	c.Stderr = stderr
-	c.Dir = dir
-	c.Env = append(os.Environ(), s.Env...)
-	c.Env = append(c.Env, os.ExpandEnv(fmt.Sprintf("PATH=$PATH:%s", goenv.GetGOBIN())))
-
-	if r.stdin != nil {
-		c.Stdin = r.stdin
-		return &cmdSignalNoWriter{c}
+		dir = runner.workdir
 	}
 
-	w, err := c.StdinPipe()
+	// Initialize command
+	command := exec.Command(step.Exec.Command, step.Exec.Args...)
+	command.Stdout = stdout
+	command.Stderr = stderr
+	command.Dir = dir
+	command.Env = append(os.Environ(), step.Env...)
+	command.Env = append(command.Env, os.ExpandEnv(
+		fmt.Sprintf("PATH=$PATH:%s", goenv.GetGOBIN()),
+	))
+
+	// If a custom stdin is provided it will be as the stdin for the command
+	if stdin != nil {
+		command.Stdin = stdin
+		return &cmdSignal{command}
+	}
+
+	// If no custom stdin, the executor can write into the stdin of the program
+	writer, err := command.StdinPipe()
 	if err != nil {
 		// TODO do not panic
 		panic(err)
 	}
-	return &cmdSignal{c, w}
+	return &cmdSignalWithWriter{command, writer}
 }
