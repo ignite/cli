c43016c4a8331c989d7f6eb553cd1f5a9ecb8426
Clockwork <alex@clockwork.gr>
2021-05-11 17:49:55 +0300

    feat: Config files and chain calls

--- scripts/data/gen-nodetime/src/relayer/lib/chain.ts
+++ scripts/data/gen-nodetime/src/relayer/lib/chain.ts
@@ -1,26 +1,71 @@
-export const ensureChainSetupMethod = "ensureChainSetup"
-export const createPathMethod = "createPath"
-export const getPathMethod = "getPath"
-export const listPathsMethod = "listPaths"
-export const getDefaultAccountMethod = "getDefaultAccount"
-export const getDefaultAccountBalanceMethod = "getDefaultAccountBalance"
-
-class EnsureChainSetupResponse {
-  // id is the chain id of chain.
-  id: string
+export const ensureChainSetupMethod = "ensureChainSetup";
+export const createPathMethod = "createPath";
+export const getPathMethod = "getPath";
+export const listPathsMethod = "listPaths";
+export const getDefaultAccountMethod = "getDefaultAccount";
+export const getDefaultAccountBalanceMethod = "getDefaultAccountBalance";
+import { readOrCreateConfig, writeConfig } from "./persistence";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { GasPrice } from "@cosmjs/stargate";
+import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
+import { stringToPath } from "@cosmjs/crypto";
+import { IbcClient } from "@confio/relayer/build";
+import { Coin } from "@cosmjs/stargate";
+type EnsureChainSetupResponse = {
+	// id is the chain id of chain.
+	id: string;
+};
+interface Chain {
+	chainId: string;
+	rpcAddr: string;
+	addrPrefix: string;
+	gasPrice: string;
 }
-
 // ensureChainSetup sets up a chain by its rpc address only if it is not set up already.
-export function ensureChainSetup([rpcAddr]: [string]): EnsureChainSetupResponse {
-  throw new Error("ensureChainSetup() not implemented");
+export async function ensureChainSetup([rpcAddr, gasPrice, addrPrefix]: [
+	string,
+	string,
+	string
+]): Promise<EnsureChainSetupResponse> {
+	try {
+		const config = readOrCreateConfig();
+		const tmClient = await Tendermint34Client.connect(rpcAddr);
+		let status = await tmClient.status();
+		const chain = {
+			chainId: status.nodeInfo.network,
+			rpcAddr,
+			addrPrefix,
+			gasPrice,
+		};
+		if (!config.chains) {
+			config.chains = [];
+			config.chains.push(chain);
+			writeConfig(config);
+			return { id: chain.chainId };
+		}
+		if (
+			config.chains &&
+			config.chains.find(
+				(x) => x.chainId == chain.chainId && x.rpcAddr == chain.rpcAddr
+			)
+		) {
+			throw new Error("chain already exists");
+		} else {
+			config.chains.push(chain);
+			writeConfig(config);
+			return { id: chain.chainId };
+		}
+	} catch (e) {
+		throw new Error("Could not setup chain: " + e);
+	}
 }
 
 interface ConnectOptions {
-  sourcePort: string
-  sourceVersion: string
-  targetPort: string
-  targetVersion: string
-  ordering: string
+	sourcePort: string;
+	sourceVersion: string;
+	targetPort: string;
+	targetVersion: string;
+	ordering: string;
 }
 
 // createPath creates a path between the source chain and dest chain by their chain ids with given options.
@@ -28,57 +73,130 @@ interface ConnectOptions {
 //
 // createPath should only record the intention of connecting source and destion chains together
 // and should not send any txs to these chains. this will later be done by link().
-export function createPath([srcID, dstID, options]: [string, string, ConnectOptions]): Path {
-  throw new Error("createPath() not implemented");
+export function createPath([srcID, dstID, options]: [
+	string,
+	string,
+	ConnectOptions
+]): Path {
+	try {
+		const config = readOrCreateConfig();
+		let path = {
+			id: srcID + "-" + dstID,
+			isLinked: false,
+			src: {
+				chainID: srcID,
+				portID: options.sourcePort,
+			},
+			dst: {
+				chainID: dstID,
+				portID: options.targetPort,
+			},
+		};
+		if (!config.paths) {
+			config.paths = [];
+		}
+		config.paths.push({ path, options });
+		writeConfig(config);
+		return path;
+	} catch (e) {
+		throw new Error("Could not create path:" + e);
+	}
 }
 
 // Path represents the connection between two chaons.
-class Path {
-  // id of the path.
-  id: string
+interface Path {
+	// id of the path.
+	id: string;
 
-  // isLinked shows whether src and dst chains are connected on the chain with ibc txs.
-  isLinked: boolean
+	// isLinked shows whether src and dst chains are connected on the chain with ibc txs.
+	isLinked: boolean;
 
-  // src represents the source chain.
-  src: PathEnd
+	// src represents the source chain.
+	src: PathEnd;
 
-  // dst represents the destionation chain.
-  dst: PathEnd
+	// dst represents the destionation chain.
+	dst: PathEnd;
 }
 
 // PathEnd represents a chain.
-class PathEnd {
-  channelID: string
-  chainID: string
-  portID: string
+interface PathEnd {
+	channelID?: string;
+	chainID: string;
+	portID: string;
 }
 
 // getPath gets connection info between chains by path id.
 export function getPath([id]: [string]): Path {
-  throw new Error("getPath() not implemented");
+	const config = readOrCreateConfig();
+	if (config.paths) {
+		let path = config.paths.find((x) => x.path.id == id);
+		if (path) {
+			return path;
+		} else {
+			throw new Error("Path does not exist");
+		}
+	} else {
+		throw new Error("Path does not exist");
+	}
 }
 
 // listPaths list all connections.
-export async function listPaths(): Promise<Path[]> {
-  throw new Error("listPaths() not implemented");
+export function listPaths(): Path[] {
+	const config = readOrCreateConfig();
+	if (config.paths) {
+		let paths = config.paths.map((x) => x.path);
+		return paths;
+	} else {
+		throw new Error("No paths defined");
+	}
 }
 
-class Account {
-  address: string
+interface Account {
+	address: string;
 }
 
 // getDefaultAccount gets the default account on chain by chain id.
-export function getDefaultAccount([chainID]: [string]): Account {
-  throw new Error("getDefaultAccount() not implemented");
-}
-
-class Coin {
-  denom: string
-  amount: number
+export async function getDefaultAccount([chainID]: [string]): Promise<Account> {
+	const config = readOrCreateConfig();
+	const chain = config.chains.find((x) => x.id == chainID);
+	if (chain) {
+		let signer = await DirectSecp256k1HdWallet.fromMnemonic(config.mnemonic, {
+			hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
+			prefix: chain.addrPrefix,
+		});
+		const [account] = await signer.getAccounts();
+		return {
+			address: account.address,
+		};
+	} else {
+		throw new Error("Chain not found");
+	}
 }
 
 // getDefaultAccountBalance gets the balance of default account on chain by chain id.
-export function getDefaultAccountBalance([chainID]: [string]): Coin[] {
-  throw new Error("getDefaultAccountBalance() not implemented");
+export async function getDefaultAccountBalance([chainID]: [string]): Promise<
+	Coin[]
+> {
+	const config = readOrCreateConfig();
+	const chain = config.chains.find((x) => x.id == chainID);
+	if (chain) {
+		let signer = await DirectSecp256k1HdWallet.fromMnemonic(config.mnemonic, {
+			hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
+			prefix: chain.addrPrefix,
+		});
+		const [account] = await signer.getAccounts();
+
+		const client = await IbcClient.connectWithSigner(
+			chain.rpcAddr,
+			signer,
+			account.address,
+			{
+				prefix: chain.addrPrefix,
+				gasPrice: GasPrice.fromString(chain.gasPrice),
+			}
+		);
+		return await client.query.bank.allBalances(account.address);
+	} else {
+		throw new Error("Chain not found");
+	}
 }
---
+++ scripts/data/gen-nodetime/src/relayer/lib/persistence.ts
@@ -0,0 +1,54 @@
+import os from "os";
+import yaml from "js-yaml";
+import fs from "fs";
+import { Bip39, Random } from "@cosmjs/crypto";
+
+export function generateMnemonic(): string {
+	return Bip39.encode(Random.getBytes(16)).toString();
+}
+const homedir = os.homedir();
+
+export function createConfigFolder() {
+	const configPath = homedir + "/.ts-relayer";
+	try {
+		if (!fs.existsSync(configPath)) {
+			fs.mkdirSync(configPath);
+		}
+	} catch (e) {
+		throw new Error("Could not create config folder: " + e);
+	}
+}
+
+export function readOrCreateConfig() {
+	createConfigFolder();
+	try {
+		if (!fs.existsSync(homedir + "/.ts-relayer/config.yaml")) {
+			let configFile = fs.readFileSync(
+				homedir + "/.ts-relayer/config.yaml",
+				"utf8"
+			);
+			return yaml.safeLoad(configFile);
+		} else {
+			let config = {
+				mnemonic: Bip39.encode(Random.getBytes(32)).toString(),
+			};
+			let configFile = yaml.safeDump(config);
+			fs.writeFileSync(
+				homedir + "/.ts-relayer/config.yaml",
+				configFile,
+				"utf8"
+			);
+			return config;
+		}
+	} catch (e) {
+		throw new Error("Failed reading config: " + e);
+	}
+}
+export function writeConfig(config) {
+	try {
+		let configFile = yaml.safeDump(config);
+		fs.writeFileSync(homedir + "/.ts-relayer/config.yaml", configFile, "utf8");
+	} catch (e) {
+		throw new Error("Failed writing  config: " + e);
+	}
+}
