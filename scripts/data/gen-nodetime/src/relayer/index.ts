a59ee754a6f92385eade807594707e911b75e719
İlker G. Öztürk <ilkergoktugozturk@gmail.com>
2021-05-18 18:47:45 +0300

    refactor: ts relayer (#1147)

--- scripts/data/gen-nodetime/src/relayer/index.ts
+++ scripts/data/gen-nodetime/src/relayer/index.ts
@@ -1,28 +1,17 @@
 import run from "./jsonrpc";
 
-import {
-	linkMethod,
-	startMethod,
-	ensureChainSetupMethod,
-	createPathMethod,
-	getPathMethod,
-	listPathsMethod,
-	getDefaultAccountMethod,
-	getDefaultAccountBalanceMethod,
-	infoMethod,
-} from "./lib/relayer";
 import Relayer from "./lib/relayer";
 
 const relayer = new Relayer();
 
 run([
-	[linkMethod, relayer.link.bind(relayer)],
-	[startMethod, relayer.start.bind(relayer)],
-	[ensureChainSetupMethod, relayer.ensureChainSetup.bind(relayer)],
-	[createPathMethod, relayer.createPath.bind(relayer)],
-	[getPathMethod, relayer.getPath.bind(relayer)],
-	[listPathsMethod, relayer.listPaths.bind(relayer)],
-	[getDefaultAccountMethod, relayer.getDefaultAccount.bind(relayer)],
-	[getDefaultAccountBalanceMethod, relayer.getDefaultAccountBalance.bind(relayer)],
-	[infoMethod, relayer.info.bind(relayer)],
+	["link", relayer.link.bind(relayer)],
+	["start", relayer.start.bind(relayer)],
+	["ensureChainSetup", relayer.ensureChainSetup.bind(relayer)],
+	["createPath", relayer.createPath.bind(relayer)],
+	["getPath", relayer.getPath.bind(relayer)],
+	["listPaths", relayer.listPaths.bind(relayer)],
+	["getDefaultAccount", relayer.getDefaultAccount.bind(relayer)],
+	["getDefaultAccountBalance", relayer.getDefaultAccountBalance.bind(relayer)],
+	["info", relayer.info.bind(relayer)],
 ]);
