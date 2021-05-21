5c8896865f6189f4856e4744d5ed2ff4d55dad1d
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2021-05-21 09:39:22 +0300

    move openapiconsole pkg out of Starport
    this is needed because it's also imported as a Go package inside user's
    app. Starport takes long time to get downloaded with go get because of the
    nodetime binaries which makes user apps to build extensively slow.

--- starport/pkg/cosmosfaucet/http.go
+++ starport/pkg/cosmosfaucet/http.go
@@ -4,8 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/ilgooz/openapiconsole"
 	"github.com/rs/cors"
-	"github.com/tendermint/starport/starport/pkg/openapiconsole"
 )
 
 // ServeHTTP implements http.Handler to expose the functionality of Faucet.Transfer() via HTTP.
