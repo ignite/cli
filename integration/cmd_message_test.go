4d2039f9d320adda9857c0616fa28b193b438f8b
ltacker <lucas.bertrand.22@gmail.com>
2021-06-09 13:28:13 +0200

    Fix tests

--- integration/cmd_message_test.go
+++ integration/cmd_message_test.go
@@ -38,7 +38,7 @@ func TestGenerateAnAppWithMessage(t *testing.T) {
 
 	env.Must(env.Exec("create a module",
 		step.NewSteps(step.New(
-			step.Exec("starport", "module", "create", "do-foo", "--require-registration"),
+			step.Exec("starport", "module", "create", "foo", "--require-registration"),
 			step.Workdir(path),
 		)),
 	))
