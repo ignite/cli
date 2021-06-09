9f60694c8ba71775ecdb9ca3341f80427a676781
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2021-06-09 16:30:07 +0300

    feat(build): add --release option
    when using this option, a release/ dir will be created under the app's source
    code with app binaries inside tarballs built for different os/arch targets and a
    checksum.txt file.
    
    * done some refactoring.

--- starport/pkg/gocmd/gocmd.go
+++ starport/pkg/gocmd/gocmd.go
@@ -1,6 +1,44 @@
 package gocmd
 
-import "os"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/tendermint/starport/starport/pkg/cmdrunner/exec"
+	"github.com/tendermint/starport/starport/pkg/cmdrunner/step"
+)
+
+const (
+	// CommandInstall represents go "install" command.
+	CommandInstall = "install"
+
+	// CommandBuild represents go "build" command.
+	CommandBuild = "build"
+
+	// CommandMod represents go "mod" command.
+	CommandMod = "mod"
+
+	// CommandModTidy represents go mod "tidy" command.
+	CommandModTidy = "tidy"
+
+	// CommandModVerify represents go mod "tidy" command.
+	CommandModVerify = "verify"
+)
+
+const (
+	FlagMod              = "-mod"
+	FlagModValueReadOnly = "readonly"
+	FlagLdflags          = "-ldflags"
+	FlagOut              = "-o"
+)
+
+const (
+	EnvGOOS   = "GOOS"
+	EnvGOARCH = "GOARCH"
+)
 
 // Name returns the name of Go binary to use.
 func Name() string {
@@ -9,4 +47,57 @@ func Name() string {
 		return custom
 	}
 	return "go"
+}
+
+// ModTidy runs go mod tidy on path with options.
+func ModTidy(ctx context.Context, path string, options ...exec.Option) error {
+	return exec.Exec(ctx, []string{Name(), CommandMod, CommandModTidy}, append(options, exec.StepOption(step.Workdir(path)))...)
+}
+
+// ModVerify runs go mod verify on path with options.
+func ModVerify(ctx context.Context, path string, options ...exec.Option) error {
+	return exec.Exec(ctx, []string{Name(), CommandMod, CommandModVerify}, append(options, exec.StepOption(step.Workdir(path)))...)
+}
+
+// BuildAll runs go build ./... on path with options.
+func BuildAll(ctx context.Context, out, path string, flags []string, options ...exec.Option) error {
+	command := []string{
+		Name(),
+		CommandBuild,
+		FlagOut, out,
+	}
+	command = append(command, flags...)
+	command = append(command, "./...")
+	return exec.Exec(ctx, command, append(options, exec.StepOption(step.Workdir(path)))...)
+}
+
+// InstallAll runs go install ./... on path with options.
+func InstallAll(ctx context.Context, path string, flags []string, options ...exec.Option) error {
+	command := []string{
+		Name(),
+		CommandInstall,
+	}
+	command = append(command, flags...)
+	command = append(command, "./...")
+	return exec.Exec(ctx, command, append(options, exec.StepOption(step.Workdir(path)))...)
+}
+
+// Ldflags returns a combined ldflags set from flags.
+func Ldflags(flags ...string) string {
+	return strings.Join(flags, " ")
+}
+
+// BuildTarget builds a GOOS:GOARCH pair.
+func BuildTarget(goos, goarch string) string {
+	return fmt.Sprintf("%s:%s", goos, goarch)
+}
+
+// ParseTarget parses GOOS:GOARCH pair.
+func ParseTarget(t string) (goos, goarch string, err error) {
+	parsed := strings.Split(t, ":")
+	if len(parsed) != 2 {
+		return "", "", errors.New("invalid Go target, expected in GOOS:GOARCH format")
+	}
+
+	return parsed[0], parsed[1], nil
 }
