74d529baab7ae061b6830146fccbbcfd0d808f68
ltacker <lucas.bertrand.22@gmail.com>
2021-06-09 09:14:47 +0200

    multiformatname lib

---
+++ starport/pkg/multiformatname/multiformatname.go
@@ -0,0 +1,47 @@
+// Package multiformatname provides names automatically converted into multiple naming convention
+package multiformatname
+
+import (
+	"errors"
+	"fmt"
+	"github.com/iancoleman/strcase"
+)
+
+// MultiFormatName represents a name with multiple naming convention representations
+// Supported naming convention are: camel, pascal, and kebab cases
+type MultiFormatName struct {
+	LowerCamel string
+	UpperCamel string
+	Kebab      string
+}
+
+// NewMultiFormatName returns a new multi-format name from a name
+func NewMultiFormatName(name string) (MultiFormatName, error) {
+	if err := CheckName(name); err != nil {
+		return MultiFormatName{}, err
+	}
+
+	return MultiFormatName{
+		LowerCamel: strcase.ToLowerCamel(name),
+		UpperCamel: strcase.ToCamel(name),
+		Kebab:      strcase.ToKebab(name),
+	}, nil
+}
+
+// CheckName checks name validity
+func CheckName(name string) error {
+	if name == "" {
+		return errors.New("name cannot be empty")
+	}
+
+	// check  characters
+	for _, c := range name {
+		// A name can contains letter, hyphen or underscore
+		authorized := ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '-' || c == '_'
+		if !authorized {
+			return fmt.Errorf("name cannot contain %v", string(c))
+		}
+	}
+
+	return nil
+}
---
+++ starport/pkg/multiformatname/multiformatname_test.go
@@ -0,0 +1,62 @@
+package multiformatname_test
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/starport/starport/pkg/multiformatname"
+	"testing"
+)
+
+func TestNewMultiFormatName(t *testing.T) {
+	// [valueToTest, lowerCamel, upperCamel, kebabCase]
+	cases := [][4]string{
+		{"foo", "foo", "Foo", "foo"},
+		{"fooBar", "fooBar", "FooBar", "foo-bar"},
+		{"foo-bar", "fooBar", "FooBar", "foo-bar"},
+		{"foo_bar", "fooBar", "FooBar", "foo-bar"},
+		{"foo_barFoobar", "fooBarFoobar", "FooBarFoobar", "foo-bar-foobar"},
+		{"foo_-_bar", "fooBar", "FooBar", "foo---bar"},
+		{"foo_-_Bar", "fooBar", "FooBar", "foo---bar"},
+		{"fooBAR", "fooBAR", "FooBAR", "foo-bar"},
+	}
+
+	// test cases
+	for _, testCase := range cases {
+		name, err := multiformatname.NewMultiFormatName(testCase[0])
+		require.NoError(t, err)
+		require.Equal(
+			t,
+			testCase[1],
+			name.LowerCamel,
+			fmt.Sprintf("%s should be converted to the correct lower camel format", testCase[0]),
+		)
+		require.Equal(
+			t,
+			testCase[2],
+			name.UpperCamel,
+			fmt.Sprintf("%s should be converted the correct upper camel format", testCase[0]),
+		)
+		require.Equal(
+			t,
+			testCase[3],
+			name.Kebab,
+			fmt.Sprintf("%s should be converted the correct kebab format", testCase[0]),
+		)
+	}
+}
+
+func TestNewMultiFormatName2(t *testing.T) {
+	// Test forbidden names
+	cases := []string{
+		"",
+		"foo bar",
+		"foo123",
+		"@foo",
+	}
+
+	// test cases
+	for _, testCase := range cases {
+		_, err := multiformatname.NewMultiFormatName(testCase)
+		require.Error(t, err)
+	}
+}
