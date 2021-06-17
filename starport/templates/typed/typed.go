020878a29badfb6d290f62230afbff7a3df5adff
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2021-06-17 09:58:52 +0200

    fix: use snake case for generated filenames (#1251)
    * Snake case
    
    * Rename files
    
    * Lint
    
    * Fix proto import
    
    * Changelog

--- starport/templates/typed/typed.go
+++ starport/templates/typed/typed.go
@@ -57,7 +57,6 @@ func Box(box packd.Walker, opts *Options, g *genny.Generator) error {
 
 	g.Transformer(plushgen.Transformer(ctx))
 	g.Transformer(genny.Replace("{{moduleName}}", opts.ModuleName))
-	g.Transformer(genny.Replace("{{typeName}}", opts.TypeName.LowerCamel))
-	g.Transformer(genny.Replace("{{TypeName}}", opts.TypeName.UpperCamel))
+	g.Transformer(genny.Replace("{{typeName}}", opts.TypeName.Snake))
 	return nil
 }
