49ee9c03c6993505e642f5fae5fa3946246ff4fd
ltacker <lucas.bertrand.22@gmail.com>
2021-06-29 15:05:46 +0200

    Add tests for app analysis

---
+++ starport/pkg/cosmosanalysis/app/app_test.go
@@ -0,0 +1,51 @@
+package app_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/starport/starport/pkg/cosmosanalysis/app"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+var (
+	source = []byte(`
+package foo
+
+type App struct {
+	FooKeeper foo.keeper
+}
+`)
+
+	sourceNoApp = []byte(`
+package foo
+
+type Bar struct {
+	FooKeeper foo.keeper
+}
+`)
+)
+
+func TestCheckKeeper(t *testing.T) {
+	tmpDir := os.TempDir()
+
+	// Test with a source file containing an app
+	tmpFile := filepath.Join(tmpDir, "source")
+	err := os.WriteFile(tmpFile, source, 0644)
+	require.NoError(t, err)
+	t.Cleanup(func() {os.Remove(tmpFile)})
+
+	err = app.CheckKeeper(tmpFile, "FooKeeper")
+	require.NoError(t, err)
+	err = app.CheckKeeper(tmpFile, "BarKeeper")
+	require.Error(t, err)
+
+	// No app in source must return an error
+	tmpFileNoApp := filepath.Join(tmpDir, "source")
+	err = os.WriteFile(tmpFileNoApp, sourceNoApp, 0644)
+	require.NoError(t, err)
+	t.Cleanup(func() {os.Remove(tmpFileNoApp)})
+
+	err = app.CheckKeeper(tmpFileNoApp, "FooKeeper")
+	require.Error(t, err)
+}
