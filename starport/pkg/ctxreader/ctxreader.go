8e36207277d1637acdb72b0d25beed52832d08d5
Pantani <danpantani@gmail.com>
2021-07-14 15:19:20 -0300

    fix Reader data race

--- starport/pkg/ctxreader/ctxreader.go
+++ starport/pkg/ctxreader/ctxreader.go
@@ -6,10 +6,16 @@ import (
 	"io"
 )
 
-type cancelableReader struct {
-	io.Reader
-	ctx context.Context
-}
+type (
+	cancelableReader struct {
+		io.Reader
+		ctx context.Context
+	}
+	ioReadResult struct {
+		n   int
+		err error
+	}
+)
 
 // New returns a new reader that emits a context error through its r.Read() method
 // when ctx canceled.
@@ -18,18 +24,21 @@ func New(ctx context.Context, r io.Reader) io.Reader {
 }
 
 func (r *cancelableReader) Read(data []byte) (n int, err error) {
-	isRead := make(chan struct{})
+	chData := make([]byte, len(data))
+	chResult := make(chan ioReadResult, 1)
 
 	go func() {
-		n, err = r.Reader.Read(data)
-		close(isRead)
+		n, err := r.Reader.Read(chData)
+		chResult <- ioReadResult{n, err}
+		close(chResult)
 	}()
 
 	select {
 	case <-r.ctx.Done():
 		return 0, r.ctx.Err()
 
-	case <-isRead:
-		return
+	case ret := <-chResult:
+		copy(data, chData)
+		return ret.n, ret.err
 	}
 }
