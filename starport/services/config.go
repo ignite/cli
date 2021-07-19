05b4b9d502fd35c9b4beb92a5e7bdd1312c99376
ltacker <lucas.bertrand.22@gmail.com>
2021-07-19 11:24:32 +0200

    Init config

--- starport/services/config.go
+++ starport/services/config.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"os"
+
 	"github.com/tendermint/starport/starport/pkg/xfilepath"
 )
 
@@ -8,3 +10,16 @@ var (
 	// StarportConfPath returns the Starport Configuration directory
 	StarportConfPath = xfilepath.JoinFromHome(xfilepath.Path(".starport"))
 )
+
+// InitConfig creates config directory if it is not yet created
+func InitConfig() error {
+	confPath, err := StarportConfPath()
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(confPath, 0700); err != nil && !os.IsExist(err) {
+		return err
+	}
+	return nil
+}
