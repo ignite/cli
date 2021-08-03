3982708a137f6326cbcad9d7236a750d5d0c0f70
İlker G. Öztürk <ilkergoktugozturk@gmail.com>
2021-08-03 11:26:40 +0300

    feat(templates/cmd): upgrade spm to add 'config' cmd (#1417)
    * feat(templates/cmd): upgrade spm to add 'config' cmd
    
    to appd.
    
    * also make $HOME/config/client.toml configurable through 'init.client'
    section of chain's Starport config.yml.
    
    * default to test
    
    * fix
    
    * fix
    
    * updated changelog
    
    * fix config application order
    
    * fix
    
    Co-authored-by: Denis Fadeev <denis@fadeev.org>

--- starport/services/chain/plugin-stargate.go
+++ starport/services/chain/plugin-stargate.go
@@ -29,14 +29,6 @@ func (p *stargatePlugin) Name() string {
 	return "Stargate"
 }
 
-func (p *stargatePlugin) Setup(ctx context.Context) error {
-	return nil
-}
-
-func (p *stargatePlugin) Configure(_ context.Context, _ chaincmdrunner.Runner, _ string) error {
-	return nil
-}
-
 func (p *stargatePlugin) Gentx(ctx context.Context, runner chaincmdrunner.Runner, v Validator) (path string, err error) {
 	return runner.Gentx(
 		ctx,
@@ -51,14 +43,17 @@ func (p *stargatePlugin) Gentx(ctx context.Context, runner chaincmdrunner.Runner
 	)
 }
 
-func (p *stargatePlugin) PostInit(homePath string, conf starportconf.Config) error {
-	if err := p.apptoml(homePath, conf); err != nil {
+func (p *stargatePlugin) Configure(homePath string, conf starportconf.Config) error {
+	if err := p.appTOML(homePath, conf); err != nil {
 		return err
 	}
-	return p.configtoml(homePath, conf)
+	if err := p.clientTOML(homePath); err != nil {
+		return err
+	}
+	return p.configTOML(homePath, conf)
 }
 
-func (p *stargatePlugin) apptoml(homePath string, conf starportconf.Config) error {
+func (p *stargatePlugin) appTOML(homePath string, conf starportconf.Config) error {
 	// TODO find a better way in order to not delete comments in the toml.yml
 	path := filepath.Join(homePath, "config/app.toml")
 	config, err := toml.LoadFile(path)
@@ -79,7 +74,7 @@ func (p *stargatePlugin) apptoml(homePath string, conf starportconf.Config) erro
 	return err
 }
 
-func (p *stargatePlugin) configtoml(homePath string, conf starportconf.Config) error {
+func (p *stargatePlugin) configTOML(homePath string, conf starportconf.Config) error {
 	// TODO find a better way in order to not delete comments in the toml.yml
 	path := filepath.Join(homePath, "config/config.toml")
 	config, err := toml.LoadFile(path)
@@ -92,6 +87,26 @@ func (p *stargatePlugin) configtoml(homePath string, conf starportconf.Config) e
 	config.Set("rpc.laddr", xurl.TCP(conf.Host.RPC))
 	config.Set("p2p.laddr", xurl.TCP(conf.Host.P2P))
 	config.Set("rpc.pprof_laddr", conf.Host.Prof)
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = config.WriteTo(file)
+	return err
+}
+
+func (p *stargatePlugin) clientTOML(homePath string) error {
+	path := filepath.Join(homePath, "config/client.toml")
+	config, err := toml.LoadFile(path)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	config.Set("keyring-backend", "test")
+	config.Set("broadcast-mode", "block")
 	file, err := os.OpenFile(path, os.O_RDWR|os.O_TRUNC, 0644)
 	if err != nil {
 		return err
