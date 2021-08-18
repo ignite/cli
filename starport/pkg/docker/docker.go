c70ded39b0597eccb910cfd6237e12e1be1fa5a3
İlker Göktuğ ÖZTÜRK <ilkergoktugozturk@gmail.com>
2021-08-18 14:47:55 +0300

    fix

--- starport/pkg/docker/docker.go
+++ starport/pkg/docker/docker.go
@@ -2,15 +2,10 @@ package docker
 
 import (
 	"os"
-	"strings"
 )
 
 // IsInDocker reports whether if running inside a Docker container or not.
 func IsInDocker() bool {
-	content, err := os.ReadFile("/proc/1/cgroup")
-	if err != nil {
-		return false
-	}
-
-	return strings.Contains(string(content), "docker")
+	_, err := os.Stat(os.ExpandEnv("$HOME/.dockerenv"))
+	return err == nil
 }
