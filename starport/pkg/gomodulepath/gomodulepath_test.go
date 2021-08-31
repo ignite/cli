2ad6a4788207dd3343366f8a7c05ed0cfd067c64
Danilo Pantani <danpantani@gmail.com>
2021-08-30 21:08:33 -0300

    fix TestParse method

--- starport/pkg/gomodulepath/gomodulepath_test.go
+++ starport/pkg/gomodulepath/gomodulepath_test.go
@@ -15,39 +15,57 @@ func TestParse(t *testing.T) {
 		path    Path
 		err     error
 	}{
-		{"standard",
-			"github.com/a/b", Path{"github.com/a/b", "b", "b"}, nil,
+		{
+			name:    "standard",
+			rawpath: "github.com/a/b",
+			path:    Path{RawPath: "github.com/a/b", Root: "b", Package: "b"},
 		},
-		{"with dash",
-			"github.com/a/b-c", Path{"github.com/a/b-c", "b-c", "bc"}, nil,
+		{
+			name:    "with dash",
+			rawpath: "github.com/a/b-c",
+			path:    Path{RawPath: "github.com/a/b-c", Root: "b-c", Package: "bc"},
 		},
-		{"long",
-			"github.com/a/b/c", Path{"github.com/a/b/c", "c", "c"}, nil,
+		{
+			name:    "long",
+			rawpath: "github.com/a/b/c",
+			path:    Path{RawPath: "github.com/a/b/c", Root: "c", Package: "c"},
 		},
-		{"invalid as go.mod module name",
-			"github.com/a/b/c@", Path{}, fmt.Errorf("app name is an invalid go module name: %w",
+		{
+			name:    "invalid as go.mod module name",
+			rawpath: "github.com/a/b/c@",
+			err: fmt.Errorf("app name is an invalid go module name: %w",
 				errors.New(`malformed module path "github.com/a/b/c@": invalid char '@'`)),
 		},
-		{"name starting with the letter v",
-			"github.com/a/vote", Path{"github.com/a/vote", "vote", "vote"}, nil,
+		{
+			name:    "name starting with the letter v",
+			rawpath: "github.com/a/vote",
+			path:    Path{RawPath: "github.com/a/vote", Root: "vote", Package: "vote"},
 		},
-		{"with version",
-			"github.com/a/b/v2", Path{"github.com/a/b/v2", "b", "b"}, nil,
+		{
+			name:    "with version",
+			rawpath: "github.com/a/b/v2",
+			path:    Path{RawPath: "github.com/a/b/v2", Root: "b", Package: "b"},
 		},
-		{"with underscore",
-			"github.com/a/b_c", Path{"github.com/a/b_c", "b_c", "bc"}, nil,
+		{
+			name:    "with underscore",
+			rawpath: "github.com/a/b_c",
+			path:    Path{RawPath: "github.com/a/b_c", Root: "b_c", Package: "bc"},
 		},
-		{"with mixed case",
-			"github.com/a/bC", Path{"github.com/a/bC", "bC", "bc"}, nil,
+		{
+			name:    "with mixed case",
+			rawpath: "github.com/a/bC",
+			path:    Path{RawPath: "github.com/a/bC", Root: "bC", Package: "bc"},
 		},
 	}
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
-			path, err := Parse(tt.rawpath)
-			require.Equal(t, tt.err, err)
+			path, err := Parse(tt.rawpath, "")
 			if err != nil {
+				require.Error(t, err)
+				require.Equal(t, tt.err, err)
 				return
 			}
+			require.NoError(t, err)
 			require.Equal(t, tt.path, path)
 		})
 	}
