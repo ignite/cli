914c8125d3f63fb6bdf5d830162bdb2f532638ac
Danilo Pantani <danpantani@gmail.com>
2021-09-13 18:59:06 -0300

    remove unecessary error check

--- starport/templates/typed/gogoproto.go
+++ starport/templates/typed/gogoproto.go
@@ -12,9 +12,6 @@ const gogoProtoFile = "gogoproto/gogo.proto"
 // EnsureGogoProtoImported add the gogo.proto import in the proto file content in case it's not defined
 func EnsureGogoProtoImported(protoFile, importPlaceholder string) string {
 	err := protoanalysis.IsImported(protoFile, gogoProtoFile)
-	if err != nil {
-		return importPlaceholder
-	}
 	if errors.Is(err, protoanalysis.ErrImportNotFound) {
 		templateGogoProtoImport := `%[1]v
 import "%[2]v";`
