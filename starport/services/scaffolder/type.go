27451f64ebed76f3fb1f4dbf144153f8299c3a6d
Danilo Pantani <danpantani@gmail.com>
2021-09-14 12:04:57 -0300

    fix duplicate PortID into the GenesisState obj

--- starport/services/scaffolder/type.go
+++ starport/services/scaffolder/type.go
@@ -141,6 +141,11 @@ func (s Scaffolder) AddType(
 		return sm, err
 	}
 
+	isIBC, err := isIBCModule(s.path, moduleName)
+	if err != nil {
+		return sm, err
+	}
+
 	var (
 		g    *genny.Generator
 		opts = &typed.Options{
@@ -153,6 +158,7 @@ func (s Scaffolder) AddType(
 			Fields:     tFields,
 			NoMessage:  o.withoutMessage,
 			MsgSigner:  mfSigner,
+			IsIBC:      isIBC,
 		}
 		gens []*genny.Generator
 	)
