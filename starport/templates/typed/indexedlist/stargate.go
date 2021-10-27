6962a5b1d4a4bdb94b12d2139ada9dedd1ed14bc
ltacker <lucas.bertrand.22@gmail.com>
2021-10-27 14:20:50 +0200

    Initialize template

---
+++ starport/templates/typed/indexedlist/stargate.go
@@ -0,0 +1,44 @@
+package indexedlist
+
+import (
+	"embed"
+	"github.com/gobuffalo/genny"
+	"github.com/tendermint/starport/starport/pkg/placeholder"
+	"github.com/tendermint/starport/starport/pkg/xgenny"
+	"github.com/tendermint/starport/starport/templates/typed"
+)
+
+var (
+	//go:embed stargate/component/* stargate/component/**/*
+	fsStargateComponent embed.FS
+
+	//go:embed stargate/messages/* stargate/messages/**/*
+	fsStargateMessages embed.FS
+)
+
+// NewStargate returns the generator to scaffold a new indexed list in a Stargate module
+func NewStargate(replacer placeholder.Replacer, opts *typed.Options) (*genny.Generator, error) {
+	var (
+		g = genny.New()
+
+		messagesTemplate = xgenny.NewEmbedWalker(
+			fsStargateMessages,
+			"stargate/messages/",
+			opts.AppPath,
+		)
+		componentTemplate = xgenny.NewEmbedWalker(
+			fsStargateComponent,
+			"stargate/component/",
+			opts.AppPath,
+		)
+	)
+
+	if !opts.NoMessage {
+		// Messages template
+		if err := typed.Box(messagesTemplate, opts, g); err != nil {
+			return nil, err
+		}
+	}
+
+	return g, typed.Box(componentTemplate, opts, g)
+}
