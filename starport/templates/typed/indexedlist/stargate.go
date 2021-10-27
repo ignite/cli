4acc564485f871c24a9795be149d52263f59e49a
ltacker <lucas.bertrand.22@gmail.com>
2021-10-27 17:14:11 +0200

    Add grpc request

--- starport/templates/typed/indexedlist/stargate.go
+++ starport/templates/typed/indexedlist/stargate.go
@@ -2,6 +2,9 @@ package indexedlist
 
 import (
 	"embed"
+	"fmt"
+	"path/filepath"
+	"strings"
 
 	"github.com/gobuffalo/genny"
 	"github.com/tendermint/starport/starport/pkg/placeholder"
@@ -34,6 +37,9 @@ func NewStargate(replacer placeholder.Replacer, opts *typed.Options) (*genny.Gen
 		)
 	)
 
+	g.RunFn(protoRPCModify(replacer, opts))
+	g.RunFn(moduleGRPCGatewayModify(replacer, opts))
+
 	//if !opts.NoMessage {
 	//	// Messages template
 	//	if err := typed.Box(messagesTemplate, opts, g); err != nil {
@@ -42,4 +48,127 @@ func NewStargate(replacer placeholder.Replacer, opts *typed.Options) (*genny.Gen
 	//}
 
 	return g, typed.Box(componentTemplate, opts, g)
+}
+
+func protoRPCModify(replacer placeholder.Replacer, opts *typed.Options) genny.RunFn {
+	return func(r *genny.Runner) error {
+		path := filepath.Join(opts.AppPath, "proto", opts.ModuleName, "query.proto")
+		f, err := r.Disk.Find(path)
+		if err != nil {
+			return err
+		}
+
+		// Import the type
+		templateImport := `import "%s/%s.proto";
+%s`
+		replacementImport := fmt.Sprintf(templateImport,
+			opts.ModuleName,
+			opts.TypeName.Snake,
+			typed.Placeholder,
+		)
+		content := replacer.Replace(f.String(), typed.Placeholder, replacementImport)
+
+		// Add gogo.proto
+		replacementGogoImport := typed.EnsureGogoProtoImported(path, typed.Placeholder)
+		content = replacer.Replace(content, typed.Placeholder, replacementGogoImport)
+
+		var lowerCamelIndexes []string
+		for _, index := range opts.Indexes {
+			lowerCamelIndexes = append(lowerCamelIndexes, fmt.Sprintf("{%s}", index.Name.LowerCamel))
+		}
+		indexPath := strings.Join(lowerCamelIndexes, "/")
+
+		// Add the service
+		templateService := `// Queries a %[3]v by index.
+	rpc %[2]v(QueryGet%[2]vRequest) returns (QueryGet%[2]vResponse) {
+		option (google.api.http).get = "/%[4]v/%[5]v/%[6]v/%[3]v/%[7]v/{id}";
+	}
+
+	// Queries a list of %[3]v items.
+	rpc %[2]vAll(QueryAll%[2]vRequest) returns (QueryAll%[2]vResponse) {
+		option (google.api.http).get = "/%[4]v/%[5]v/%[6]v/%[3]v/%[7]v";
+	}
+
+%[1]v`
+		replacementService := fmt.Sprintf(templateService, typed.Placeholder2,
+			opts.TypeName.UpperCamel,
+			opts.TypeName.LowerCamel,
+			opts.OwnerName,
+			opts.AppName,
+			opts.ModuleName,
+			indexPath,
+		)
+		content = replacer.Replace(content, typed.Placeholder2, replacementService)
+
+		// Add the service messages
+		var queryIndexFields string
+		for i, index := range opts.Indexes {
+			queryIndexFields += fmt.Sprintf("  %s\n", index.ProtoType(i+1))
+		}
+
+		// Ensure custom types are imported
+		protoImports := opts.Fields.ProtoImports()
+		for _, f := range opts.Fields.Custom() {
+			protoImports = append(protoImports,
+				fmt.Sprintf("%[1]v/%[2]v.proto", opts.ModuleName, f),
+			)
+		}
+		for _, f := range protoImports {
+			importModule := fmt.Sprintf(`
+import "%[1]v";`, f)
+			content = strings.ReplaceAll(content, importModule, "")
+			replacementImport := fmt.Sprintf("%[1]v%[2]v", typed.Placeholder, importModule)
+			content = replacer.Replace(content, typed.Placeholder, replacementImport)
+		}
+
+		templateMessage := `message QueryGet%[2]vRequest {
+	%[4]v
+	uint64 id = %[5]v;
+}
+
+message QueryGet%[2]vResponse {
+	%[2]v %[3]v = 1 [(gogoproto.nullable) = false];
+}
+
+message QueryAll%[2]vRequest {
+	%[4]v
+	cosmos.base.query.v1beta1.PageRequest pagination = %[5]v;
+}
+
+message QueryAll%[2]vResponse {
+	repeated %[2]v %[3]v = 1 [(gogoproto.nullable) = false];
+	cosmos.base.query.v1beta1.PageResponse pagination = 2;
+}
+
+%[1]v`
+		replacementMessage := fmt.Sprintf(templateMessage,
+			typed.Placeholder3,
+			opts.TypeName.UpperCamel,
+			opts.TypeName.LowerCamel,
+			queryIndexFields,
+			len(queryIndexFields)+1,
+		)
+		content = replacer.Replace(content, typed.Placeholder3, replacementMessage)
+
+		newFile := genny.NewFileS(path, content)
+		return r.File(newFile)
+	}
+}
+
+func moduleGRPCGatewayModify(replacer placeholder.Replacer, opts *typed.Options) genny.RunFn {
+	return func(r *genny.Runner) error {
+		path := filepath.Join(opts.AppPath, "x", opts.ModuleName, "module.go")
+		f, err := r.Disk.Find(path)
+		if err != nil {
+			return err
+		}
+		replacement := `"context"`
+		content := replacer.ReplaceOnce(f.String(), typed.Placeholder, replacement)
+
+		replacement = `types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))`
+		content = replacer.ReplaceOnce(content, typed.Placeholder2, replacement)
+
+		newFile := genny.NewFileS(path, content)
+		return r.File(newFile)
+	}
 }
