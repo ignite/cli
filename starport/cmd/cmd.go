01d137ee8c0f93bbc1b198cd247142e7fc57ca29
İlker G. Öztürk <ilkergoktugozturk@gmail.com>
2021-10-28 16:56:34 +0300

    re-add network command

--- starport/cmd/cmd.go
+++ starport/cmd/cmd.go
@@ -61,6 +61,7 @@ starport scaffold chain github.com/cosmonaut/mars`,
 	c.AddCommand(NewScaffold())
 	c.AddCommand(NewChain())
 	c.AddCommand(NewGenerate())
+	c.AddCommand(NewNetwork())
 	c.AddCommand(NewAccount())
 	c.AddCommand(NewRelayer())
 	c.AddCommand(NewTools())
