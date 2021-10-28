034f3abe0afa8f8df3c70500a95fa426f52e5ab7
ltacker <lucas.bertrand.22@gmail.com>
2021-10-28 18:39:42 +0200

    Refactor count

--- starport/templates/typed/indexedlist/genesis.go
+++ starport/templates/typed/indexedlist/genesis.go
@@ -1,8 +1,9 @@
-package list
+package indexedlist
 
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/gobuffalo/genny"
 	"github.com/tendermint/starport/starport/pkg/placeholder"
@@ -47,7 +48,7 @@ func genesisProtoModify(replacer placeholder.Replacer, opts *typed.Options) genn
 		}
 
 		templateProtoState := `repeated %[2]v %[3]vList = %[4]v [(gogoproto.nullable) = false];
-  uint64 %[3]vCount = %[5]v;
+  repeated %[2]vCount %[3]vCountList = %[5]v;
   %[1]v`
 		replacementProtoState := fmt.Sprintf(
 			templateProtoState,
@@ -78,6 +79,7 @@ func genesisTypesModify(replacer placeholder.Replacer, opts *typed.Options) genn
 		content = replacer.ReplaceOnce(content, typed.PlaceholderGenesisTypesImport, templateTypesImport)
 
 		templateTypesDefault := `%[2]vList: []%[2]v{},
+%[2]vCountList: []%[2]vCount{},
 %[1]v`
 		replacementTypesDefault := fmt.Sprintf(
 			templateTypesDefault,
@@ -86,17 +88,40 @@ func genesisTypesModify(replacer placeholder.Replacer, opts *typed.Options) genn
 		)
 		content = replacer.Replace(content, typed.PlaceholderGenesisTypesDefault, replacementTypesDefault)
 
-		templateTypesValidate := `// Check for duplicated ID in %[2]v
-%[2]vIdMap := make(map[uint64]bool)
-%[2]vCount := gs.Get%[3]vCount()
+		// Array of the indexes of the element
+		var indexArgs []string
+		for _, index := range opts.Indexes {
+			indexArgs = append(indexArgs, "elem."+index.Name.UpperCamel)
+		}
+		indexCall := strings.Join(indexArgs, ",\n")
+
+		templateTypesValidate := `
+// Checkout %[2]vCounts to perform verification
+%[2]vCountMap := make(map[string]uint64)
+for _, elem := range gs.%[2]vCountList {
+	countKey := string(All%[3]vKeyPath(
+		%[4]v))
+	if _, ok := %[2]vCountMap[countKey]; ok {
+		return errors.New("duplicated %[2]v count")
+	}
+	%[2]vCountMap[countKey] = elem.Count
+}
+
+// Check for duplicated ID in %[2]v
+%[2]vIdMap := make(map[string]struct{})
 for _, elem := range gs.%[3]vList {
-	if _, ok := %[2]vIdMap[elem.Id]; ok {
-		return fmt.Errorf("duplicated id for %[2]v")
+	elemKey := string(%[3]vKeyPath(
+		%[4]v elem.Id,
+	))
+	if _, ok := %[2]vIdMap[elemKey]; ok {
+		return errors.New("duplicated id for %[2]v")
 	}
-	if elem.Id >= %[2]vCount {
-		return fmt.Errorf("%[2]v id should be lower or equal than the last id")
+	countKey := string(All%[3]vKeyPath(
+		%[4]v))
+	if elem.Id >= %[2]vCountMap[countKey] {
+		return errors.New("%[2]v id should be lower or equal than the biggest reported id")
 	}
-	%[2]vIdMap[elem.Id] = true
+	%[2]vIdMap[elemKey] = struct{}{}
 }
 %[1]v`
 		replacementTypesValidate := fmt.Sprintf(
@@ -104,6 +129,7 @@ for _, elem := range gs.%[3]vList {
 			typed.PlaceholderGenesisTypesValidate,
 			opts.TypeName.LowerCamel,
 			opts.TypeName.UpperCamel,
+			indexCall,
 		)
 		content = replacer.Replace(content, typed.PlaceholderGenesisTypesValidate, replacementTypesValidate)
 
@@ -120,29 +146,45 @@ func genesisModuleModify(replacer placeholder.Replacer, opts *typed.Options) gen
 			return err
 		}
 
+		// Array of the indexes of the element
+		var indexArgs []string
+		for _, index := range opts.Indexes {
+			indexArgs = append(indexArgs, "elem."+index.Name.UpperCamel)
+		}
+		indexCall := strings.Join(indexArgs, ",\n")
+
 		templateModuleInit := `// Set all the %[2]v
 for _, elem := range genState.%[3]vList {
 	k.Set%[3]v(ctx, elem)
 }
 
 // Set %[2]v count
-k.Set%[3]vCount(ctx, genState.%[3]vCount)
+for _, elem := range genState.%[3]vCountList {
+	k.Set%[3]vCount(
+		ctx,
+		%[4]v  elem.Count,
+	)
+}
 %[1]v`
 		replacementModuleInit := fmt.Sprintf(
 			templateModuleInit,
 			typed.PlaceholderGenesisModuleInit,
 			opts.TypeName.LowerCamel,
 			opts.TypeName.UpperCamel,
+			indexCall,
 		)
 		content := replacer.Replace(f.String(), typed.PlaceholderGenesisModuleInit, replacementModuleInit)
 
 		templateModuleExport := `genesis.%[2]vList = k.GetAll%[2]v(ctx)
-genesis.%[2]vCount = k.Get%[2]vCount(ctx)
+
+// Get %[3]v counts
+
 %[1]v`
 		replacementModuleExport := fmt.Sprintf(
 			templateModuleExport,
 			typed.PlaceholderGenesisModuleExport,
 			opts.TypeName.UpperCamel,
+			opts.TypeName.LowerCamel,
 		)
 		content = replacer.Replace(content, typed.PlaceholderGenesisModuleExport, replacementModuleExport)
 
@@ -151,107 +193,3 @@ genesis.%[2]vCount = k.Get%[2]vCount(ctx)
 	}
 }
 
-func genesisTestsModify(replacer placeholder.Replacer, opts *typed.Options) genny.RunFn {
-	return func(r *genny.Runner) error {
-		path := filepath.Join(opts.AppPath, "x", opts.ModuleName, "genesis_test.go")
-		f, err := r.Disk.Find(path)
-		if err != nil {
-			return err
-		}
-
-		templateState := `%[2]vList: []types.%[2]v{
-	{
-		Id: 0,
-	},
-	{
-		Id: 1,
-	},
-},
-%[2]vCount: 2,
-%[1]v`
-		replacementValid := fmt.Sprintf(
-			templateState,
-			module.PlaceholderGenesisTestState,
-			opts.TypeName.UpperCamel,
-		)
-		content := replacer.Replace(f.String(), module.PlaceholderGenesisTestState, replacementValid)
-
-		templateAssert := `require.ElementsMatch(t, genesisState.%[2]vList, got.%[2]vList)
-require.Equal(t, genesisState.%[2]vCount, got.%[2]vCount)
-%[1]v`
-		replacementTests := fmt.Sprintf(
-			templateAssert,
-			module.PlaceholderGenesisTestAssert,
-			opts.TypeName.UpperCamel,
-		)
-		content = replacer.Replace(content, module.PlaceholderGenesisTestAssert, replacementTests)
-
-		newFile := genny.NewFileS(path, content)
-		return r.File(newFile)
-	}
-}
-
-func genesisTypesTestsModify(replacer placeholder.Replacer, opts *typed.Options) genny.RunFn {
-	return func(r *genny.Runner) error {
-		path := filepath.Join(opts.AppPath, "x", opts.ModuleName, "types/genesis_test.go")
-		f, err := r.Disk.Find(path)
-		if err != nil {
-			return err
-		}
-
-		templateValid := `%[2]vList: []types.%[2]v{
-	{
-		Id: 0,
-	},
-	{
-		Id: 1,
-	},
-},
-%[2]vCount: 2,
-%[1]v`
-		replacementValid := fmt.Sprintf(
-			templateValid,
-			module.PlaceholderTypesGenesisValidField,
-			opts.TypeName.UpperCamel,
-		)
-		content := replacer.Replace(f.String(), module.PlaceholderTypesGenesisValidField, replacementValid)
-
-		templateTests := `{
-	desc:     "duplicated %[2]v",
-	genState: &types.GenesisState{
-		%[3]vList: []types.%[3]v{
-			{
-				Id: 0,
-			},
-			{
-				Id: 0,
-			},
-		},
-	},
-	valid:    false,
-},
-{
-	desc:     "invalid %[2]v count",
-	genState: &types.GenesisState{
-		%[3]vList: []types.%[3]v{
-			{
-				Id: 1,
-			},
-		},
-		%[3]vCount: 0,
-	},
-	valid:    false,
-},
-%[1]v`
-		replacementTests := fmt.Sprintf(
-			templateTests,
-			module.PlaceholderTypesGenesisTestcase,
-			opts.TypeName.LowerCamel,
-			opts.TypeName.UpperCamel,
-		)
-		content = replacer.Replace(content, module.PlaceholderTypesGenesisTestcase, replacementTests)
-
-		newFile := genny.NewFileS(path, content)
-		return r.File(newFile)
-	}
-}
