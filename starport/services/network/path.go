a4b1b863ca009d55f1577346c96c58c026d7ceba
ltacker <lucas.bertrand.22@gmail.com>
2021-11-03 16:50:40 +0100

    Add path

---
+++ starport/services/network/path.go
@@ -0,0 +1,17 @@
+package network
+
+import (
+	"strconv"
+
+	"github.com/tendermint/starport/starport/pkg/xfilepath"
+)
+
+var (
+	// SpnPath returns the path used to store chain home from SPN
+	SpnPath = xfilepath.JoinFromHome(xfilepath.Path("spn"))
+)
+
+// ChainHome returns the default home dir used for a chain from SPN
+func ChainHome(launchID uint64) (string, error) {
+	return xfilepath.Join(SpnPath, xfilepath.Path(strconv.FormatUint(launchID, 10)))()
+}
