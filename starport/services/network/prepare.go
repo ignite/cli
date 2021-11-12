1051216bd0170f7eac741e803ecc3c4b8633a777
ltacker <lucas.bertrand.22@gmail.com>
2021-11-12 10:59:44 +0100

    Apply genesis accounts

--- starport/services/network/prepare.go
+++ starport/services/network/prepare.go
@@ -3,6 +3,7 @@ package network
 import (
 	"context"
 	launchtypes "github.com/tendermint/spn/x/launch/types"
+	"github.com/tendermint/starport/starport/pkg/cosmosaddress"
 )
 
 // Prepare queries launch information and prepare the chain to be launched from these information
@@ -24,10 +25,29 @@ func (b Blockchain) Prepare(ctx context.Context) error {
 
 // applyGenesisAccounts adds the genesis account into the genesis using the chain CLI
 func (b Blockchain) applyGenesisAccounts(ctx context.Context, genesisAccs []launchtypes.GenesisAccount) error {
+	var err error
 	// TODO: detect the correct prefix
 	prefix := "cosmos"
 
+	cmd, err := b.chain.Commands(ctx)
+	if err != nil {
+		return err
+	}
 
+	for i := range genesisAccs {
+		// change the address prefix to the target chain prefix
+		genesisAccs[i].Address, err = cosmosaddress.ChangePrefix(genesisAccs[i].Address, prefix)
+		if err != nil {
+			return err
+		}
+		acc := genesisAccs[i]
+
+		// call add genesis account cli command
+		err = cmd.AddGenesisAccount(ctx, acc.Address, acc.Coins.String())
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
