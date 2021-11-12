271985da63b527953f58df62b6df3d6f3259d6d5
ltacker <lucas.bertrand.22@gmail.com>
2021-11-12 10:43:22 +0100

    change and get prefixes

---
+++ starport/pkg/cosmosaddress/cosmosaddress.go
@@ -0,0 +1,26 @@
+// Package cosmosaddress implements helper methods to interact with Cosmos-SDK address
+package cosmosaddress
+
+import (
+	"errors"
+
+	"github.com/cosmos/cosmos-sdk/types/bech32"
+)
+
+// ChangePrefix returns the address with another prefix
+func ChangePrefix(address, newPrefix string) (string, error) {
+	if newPrefix == "" {
+		return "", errors.New("empty prefix")
+	}
+	_, pubKey, err := bech32.DecodeAndConvert(address)
+	if err != nil {
+		return "", err
+	}
+	return bech32.ConvertAndEncode(newPrefix, pubKey)
+}
+
+// GetPrefix returns the bech 32 prefix used by the address
+func GetPrefix(address string) (string, error) {
+	prefix, _, err := bech32.DecodeAndConvert(address)
+	return prefix, err
+}
