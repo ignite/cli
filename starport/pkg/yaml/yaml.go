ced4b6f92ccf11a6cf6646128d0c7a8ee4381822
Danilo Pantani <danpantani@gmail.com>
2021-12-02 16:13:23 -0300

    add coment to ParseString method

--- starport/pkg/yaml/yaml.go
+++ starport/pkg/yaml/yaml.go
@@ -9,6 +9,8 @@ import (
 	"github.com/goccy/go-yaml/parser"
 )
 
+// ParseString converts an object to a string in a YAML format and transforms
+// the byte slice fields from the path to string to be more readable.
 func ParseString(ctx context.Context, obj interface{}, paths ...string) (string, error) {
 	requestYaml, err := yaml.MarshalContext(ctx, obj)
 	if err != nil {
