ca538c2f4ec960cc9b15b8488eab901c19ecf38a
Hwangjae Lee <meetrick@gmail.com>
2021-12-03 10:50:36 +0900

    feat(plugin): Fill up the downloader function
    Signed-off-by: Hwangjae Lee <meetrick@gmail.com>

--- starport/services/plugin/plugin_builder.go
+++ starport/services/plugin/plugin_builder.go
@@ -1,6 +1,10 @@
 package plugin
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/go-git/go-git/v5"
 	"github.com/tendermint/starport/starport/chainconfig"
 )
 
@@ -20,6 +24,7 @@ type Builder interface {
 }
 
 type builder struct {
+	pluginSpec *starportplugin
 }
 
 func (b *builder) Build(config chainconfig.Plugin) error {
@@ -29,6 +34,7 @@ func (b *builder) Build(config chainconfig.Plugin) error {
 		return err
 	}
 
+	// who can fill-up the config.Name?
 	err = b.build(config.Name)
 	return err
 }
@@ -36,6 +42,39 @@ func (b *builder) Build(config chainconfig.Plugin) error {
 func (b *builder) download(url string) error {
 	_ = pluginDir
 
+	_, err := git.PlainClone(pluginDir, false, &git.CloneOptions{
+		URL: url,
+		Progress: os.Stdout,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	repository, err := git.PlainOpen(pluginDir)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	worktree, err := repository.Worktree()
+		if err != nil {
+			panic(err)
+	}
+
+	err = worktree.Pull(&git.PullOptions{RemoteName: "origin"})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	reference, err := repository.Head()
+	if err != nil {
+		panic(err)
+	}
+	commit, err := repository.CommitObject(reference.Hash())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(commit)
 	// TODO:
 	// Create `pluginDir` if not exist.
 	// Clone repo from url on `pluginDir`.
@@ -45,6 +84,7 @@ func (b *builder) download(url string) error {
 func (b *builder) build(name string) error {
 	// TODO:
 	// Build plugin.
+	// go build -buildmode=plugin
 	return nil
 }
 
--- starport/services/plugin/plugin_builder_test.go
+++ starport/services/plugin/plugin_builder_test.go
@@ -1,1 +1,97 @@
 package plugin
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/tendermint/starport/starport/chainconfig"
+)
+
+//
+// Builder handles download build process for new plugin.
+//
+
+// TODO: How to divide GIT module?
+
+const (
+	pluginDir = "plugin"
+)
+
+// Builder provides interfaces to build plugins.
+type Builder interface {
+	download(url string)
+	build(name string)
+}
+
+type builder struct {
+}
+
+func (b *builder) Test_Build(t *testing.T) error {
+	// TODO:
+	err := b.download(config.RepositoryURL)
+	if err != nil {
+		return err
+	}
+
+	// who can fill-up the config.Name?
+	err = b.build(config.Name)
+	return err
+}
+
+func (b *builder) download(url string) error {
+	_ = pluginDir
+
+	_, err := git.PlainClone(pluginDir, false, &git.CloneOptions{
+		URL: url,
+		Progress: os.Stdout,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	repository, err := git.PlainOpen(pluginDir)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	worktree, err := repository.Worktree()
+		if err != nil {
+			panic(err)
+	}
+
+	err = worktree.Pull(&git.PullOptions{RemoteName: "origin"})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	reference, err := repository.Head()
+	if err != nil {
+		panic(err)
+	}
+	commit, err := repository.CommitObject(reference.Hash())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(commit)
+	// TODO:
+	// Create `pluginDir` if not exist.
+	// Clone repo from url on `pluginDir`.
+	return nil
+}
+
+func (b *builder) build(name string) error {
+	// TODO:
+	// Build plugin.
+	// go build -buildmode=plugin
+	return nil
+}
+
+// NewBuilder creates new plugin builder.
+// TODO: Need parameters?
+func NewBuilder() (Builder, error) {
+	// TODO:
+	return &builder{}, nil
+}
