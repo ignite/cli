88f76e1fb0ebc39760f2ad07f99cbe941abf9d74
Joonkyo Kim <rootwarp@gmail.com>
2021-12-03 15:41:20 +0000

    feat(plugin): build plugin.

--- starport/services/plugin/plugin_builder.go
+++ starport/services/plugin/plugin_builder.go
@@ -36,30 +36,31 @@ type builder struct {
 }
 
 func (b *builder) Build(config chainconfig.Plugin) error {
-	err := b.download(config.Name, config.RepositoryURL)
+	path, err := b.download(config.Name, config.RepositoryURL)
 	if err != nil {
+		log.Println("Download ", err)
 		return err
 	}
 
-	err = b.build(config.Name)
+	err = b.build(path, config.Name)
 	return err
 }
 
-func (b *builder) download(name, url string) error {
+func (b *builder) download(pluginName, url string) (string, error) {
 	log.Printf("Clone %s\n", url)
 
 	starportHome, err := chainconfig.ConfigDirPath()
 	if err != nil {
 		log.Println(err)
-		return err
+		return "", err
 	}
 
 	pathTokens := strings.Split(url, "/")
 	repoName := pathTokens[len(pathTokens)-1]
 
-	pluginHome := fmt.Sprintf("%s/%s/%s", starportHome, pluginDir, repoName)
+	repoPath := fmt.Sprintf("%s/%s/%s", starportHome, pluginDir, repoName)
 
-	_, err = git.PlainClone(pluginHome, false, &git.CloneOptions{
+	_, err = git.PlainClone(repoPath, false, &git.CloneOptions{
 		URL:      url,
 		Progress: os.Stdout,
 		Depth:    1,
@@ -67,60 +68,60 @@ func (b *builder) download(name, url string) error {
 
 	if err != nil {
 		log.Println("clone", err)
-		return err
+		return "", err
 	}
 
-	repository, err := git.PlainOpen(pluginDir)
-	if err != nil {
-		log.Println("open", err)
-		return err
-	}
+	/*
+		repository, err := git.PlainOpen(pluginHome)
+		if err != nil {
+			log.Println("open", err)
+			return err
+		}
 
-	worktree, err := repository.Worktree()
-	if err != nil {
-		log.Println("open", err)
-		return err
-	}
+		worktree, err := repository.Worktree()
+		if err != nil {
+			log.Println("open", err)
+			return err
+		}
 
-	err = worktree.Pull(&git.PullOptions{RemoteName: "origin"})
-	if err != nil {
-		log.Println("pull", err)
-		return err
-	}
+		err = worktree.Pull(&git.PullOptions{RemoteName: "origin"})
+		if err != nil {
+			log.Println("pull", err)
+			return err
+		}
 
-	reference, err := repository.Head()
-	if err != nil {
-		log.Println(err)
-		return err
-	}
+		reference, err := repository.Head()
+		if err != nil {
+			log.Println("Head ", err)
+			return err
+		}
 
-	_, err = repository.CommitObject(reference.Hash())
-	if err != nil {
-		log.Println(err)
-		return err
-	}
+		_, err = repository.CommitObject(reference.Hash())
+		if err != nil {
+			log.Println("hash ", err)
+			return err
+		}
+	*/
 
-	_ = name
 	_ = b.pluginSpec
 
-	return nil
+	return fmt.Sprintf("%s/%s", repoPath, pluginName), nil
 }
 
-func (b *builder) build(name string) error {
-	log.Printf("Build plugin %s...\n", name)
+func (b *builder) build(path, name string) error {
+	log.Printf("Build plugin %s\n", name)
 
 	errb := &bytes.Buffer{}
 
 	err := cmdrunner.
-		New(cmdrunner.DefaultStderr(errb), cmdrunner.DefaultWorkdir(pluginDir)).
+		New(cmdrunner.DefaultStderr(errb), cmdrunner.DefaultWorkdir(path)).
 		Run(context.Background(), step.New(
 			step.Exec(
 				"go",
 				"build",
 				"-buildmode=plugin",
 				"-o",
-				// plugin output name ex) xxx.so
-				"<name>.so",
+				fmt.Sprintf("%s.so", name),
 			),
 		),
 		)
