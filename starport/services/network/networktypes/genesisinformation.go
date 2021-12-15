b0909445cb97d8b79d10130fd9a8eeb8a00d954c
ltacker <lucas.bertrand.22@gmail.com>
2021-12-15 10:02:00 +0100

    Implement apply request

--- starport/services/network/networktypes/genesisinformation.go
+++ starport/services/network/networktypes/genesisinformation.go
@@ -1,16 +1,19 @@
 package networktypes
 
 import (
-	"errors"
+	"github.com/pkg/errors"
 
 	launchtypes "github.com/tendermint/spn/x/launch/types"
 )
 
+// errInvalidRequests is an error returned in methods manipulating requests when they are invalid
+var errInvalidRequests = errors.New("requests are invalid")
+
 // GenesisInformation represents all information for a chain to construct the genesis.
 type GenesisInformation struct {
-	GenesisAccounts   []GenesisAccount
-	VestingAccounts   []VestingAccount
-	GenesisValidators []GenesisValidator
+	GenesisAccounts   map[string]GenesisAccount
+	VestingAccounts   map[string]VestingAccount
+	GenesisValidators map[string]GenesisValidator
 }
 
 // GenesisAccount represents an account with initial coin allocation for the chain for the chain genesis
@@ -30,21 +33,9 @@ type VestingAccount struct {
 
 // GenesisValidator represents a genesis validator associated with a gentx in the chain genesis
 type GenesisValidator struct {
+	Address string
 	Gentx []byte
 	Peer  string
-}
-
-// NewGenesisInformation initializes a new GenesisInformation
-func NewGenesisInformation(
-	genAccs []GenesisAccount,
-	vestingAccs []VestingAccount,
-	genVals []GenesisValidator,
-) GenesisInformation {
-	return GenesisInformation{
-		GenesisAccounts:   genAccs,
-		VestingAccounts:   vestingAccs,
-		GenesisValidators: genVals,
-	}
 }
 
 // ToGenesisAccount converts genesis account from SPN
@@ -73,16 +64,84 @@ func ToVestingAccount(acc launchtypes.VestingAccount) (VestingAccount, error) {
 // ToGenesisValidator converts genesis validator from SPN
 func ToGenesisValidator(val launchtypes.GenesisValidator) GenesisValidator {
 	return GenesisValidator{
+		Address: val.Address,
 		Gentx: val.GenTx,
 		Peer:  val.Peer,
 	}
 }
 
+// NewGenesisInformation initializes a new GenesisInformation
+func NewGenesisInformation(
+	genAccs []GenesisAccount,
+	vestingAccs []VestingAccount,
+	genVals []GenesisValidator,
+) (gi GenesisInformation) {
+	// convert accounts array into map
+	for _, genAcc := range genAccs {
+		gi.GenesisAccounts[genAcc.Address] = genAcc
+	}
+	for _, vestingAcc := range vestingAccs {
+		gi.VestingAccounts[vestingAcc.Address] = vestingAcc
+	}
+	for _, genVal := range genVals {
+		gi.GenesisValidators[genVal.Address] = genVal
+	}
+	return gi
+}
+
 // ApplyRequest applies to the genesisInformation the changes implied by the approval of a request
-func ApplyRequest(genesisInformation GenesisInformation, request launchtypes.Request) (GenesisInformation, error) {
-	//switch requestContent := request.Content.Content.(type) {
-	//case *launchtypes.RequestContent_GenesisAccount:
-	//	ga := requestContent.GenesisAccount
-	//
-	//}
+func ApplyRequest(gi GenesisInformation, request launchtypes.Request) (GenesisInformation, error) {
+	switch requestContent := request.Content.Content.(type) {
+	case *launchtypes.RequestContent_GenesisAccount:
+		// new genesis account in the genesis
+		ga := ToGenesisAccount(*requestContent.GenesisAccount)
+		if _, ok := gi.GenesisAccounts[ga.Address]; ok {
+			return gi, errors.Wrapf(errInvalidRequests, "genesis account %s already in genesis", ga.Address)
+		}
+		gi.GenesisAccounts[ga.Address] = ga
+
+	case *launchtypes.RequestContent_VestingAccount:
+		// new vesting account in the genesis
+		va, err := ToVestingAccount(*requestContent.VestingAccount)
+		if err != nil {
+			// we don't treat this error as errInvalidRequests
+			// because it can occur if we don't support this format of vesting account
+			// but the request is still correct
+			return gi, err
+		}
+
+		if _, ok := gi.VestingAccounts[va.Address]; ok {
+			return gi, errors.Wrapf(errInvalidRequests, "vesting account %s already in genesis", va.Address)
+		}
+		gi.VestingAccounts[va.Address] = va
+
+	case *launchtypes.RequestContent_AccountRemoval:
+		// account removed from the genesis
+		ar := requestContent.AccountRemoval
+		_, genExist := gi.GenesisAccounts[ar.Address]
+		_, vestingExist := gi.VestingAccounts[ar.Address]
+		if !genExist && !vestingExist {
+			return gi, errors.Wrapf(errInvalidRequests, "account %s can't be removed because it doesn't exist", ar.Address)
+		}
+		delete(gi.GenesisAccounts, ar.Address)
+		delete(gi.VestingAccounts, ar.Address)
+
+	case *launchtypes.RequestContent_GenesisValidator:
+		// new genesis validator in the genesis
+		gv := ToGenesisValidator(*requestContent.GenesisValidator)
+		if _, ok := gi.GenesisValidators[gv.Address]; ok {
+			return gi, errors.Wrapf(errInvalidRequests, "genesis validator %s already in genesis", gv.Address)
+		}
+		gi.GenesisValidators[gv.Address] = gv
+
+
+	case *launchtypes.RequestContent_ValidatorRemoval:
+		// validator removed from the genesis
+		vr := requestContent.ValidatorRemoval
+		if _, ok := gi.GenesisValidators[vr.ValAddress]; !ok {
+			return gi, errors.Wrapf(errInvalidRequests, "genesis validator %s can't be removed because it doesn't exist", vr.ValAddress)
+		}
+	}
+
+	return gi, nil
 }
