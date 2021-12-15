b57456801cb5c8c83a67cf08dec2c1419d3e8e99
Danilo Pantani <danpantani@gmail.com>
2021-12-14 23:51:25 -0300

    separede the commands as a cobra command

--- starport/cmd/network_chain_show.go
+++ starport/cmd/network_chain_show.go
@@ -2,7 +2,6 @@ package starportcmd
 
 import (
 	"bytes"
-	"context"
 	"errors"
 	"fmt"
 	"os"
@@ -18,221 +17,284 @@ import (
 	"github.com/tendermint/starport/starport/services/network/networktypes"
 )
 
-type ShowType string
-
-const (
-	chainShowInfo       ShowType = "info"
-	chainShowGenesis    ShowType = "genesis"
-	chainShowAccounts   ShowType = "accounts"
-	chainShowValidators ShowType = "validators"
-	chainShowPeers      ShowType = "peers"
-)
-
 var (
-	showTypes = map[ShowType]struct{}{
-		chainShowInfo:       {},
-		chainShowGenesis:    {},
-		chainShowAccounts:   {},
-		chainShowValidators: {},
-		chainShowPeers:      {},
-	}
-
 	chainGenesisValSummaryHeader = []string{"Genesis Validator", "Self Delegation", "Peer"}
 	chainGenesisAccSummaryHeader = []string{"Genesis Account", "Coins"}
 	chainVestingAccSummaryHeader = []string{"Vesting Account", "Total Balance", "Vesting", "EndTime"}
 )
 
-// NewNetworkChainShow creates a new chain show command to show
-// a chain on SPN.
+// NewNetworkChainShow creates a new chain show
+// command to show a chain details on SPN.
 func NewNetworkChainShow() *cobra.Command {
 	c := &cobra.Command{
-		Use:   "show [info|genesis|accounts|validators|peers] [launch-id]",
+		Use:   "show",
 		Short: "Show details of a chain",
-		RunE:  networkChainShowHandler,
-		Args:  cobra.ExactArgs(2),
 	}
-
-	c.Flags().AddFlagSet(flagSetKeyringBackend())
-	c.Flags().AddFlagSet(flagNetworkFrom())
-	c.Flags().AddFlagSet(flagSetHome())
-
+	c.AddCommand(
+		newNetworkChainShowInfo(),
+		newNetworkChainShowGenesis(),
+		newNetworkChainShowAccounts(),
+		newNetworkChainShowValidators(),
+		newNetworkChainShowPeers(),
+	)
+	c.PersistentFlags().AddFlagSet(flagNetworkFrom())
+	c.PersistentFlags().AddFlagSet(flagSetKeyringBackend())
 	return c
 }
 
-func networkChainShowHandler(cmd *cobra.Command, args []string) error {
-	showType := ShowType(args[0])
-	if _, ok := showTypes[showType]; !ok {
-		cmd.Usage()
-		return fmt.Errorf("invalid arg %s", showType)
-	}
-
+func networkChainLaunch(cmd *cobra.Command, args []string) (NetworkBuilder, uint64, error) {
 	nb, err := newNetworkBuilder(cmd)
 	if err != nil {
-		return err
+		return nb, 0, err
 	}
-	defer nb.Cleanup()
-
 	// parse launch ID.
-	launchID, err := network.ParseLaunchID(args[1])
+	launchID, err := network.ParseLaunchID(args[0])
 	if err != nil {
-		return err
+		return nb, launchID, err
 	}
-
-	n, err := nb.Network()
-	if err != nil {
-		return err
-	}
-
-	chainLaunch, err := n.ChainLaunch(cmd.Context(), launchID)
-	if err != nil {
-		return err
-	}
-
-	content := ""
-	switch showType {
-	case chainShowGenesis:
-		content, err = formatChainGenesis(nb, chainLaunch)
-	case chainShowInfo:
-		content, err = formatChainInfo(cmd.Context(), chainLaunch)
-	case chainShowAccounts:
-		content, err = formatChainAccounts(cmd.Context(), n, launchID)
-	case chainShowValidators:
-		content, err = formatChainValidators(cmd.Context(), n, launchID)
-	case chainShowPeers:
-		content, err = formatChainPeers(cmd.Context(), n, launchID)
-	}
-	if err != nil {
-		return err
-	}
-
-	nb.Spinner.Stop()
-	fmt.Print(content)
-	return nil
+	return nb, launchID, err
 }
 
-func formatChainGenesis(nb NetworkBuilder, chainLaunch networktypes.ChainLaunch) (string, error) {
-	c, err := nb.Chain(networkchain.SourceLaunch(chainLaunch))
-	if err != nil {
-		return "", err
-	}
+func newNetworkChainShowInfo() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "info [launch-id]",
+		Short: "Show info details of the chain",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			nb, launchID, err := networkChainLaunch(cmd, args)
+			if err != nil {
+				return err
+			}
+			defer nb.Cleanup()
+			n, err := nb.Network()
+			if err != nil {
+				return err
+			}
 
-	genesisPath, err := c.GenesisPath()
-	if err != nil {
-		return "", err
+			chainLaunch, err := n.ChainLaunch(cmd.Context(), launchID)
+			if err != nil {
+				return err
+			}
+
+			var genesis []byte
+			if chainLaunch.GenesisURL != "" {
+				genesis, _, err = cosmosutil.GenesisAndHashFromURL(cmd.Context(), chainLaunch.GenesisURL)
+				if err != nil {
+					return err
+				}
+			}
+			chainInfo := struct {
+				Chain   networktypes.ChainLaunch `json:"Chain"`
+				Genesis []byte                   `json:"Genesis"`
+			}{
+				Chain:   chainLaunch,
+				Genesis: genesis,
+			}
+			info, err := yaml.Marshal(cmd.Context(), chainInfo, "$.Genesis")
+			if err != nil {
+				return err
+			}
+			nb.Spinner.Stop()
+			fmt.Print(info)
+			return nil
+		},
 	}
-	// TODO run the prepare command in dry run mode and print the genesis if not exist
-	if _, err = os.Stat(genesisPath); os.IsNotExist(err) {
-		return "", errors.New("chain genesis not found, run the 'network chain prepare' command first")
-	}
-	genesisFile, err := os.ReadFile(genesisPath)
-	if err != nil {
-		return "", err
-	}
-	return string(genesisFile), nil
+	return c
 }
 
-func formatChainInfo(ctx context.Context, chainLaunch networktypes.ChainLaunch) (info string, err error) {
-	var genesis []byte
-	if chainLaunch.GenesisURL != "" {
-		genesis, _, err = cosmosutil.GenesisAndHashFromURL(ctx, chainLaunch.GenesisURL)
-		if err != nil {
-			return "", err
-		}
+func newNetworkChainShowGenesis() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "genesis [launch-id]",
+		Short: "Show the chain genesis file",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			nb, launchID, err := networkChainLaunch(cmd, args)
+			if err != nil {
+				return err
+			}
+			defer nb.Cleanup()
+			n, err := nb.Network()
+			if err != nil {
+				return err
+			}
+
+			chainLaunch, err := n.ChainLaunch(cmd.Context(), launchID)
+			if err != nil {
+				return err
+			}
+			c, err := nb.Chain(networkchain.SourceLaunch(chainLaunch))
+			if err != nil {
+				return err
+			}
+			genesisPath, err := c.GenesisPath()
+			if err != nil {
+				return err
+			}
+			// TODO run the prepare command in dry run mode and print the genesis if not exist
+			if _, err = os.Stat(genesisPath); os.IsNotExist(err) {
+				return errors.New("chain genesis not found, run the 'network chain prepare' command first")
+			}
+			genesisFile, err := os.ReadFile(genesisPath)
+			if err != nil {
+				return err
+			}
+			nb.Spinner.Stop()
+			fmt.Print(string(genesisFile))
+			return nil
+		},
 	}
-	chainInfo := struct {
-		Chain   networktypes.ChainLaunch `json:"Chain"`
-		Genesis []byte                   `json:"Genesis"`
-	}{
-		Chain:   chainLaunch,
-		Genesis: genesis,
-	}
-	return yaml.Marshal(ctx, chainInfo, "$.Genesis")
+	return c
 }
 
-func formatChainAccounts(ctx context.Context, n network.Network, launchID uint64) (string, error) {
-	accountSummary := bytes.NewBufferString("")
-	genesisAccs, err := n.GenesisAccounts(ctx, launchID)
-	if err != nil {
-		return "", err
-	}
-	genesisAccEntries := make([][]string, 0)
-	for _, acc := range genesisAccs {
-		genesisAccEntries = append(genesisAccEntries, []string{
-			acc.Address,
-			acc.Coins,
-		})
-	}
-	if len(genesisAccEntries) > 0 {
-		if err = entrywriter.MustWrite(
-			accountSummary,
-			chainGenesisAccSummaryHeader,
-			genesisAccEntries...,
-		); err != nil {
-			return "", err
-		}
-	}
+func newNetworkChainShowAccounts() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "accounts [launch-id]",
+		Short: "Show all vesting and genesis accounts of the chain",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			nb, launchID, err := networkChainLaunch(cmd, args)
+			if err != nil {
+				return err
+			}
+			defer nb.Cleanup()
+			n, err := nb.Network()
+			if err != nil {
+				return err
+			}
 
-	vestingAccs, err := n.VestingAccounts(ctx, launchID)
-	if err != nil {
-		return "", err
+			accountSummary := bytes.NewBufferString("")
+
+			// get all chain genesis accounts
+			genesisAccs, err := n.GenesisAccounts(cmd.Context(), launchID)
+			if err != nil {
+				return err
+			}
+			genesisAccEntries := make([][]string, 0)
+			for _, acc := range genesisAccs {
+				genesisAccEntries = append(genesisAccEntries, []string{
+					acc.Address,
+					acc.Coins,
+				})
+			}
+			if len(genesisAccEntries) > 0 {
+				if err = entrywriter.MustWrite(
+					accountSummary,
+					chainGenesisAccSummaryHeader,
+					genesisAccEntries...,
+				); err != nil {
+					return err
+				}
+			}
+
+			// get all chain vesting accounts
+			vestingAccs, err := n.VestingAccounts(cmd.Context(), launchID)
+			if err != nil {
+				return err
+			}
+			genesisVestingAccEntries := make([][]string, 0)
+			for _, acc := range vestingAccs {
+				genesisVestingAccEntries = append(genesisVestingAccEntries, []string{
+					acc.Address,
+					acc.TotalBalance,
+					acc.Vesting,
+					strconv.FormatInt(acc.EndTime, 10),
+				})
+			}
+			if len(genesisVestingAccEntries) > 0 {
+				if err = entrywriter.MustWrite(
+					accountSummary,
+					chainVestingAccSummaryHeader,
+					genesisVestingAccEntries...,
+				); err != nil {
+					return err
+				}
+			}
+			nb.Spinner.Stop()
+			fmt.Print(accountSummary.String())
+			return nil
+		},
 	}
-	genesisVestingAccEntries := make([][]string, 0)
-	for _, acc := range vestingAccs {
-		genesisVestingAccEntries = append(genesisVestingAccEntries, []string{
-			acc.Address,
-			acc.TotalBalance,
-			acc.Vesting,
-			strconv.FormatInt(acc.EndTime, 10),
-		})
-	}
-	if len(genesisVestingAccEntries) > 0 {
-		if err = entrywriter.MustWrite(
-			accountSummary,
-			chainVestingAccSummaryHeader,
-			genesisVestingAccEntries...,
-		); err != nil {
-			return "", err
-		}
-	}
-	return accountSummary.String(), err
+	return c
 }
 
-func formatChainValidators(ctx context.Context, n network.Network, launchID uint64) (string, error) {
-	validatorSummary := bytes.NewBufferString("")
-	validators, err := n.GenesisValidators(ctx, launchID)
-	if err != nil {
-		return "", err
+func newNetworkChainShowValidators() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "validators [launch-id]",
+		Short: "Show all validators of the chain",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			nb, launchID, err := networkChainLaunch(cmd, args)
+			if err != nil {
+				return err
+			}
+			defer nb.Cleanup()
+			n, err := nb.Network()
+			if err != nil {
+				return err
+			}
+
+			validatorSummary := bytes.NewBufferString("")
+			validators, err := n.GenesisValidators(cmd.Context(), launchID)
+			if err != nil {
+				return err
+			}
+			validatorEntries := make([][]string, 0)
+			for _, acc := range validators {
+				validatorEntries = append(validatorEntries, []string{
+					acc.Address,
+					acc.SelfDelegation.String(),
+					acc.Peer,
+				})
+			}
+			if len(validatorEntries) > 0 {
+				if err = entrywriter.MustWrite(
+					validatorSummary,
+					chainGenesisValSummaryHeader,
+					validatorEntries...,
+				); err != nil {
+					return err
+				}
+			}
+			nb.Spinner.Stop()
+			fmt.Print(validatorSummary.String())
+			return nil
+		},
 	}
-	validatorEntries := make([][]string, 0)
-	for _, acc := range validators {
-		validatorEntries = append(validatorEntries, []string{
-			acc.Address,
-			acc.SelfDelegation.String(),
-			acc.Peer,
-		})
-	}
-	if len(validatorEntries) > 0 {
-		if err = entrywriter.MustWrite(
-			validatorSummary,
-			chainGenesisValSummaryHeader,
-			validatorEntries...,
-		); err != nil {
-			return "", err
-		}
-	}
-	return validatorSummary.String(), err
+	return c
 }
 
-func formatChainPeers(ctx context.Context, n network.Network, launchID uint64) (string, error) {
-	genVals, err := n.GenesisValidators(ctx, launchID)
-	if err != nil {
-		return "", err
-	}
+func newNetworkChainShowPeers() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "peers [launch-id]",
+		Short: "Show peers list of the chain",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			nb, launchID, err := networkChainLaunch(cmd, args)
+			if err != nil {
+				return err
+			}
+			defer nb.Cleanup()
+			n, err := nb.Network()
+			if err != nil {
+				return err
+			}
+			genVals, err := n.GenesisValidators(cmd.Context(), launchID)
+			if err != nil {
+				return err
+			}
 
-	peers := make([]string, 0)
-	for _, acc := range genVals {
-		peers = append(peers, acc.Peer)
+			peers := make([]string, 0)
+			for _, acc := range genVals {
+				peers = append(peers, acc.Peer)
+			}
+			nb.Spinner.Stop()
+			if len(peers) > 0 {
+				fmt.Printf("Peers: %s\n", strings.Join(peers, ","))
+			} else {
+				fmt.Print("empty peer list")
+			}
+			return nil
+		},
 	}
-
-	return fmt.Sprintf("Peers: %s\n", strings.Join(peers, ",")), nil
+	return c
 }
