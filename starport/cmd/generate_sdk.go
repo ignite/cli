cc4e19e451fa92d5e3ecd7606651b7b5fc989711
Arthur Marinho <arthurtutuca@gmail.com>
2022-03-02 17:05:04 -0300

    Update generate_sdk.go

--- starport/cmd/generate_sdk.go
+++ starport/cmd/generate_sdk.go
@@ -4,22 +4,21 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-
 	"github.com/tendermint/starport/starport/pkg/clispinner"
 	"github.com/tendermint/starport/starport/services/chain"
 )
 
-func NewGenerateVuex() *cobra.Command {
+func NewGenerateSDK() *cobra.Command {
 	c := &cobra.Command{
-		Use:   "vuex",
-		Short: "Generate Vuex store for you chain's frontend from your config.yml",
-		RunE:  generateVuexHandler,
+		Use:   "sdk",
+		Short: "Generate Typescript SDK for you chain's frontend from your config.yml",
+		RunE:  generateSDKHandler,
 	}
 	c.Flags().AddFlagSet(flagSetProto3rdParty(""))
 	return c
 }
 
-func generateVuexHandler(cmd *cobra.Command, args []string) error {
+func generateSDKHandler(cmd *cobra.Command, args []string) error {
 	s := clispinner.New().SetText("Generating...")
 	defer s.Stop()
 
@@ -28,12 +27,12 @@ func generateVuexHandler(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	if err := c.Generate(cmd.Context(), chain.GenerateVuex()); err != nil {
+	if err := c.Generate(cmd.Context(), chain.GenerateSDK()); err != nil {
 		return err
 	}
 
 	s.Stop()
-	fmt.Println("⛏️  Generated vuex stores.")
+	fmt.Println("⛏️  Generated SDK")
 
 	return nil
 }
