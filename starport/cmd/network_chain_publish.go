2800591ee012ce1fbf23f4bfdb824deb5eaa4b0c
Danilo Pantani <danpantani@gmail.com>
2022-03-09 19:46:08 -0300

    update spn version

--- starport/cmd/network_chain_publish.go
+++ starport/cmd/network_chain_publish.go
@@ -6,6 +6,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
+
 	"github.com/tendermint/starport/starport/pkg/clispinner"
 	"github.com/tendermint/starport/starport/services/network"
 	"github.com/tendermint/starport/starport/services/network/networkchain"
