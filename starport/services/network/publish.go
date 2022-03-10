86359f42f902022d5b9969df94a688a0fe5ad2f6
Danilo Pantani <danpantani@gmail.com>
2022-03-10 17:28:36 -0300

    fix the error return for network Publish method

--- starport/services/network/publish.go
+++ starport/services/network/publish.go
@@ -173,7 +173,7 @@ func (n Network) Publish(ctx context.Context, c Chain, options ...PublishOption)
 	// FIXME: get the operator address from the cosmos-sdk
 	operatorAddress := ""
 	if err := n.SetValidatorOperatorAddress(operatorAddress); err != nil {
-		return 0, 0, err
+		return 0, 0, 0, err
 	}
 
 	if !o.totalShares.Empty() {
