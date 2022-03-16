1e3eee787a9024eb4ce318689142e4c5243fdea1
Alex Megalokonomos <alex@clockwork.gr>
2022-03-16 09:47:04 +0200

    fix(codegen): protoc to keep snake_case for swagger and ts out (#2149)
    * fix: protoc to keep snake_case for swagger and ts out
    
    * refactor
    
    * removed SpVuexError
    
    Co-authored-by: İlker G. Öztürk <ilkergoktugozturk@gmail.com>
    Co-authored-by: marinhoarthur <arthurtutuca@gmail.com>

--- starport/pkg/cosmosgen/generate_javascript.go
+++ starport/pkg/cosmosgen/generate_javascript.go
@@ -26,7 +26,7 @@ var (
 	}
 
 	jsOpenAPIOut = []string{
-		"--openapiv2_out=logtostderr=true,allow_merge=true,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:.",
+		"--openapiv2_out=logtostderr=true,allow_merge=true,json_names_for_fields=false,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:.",
 	}
 )
 
@@ -103,7 +103,7 @@ func (g *jsGenerator) generateModule(ctx context.Context, tsprotoPluginPath, app
 		m.Pkg.Path,
 		includePaths,
 		tsOut,
-		protoc.Plugin(tsprotoPluginPath),
+		protoc.Plugin(tsprotoPluginPath, "--ts_proto_opt=snakeToCamel=false"),
 	)
 	if err != nil {
 		return err
