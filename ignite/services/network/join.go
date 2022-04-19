35ec162fcb86e1ca675e5830249407539b32d3b6
Danilo Pantani <danpantani@gmail.com>
2022-04-19 04:19:11 -0300

    fix(network): fix wrong account address check for the join command (#2386)

--- ignite/services/network/join.go
+++ ignite/services/network/join.go
@@ -125,10 +125,9 @@ func (n Network) sendAccountRequest(
 	genesisPath string,
 	isCustomGentx bool,
 	launchID uint64,
-	accountAddress string,
+	address string,
 	amount sdk.Coins,
 ) (err error) {
-	address := n.account.Address(networktypes.SPN)
 	n.ev.Send(events.New(events.StatusOngoing, "Verifying account already exists "+address))
 
 	// if is custom gentx path, avoid to check account into genesis from the home folder
@@ -154,7 +153,7 @@ func (n Network) sendAccountRequest(
 	msg := launchtypes.NewMsgRequestAddAccount(
 		n.account.Address(networktypes.SPN),
 		launchID,
-		accountAddress,
+		address,
 		amount,
 	)
 
