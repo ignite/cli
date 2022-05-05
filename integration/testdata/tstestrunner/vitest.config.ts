f50ad1614d4458341dacd11e33ee338c9345f804
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-05-05 18:19:14 +0200

    feat: add tstestrunner package to integration tests
    The package uses Vitest [https://vitest.dev] to run test written in
    typescript.

---
+++ integration/testdata/tstestrunner/vitest.config.ts
@@ -0,0 +1,39 @@
+import path from 'path'
+import { readdirSync, Dirent } from 'fs'
+import { defineConfig } from 'vitest/config'
+
+type Alias = {
+  [key: string]: string
+}
+
+const collectModuleAliases = (alias: Alias, modulesPath: string) => {
+  readdirSync(modulesPath, { withFileTypes: true }).forEach((item: Dirent) => {
+    if (item.name.startsWith('.') || item.isFile()) {
+      return
+    }
+
+    alias[item.name] = path.join(modulesPath, item.name)
+  });
+}
+
+// Absolute path to the blockchain app directory
+const chainPath = process.env.TSC_CHAIN_PATH
+
+// The module aliases are used to be able to import generated code within the tests
+const alias: Alias = {}
+
+// Collect the module aliases for the chain app
+collectModuleAliases(alias, `${chainPath}/vue/node_modules`)
+
+// Collect the module aliases for the generated Vuex store
+collectModuleAliases(alias, `${chainPath}/vue/src/store/generated/cosmos/cosmos-sdk`)
+collectModuleAliases(alias, `${chainPath}/vue/src/store/generated/cosmos/ibc-go`)
+
+export default defineConfig({
+  test: {
+    include: ['**/*_test.ts']
+  },
+  resolve: {
+    alias
+  },
+})
