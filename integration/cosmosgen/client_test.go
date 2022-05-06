e997b5cbe5737b40285e42adc222003b84d93f91
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-05-06 17:37:30 +0200

    test: add integration TS client test to perform a transfer

---
+++ integration/cosmosgen/client_test.go
@@ -0,0 +1,79 @@
+package cosmosgen
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ignite-hq/cli/ignite/chainconfig"
+	"github.com/ignite-hq/cli/ignite/pkg/cmdrunner/step"
+	"github.com/ignite-hq/cli/ignite/pkg/xurl"
+	envtest "github.com/ignite-hq/cli/integration"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClientBank(t *testing.T) {
+	var (
+		env  = envtest.New(t)
+		path = env.Scaffold("chain")
+		host = env.RandomizeServerPorts(path, "")
+	)
+
+	queryAPI, err := xurl.HTTP(host.API)
+	require.NoError(t, err)
+
+	txAPI, err := xurl.TCP(host.RPC)
+	require.NoError(t, err)
+
+	// Accounts to be included in the genesis
+	env.UpdateConfig(path, "", func(cfg *chainconfig.Config) error {
+		accounts := []chainconfig.Account{
+			{
+				Name:    "account1",
+				Address: "cosmos1j8hw8283hj80hhq8urxaj40syrzqp77dt8qwhm",
+				Coins:   []string{"10000token", "10000stake"},
+			},
+			{
+				Name:    "account2",
+				Address: "cosmos19yy9sf00k00cjcwh532haeq8s63uhdy7qs5m2n",
+				Coins:   []string{"100token", "100stake"},
+			},
+			{
+				Name:    "account3",
+				Address: "cosmos10957ee377t2xpwyt4mlpedjldp592h0ylt8uz7",
+				Coins:   []string{"100token", "100stake"},
+			},
+		}
+
+		cfg.Accounts = append(cfg.Accounts, accounts...)
+
+		return nil
+	})
+
+	env.Must(env.Exec("generate vuex store", step.NewSteps(
+		step.New(
+			step.Exec(envtest.IgniteApp, "g", "vuex", "--proto-all-modules", "--yes"),
+			step.Workdir(path),
+		),
+	)))
+
+	env.InstallClientVueDeps(path)
+
+	ctx, cancel := context.WithTimeout(env.Ctx(), envtest.ServeTimeout)
+
+	go func() {
+		defer cancel()
+		err = env.IsAppServed(ctx, host)
+	}()
+
+	env.Must(env.Serve("should serve", path, "", "", envtest.ExecCtx(ctx)))
+	require.NoError(t, err, "app cannot get online in time")
+
+	env.RunClientTests(
+		path,
+		envtest.ClientTestName("Bank"),
+		envtest.ClientEnv(map[string]string{
+			"TEST_QUERY_API": queryAPI,
+			"TEST_TX_API":    txAPI,
+		}),
+	)
+}
---
+++ integration/cosmosgen/testdata/bank_test.ts
@@ -0,0 +1,61 @@
+import { beforeAll, describe, expect, test } from 'vitest'
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing'
+import { isDeliverTxSuccess } from '@cosmjs/stargate'
+
+import { txClient, queryClient } from 'cosmos.bank.v1beta1/module'
+
+describe('Bank', () => {
+  let txApi: string
+  let queryApi: string
+
+  beforeAll(() => {
+    txApi = process.env.TEST_TX_API || ''
+    queryApi = process.env.TEST_QUERY_API || ''
+
+    expect(txApi, 'TEST_TX_API is required').not.toEqual('')
+    expect(queryApi, 'TEST_QUERY_API is required').not.toEqual('')
+  })
+
+  test('transfers to two different addresses', async () => {
+    const denom = 'token'
+    const toAddresses = [
+      'cosmos19yy9sf00k00cjcwh532haeq8s63uhdy7qs5m2n',
+      'cosmos10957ee377t2xpwyt4mlpedjldp592h0ylt8uz7',
+    ]
+
+    // TODO: should we send values from the chain integration test (mnemonic, addresses, ...) ?
+    const mnemonic = 'toe mail light plug pact length excess predict real artwork laundry when steel online adapt clutch debate vehicle dash alter rifle virtual season almost'
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic)
+    const [account] = await wallet.getAccounts();
+    const tx = await txClient(wallet, { addr: txApi })
+
+    // Both accounts start with 100token before the transfer
+    const result = await tx.signAndBroadcast([
+      tx.msgSend({
+        from_address: account.address,
+        to_address: toAddresses[0],
+        amount: [{ denom, amount: '100' }],
+      }),
+      tx.msgSend({
+        from_address: account.address,
+        to_address: toAddresses[1],
+        amount: [{ denom, amount: '200' }],
+      }),
+    ])
+
+    expect(isDeliverTxSuccess(result)).toEqual(true)
+
+    const query = await queryClient({ addr: queryApi })
+    const cases = [
+      { address: toAddresses[0], wantAmount: '200' },
+      { address: toAddresses[1], wantAmount: '300' },
+    ]
+
+    for (let tc of cases) {
+      let response = await query.queryBalance(tc.address, { denom })
+
+      expect(response.statusText).toEqual('OK')
+      expect(response.data.balance.amount).toEqual(tc.wantAmount)
+    }
+  })
+})
