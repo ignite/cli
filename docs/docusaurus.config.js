c0e2849d98e090fb6f09f7e07aa0a76be7ea32d0
Alexis Colin <nesquimo@gmail.com>
2022-05-17 10:54:52 +0200

    fix: scrolltotop button

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -54,6 +54,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      docs: {
+        sidebar: {
+          autoCollapseCategories: true,
+        },
+      },
       navbar: {
         logo: {
           alt: "Ignite Logo",
