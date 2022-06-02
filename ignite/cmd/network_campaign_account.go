8287eba582b0002c04ea5047f53ea625be3e53ee
ltacker <lucas.bertrand.22@gmail.com>
2022-06-02 10:38:49 +0200

    remove vesting accounts from campaign accounts

--- ignite/cmd/network_campaign_account.go
+++ ignite/cmd/network_campaign_account.go
@@ -2,8 +2,6 @@ package ignitecmd
 
 import (
 	"context"
-	"strconv"
-
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
 
@@ -15,7 +13,6 @@ import (
 
 var (
 	campaignMainnetsAccSummaryHeader = []string{"Mainnet Account", "Shares"}
-	campaignVestingAccSummaryHeader  = []string{"Vesting Account", "Total Shares", "Vesting", "End Time"}
 )
 
 // NewNetworkCampaignAccount creates a new campaign account command that holds some other
@@ -55,12 +52,12 @@ func newNetworkCampaignAccountListHandler(cmd *cobra.Command, args []string) err
 	}
 
 	// get all campaign accounts
-	mainnetAccs, vestingAccs, err := getAccounts(cmd.Context(), n, campaignID)
+	mainnetAccs, err := getAccounts(cmd.Context(), n, campaignID)
 	if err != nil {
 		return err
 	}
 
-	if len(mainnetAccs)+len(vestingAccs) == 0 {
+	if len(mainnetAccs) == 0 {
 		session.StopSpinner()
 		return session.Printf("%s %s\n", icons.Info, "no campaign account found")
 	}
@@ -69,15 +66,6 @@ func newNetworkCampaignAccountListHandler(cmd *cobra.Command, args []string) err
 	for _, acc := range mainnetAccs {
 		mainnetAccEntries = append(mainnetAccEntries, []string{acc.Address, acc.Shares.String()})
 	}
-	mainnetVestingAccEntries := make([][]string, 0)
-	for _, acc := range vestingAccs {
-		mainnetVestingAccEntries = append(mainnetVestingAccEntries, []string{
-			acc.Address,
-			acc.TotalShares.String(),
-			acc.Vesting.String(),
-			strconv.FormatInt(acc.EndTime, 10),
-		})
-	}
 
 	session.StopSpinner()
 	if len(mainnetAccEntries) > 0 {
@@ -85,30 +73,23 @@ func newNetworkCampaignAccountListHandler(cmd *cobra.Command, args []string) err
 			return err
 		}
 	}
-	if len(mainnetVestingAccEntries) > 0 {
-		if err = session.PrintTable(campaignVestingAccSummaryHeader, mainnetVestingAccEntries...); err != nil {
-			return err
-		}
-	}
 
 	return nil
 }
 
-// getAccounts get all campaign mainnet and vesting accounts.
+// getAccounts get all campaign mainnet accounts.
 func getAccounts(
 	ctx context.Context,
 	n network.Network,
 	campaignID uint64,
 ) (
 	[]networktypes.MainnetAccount,
-	[]networktypes.MainnetVestingAccount,
 	error,
 ) {
 	// start serving components.
 	g, ctx := errgroup.WithContext(ctx)
 	var (
 		mainnetAccs []networktypes.MainnetAccount
-		vestingAccs []networktypes.MainnetVestingAccount
 		err         error
 	)
 	// get all campaign mainnet accounts
@@ -117,10 +98,5 @@ func getAccounts(
 		return err
 	})
 
-	// get all campaign vesting accounts
-	g.Go(func() error {
-		vestingAccs, err = n.MainnetVestingAccounts(ctx, campaignID)
-		return err
-	})
-	return mainnetAccs, vestingAccs, g.Wait()
+	return mainnetAccs, g.Wait()
 }
