ab8f441240d41172c02d0cff2752beb4cce6a9c3
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-06-13 10:03:16 +0200

    refactor: change postgres filters to allow custom field filtering
    This is required for call queries because the field names used in the
    views or functions are not the standard TX, event and attr ones.

--- ignite/pkg/cosmosmetric/adapter/postgres/filters.go
+++ ignite/pkg/cosmosmetric/adapter/postgres/filters.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/ignite-hq/cli/ignite/pkg/cosmosmetric/adapter"
 	"github.com/ignite-hq/cli/ignite/pkg/cosmosmetric/query"
 )
 
@@ -11,48 +12,53 @@ const (
 	filterPlaceholder = "?"
 )
 
-func NewTextFilter(field query.Field, value string) TextFilter {
-	return TextFilter{field, value}
+func NewFilter(field, value string) Filter {
+	return Filter{field, value}
 }
 
-type TextFilter struct {
-	field query.Field
-	value string
+type Filter struct {
+	field, value string
 }
 
-func (f TextFilter) String() string {
-	return fmt.Sprintf("%s = %s", fieldMap[f.field], filterPlaceholder)
+func (f Filter) String() string {
+	return fmt.Sprintf("%s = %s", f.field, filterPlaceholder)
 }
 
-func (f TextFilter) GetField() query.Field {
+func (f Filter) GetField() string {
 	return f.field
 }
 
-func (f TextFilter) GetValue() any {
+func (f Filter) GetValue() any {
 	return f.value
+}
+
+func NewTextFieldFilter(f query.Field, value string) Filter {
+	field := fieldMap[f]
+
+	return Filter{field, value}
 }
 
 func NewTextEventAttrValueFilter(value string) TextEventAttrValueFilter {
 	return TextEventAttrValueFilter{
 		// The string value must be quoted to match with the JSONB text
-		NewTextFilter(query.FieldEventAttrValue, strconv.Quote(value)),
+		NewTextFieldFilter(adapter.FieldEventAttrValue, strconv.Quote(value)),
 	}
 }
 
 type TextEventAttrValueFilter struct {
-	TextFilter
+	Filter
 }
 
 func (f TextEventAttrValueFilter) String() string {
-	return fmt.Sprintf("%s::text = %s", fieldMap[f.field], filterPlaceholder)
+	return fmt.Sprintf("%s::text = %s", f.GetField(), filterPlaceholder)
 }
 
-func FilterByEventType(eventType string) TextFilter {
-	return NewTextFilter(query.FieldEventType, eventType)
+func FilterByEventType(eventType string) Filter {
+	return NewTextFieldFilter(adapter.FieldEventType, eventType)
 }
 
-func FilterByEventAttrName(name string) TextFilter {
-	return NewTextFilter(query.FieldEventAttrName, name)
+func FilterByEventAttrName(name string) Filter {
+	return NewTextFieldFilter(adapter.FieldEventAttrName, name)
 }
 
 func FilterByEventAttrValue(v string) TextEventAttrValueFilter {
