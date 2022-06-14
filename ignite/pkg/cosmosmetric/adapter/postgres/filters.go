d6fb75f9325c23395e4e6f2287df53232fc4ecc6
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-06-14 11:12:44 +0200

    refactor: change postgres filters to be more flexible
    These changes make the Filter more generic to avoid defining extra types
    to customize the filters behavior.

--- ignite/pkg/cosmosmetric/adapter/postgres/filters.go
+++ ignite/pkg/cosmosmetric/adapter/postgres/filters.go
@@ -12,16 +12,60 @@ const (
 	filterPlaceholder = "?"
 )
 
-func NewFilter(field, value string) Filter {
-	return Filter{field, value}
+// Modifier defines a function that can be used to modify a field name or value.
+type Modifier func(field string) string
+
+// CastJSONToText modifier casts a JSON/JSONB field to text.
+func CastJSONToText(f string) string {
+	return fmt.Sprintf("%s::text", f)
 }
 
+// CastJSONToNumeric modifier casts a JSON/JSONB field to numeric.
+func CastJSONToNumeric(f string) string {
+	return fmt.Sprintf("%s::numeric", f)
+}
+
+// FilterOption defines an option for filters.
+type FilterOption func(*Filter)
+
+// WithModifiers assigns one or more field modifier functions to the filter.
+// Field modifiers can be used to change the behavior of a filtered field.
+func WithModifiers(m ...Modifier) FilterOption {
+	return func(f *Filter) {
+		f.modifiers = m
+	}
+}
+
+// NewFilter creates a new generic equality filter for a query field.
+// The result is a generic equality filter that maps the query field
+// to the database field name.
+func NewFilterFromField(f query.Field, value any, options ...FilterOption) Filter {
+	return NewFilter(fieldMap[f], value, options...)
+}
+
+// NewFilter creates a new generic equality filter.
+func NewFilter(field string, value any, options ...FilterOption) Filter {
+	f := Filter{
+		field: field,
+		value: value,
+	}
+
+	for _, o := range options {
+		o(&f)
+	}
+
+	return f
+}
+
+// Filter defines a generic equality filter.
 type Filter struct {
-	field, value string
+	field     string
+	value     any
+	modifiers []Modifier
 }
 
 func (f Filter) String() string {
-	return fmt.Sprintf("%s = %s", f.field, filterPlaceholder)
+	return fmt.Sprintf("%s = %s", f.applyModifiers(f.field), filterPlaceholder)
 }
 
 func (f Filter) GetField() string {
@@ -32,35 +76,30 @@ func (f Filter) GetValue() any {
 	return f.value
 }
 
-func NewTextFieldFilter(f query.Field, value string) Filter {
-	field := fieldMap[f]
-
-	return Filter{field, value}
-}
-
-func NewTextEventAttrValueFilter(value string) TextEventAttrValueFilter {
-	return TextEventAttrValueFilter{
-		// The string value must be quoted to match with the JSONB text
-		NewTextFieldFilter(adapter.FieldEventAttrValue, strconv.Quote(value)),
+func (f Filter) applyModifiers(field string) string {
+	// Apply all the field modifiers in order
+	for _, m := range f.modifiers {
+		field = m(field)
 	}
+
+	return field
 }
 
-type TextEventAttrValueFilter struct {
-	Filter
-}
-
-func (f TextEventAttrValueFilter) String() string {
-	return fmt.Sprintf("%s::text = %s", f.GetField(), filterPlaceholder)
-}
-
+// FilterByEventType creates a new filter to match events by type.
 func FilterByEventType(eventType string) Filter {
-	return NewTextFieldFilter(adapter.FieldEventType, eventType)
+	return NewFilterFromField(adapter.FieldEventType, eventType)
 }
 
+// FilterByEventAttrName creates a new filter to match events by attribute name.
 func FilterByEventAttrName(name string) Filter {
-	return NewTextFieldFilter(adapter.FieldEventAttrName, name)
+	return NewFilterFromField(adapter.FieldEventAttrName, name)
 }
 
-func FilterByEventAttrValue(v string) TextEventAttrValueFilter {
-	return NewTextEventAttrValueFilter(v)
+// FilterByEventAttrValue creates a new filter to match events by attribute value.
+func FilterByEventAttrValue(v string) Filter {
+	// The string value must be quoted to match with the JSONB text
+	v = strconv.Quote(v)
+
+	// Use a field modifier to cast the event attribute value JSONB field to text
+	return NewFilterFromField(adapter.FieldEventAttrValue, v, WithModifiers(CastJSONToText))
 }
