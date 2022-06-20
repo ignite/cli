b6c9e4ba920cd222e48397b96b4d30243b7407b2
Petr Ivanov <petr.ivanov@tendermint.com>
2022-06-20 12:01:24 +0400

    fix show genesis

--- ignite/cmd/network_chain_show_genesis.go
+++ ignite/cmd/network_chain_show_genesis.go
@@ -1,6 +1,7 @@
 package ignitecmd
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -55,59 +56,50 @@ func networkChainShowGenesisHandler(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	genesisPath, err := c.GenesisPath()
-	if err != nil {
-		return err
-	}
-
 	spnChainID, err := n.ChainID(cmd.Context())
 	if err != nil {
 		return err
 	}
 
-	// check if the genesis already exists
-	if _, err = os.Stat(genesisPath); os.IsNotExist(err) {
-		// fetch the information to construct genesis
-		genesisInformation, err := n.GenesisInformation(cmd.Context(), launchID)
-		if err != nil {
-			return err
-		}
+	// fetch the information to construct genesis
+	genesisInformation, err := n.GenesisInformation(cmd.Context(), launchID)
+	if err != nil {
+		return err
+	}
 
-		// create the chain in a temp dir
-		tmpHome, err := os.MkdirTemp("", "*-spn")
-		if err != nil {
-			return err
-		}
-		defer os.RemoveAll(tmpHome)
+	// create the chain in a temp dir
+	tmpHome, err := os.MkdirTemp("", "*-spn")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tmpHome)
 
-		c.SetHome(tmpHome)
+	c.SetHome(tmpHome)
 
-		rewardsInfo, lastBlockHeight, unboundingTime, err := n.RewardsInfo(
-			cmd.Context(),
-			launchID,
-			chainLaunch.ConsumerRevisionHeight,
-		)
-		if err != nil {
-			return err
-		}
+	rewardsInfo, lastBlockHeight, unboundingTime, err := n.RewardsInfo(
+		cmd.Context(),
+		launchID,
+		chainLaunch.ConsumerRevisionHeight,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to fetch reward info: %w", err)
+	}
 
-		if err = c.Prepare(
-			cmd.Context(),
-			cacheStorage,
-			genesisInformation,
-			rewardsInfo,
-			spnChainID,
-			lastBlockHeight,
-			unboundingTime,
-		); err != nil {
-			return err
-		}
+	if err = c.Prepare(
+		cmd.Context(),
+		cacheStorage,
+		genesisInformation,
+		rewardsInfo,
+		spnChainID,
+		lastBlockHeight,
+		unboundingTime,
+	); err != nil {
+		return fmt.Errorf("failed to fetch genesis info: %w", err)
+	}
 
-		// get the new genesis path
-		genesisPath, err = c.GenesisPath()
-		if err != nil {
-			return err
-		}
+	genesisPath, err := c.GenesisPath()
+	if err != nil {
+		return err
 	}
 
 	if err := os.MkdirAll(filepath.Dir(out), 0744); err != nil {
