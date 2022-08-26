5e46d12dad8f91ac95b891fb8c24cf8c2eec1358
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-08-26 17:40:41 +0200

    refactor: change `ConvertLatest` to return `*v1.Config`
    This function must always return the latest config.

--- ignite/chainconfig/convert.go
+++ ignite/chainconfig/convert.go
@@ -1,21 +1,50 @@
 package chainconfig
 
-import "github.com/ignite-hq/cli/ignite/chainconfig/config"
+import (
+	"os"
 
-// ConvertLatest converts a Config to the latest version of Config.
-func ConvertLatest(cfg config.Converter) (config.Converter, error) {
+	"github.com/ignite-hq/cli/ignite/chainconfig/config"
+	v1 "github.com/ignite-hq/cli/ignite/chainconfig/v1"
+	"gopkg.in/yaml.v2"
+)
+
+// Build time check for the latest config version type.
+// This is required to be sure that conversion to latest
+// doesn't break when a new config version is added without
+// updating the references to the previous version.
+var _ = Versions[LatestVersion].(*v1.Config)
+
+// ConvertLatest converts a config to the latest version.
+func ConvertLatest(c config.Converter) (*v1.Config, error) {
 	var err error
 
-	version := cfg.GetVersion()
-
-	for version < LatestVersion {
-		cfg, err = cfg.ConvertNext()
+	for c.GetVersion() < LatestVersion {
+		c, err = c.ConvertNext()
 		if err != nil {
 			return nil, err
 		}
-
-		version = cfg.GetVersion()
 	}
 
-	return cfg, nil
+	// Cast to the latest version type.
+	// This is safe because there is a build time check that makes sure
+	// the type for the latest config version is the right one here.
+	return c.(*v1.Config), nil
+}
+
+// MigrateLatest migrates a config file to the latest version.
+// TODO: Change to receive an io.Reader and an io.Writer instead of saving to file
+func MigrateLatest(path string) error {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	cfg, err := Parse(file)
+	if err != nil {
+		return err
+	}
+
+	return yaml.NewEncoder(file).Encode(cfg)
 }
--- ignite/chainconfig/convert_test.go
+++ ignite/chainconfig/convert_test.go
@@ -3,39 +3,20 @@ package chainconfig_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ignite-hq/cli/ignite/chainconfig"
-	"github.com/ignite-hq/cli/ignite/chainconfig/config"
 	v0testdata "github.com/ignite-hq/cli/ignite/chainconfig/v0/testdata"
-	v1 "github.com/ignite-hq/cli/ignite/chainconfig/v1"
 )
 
 func TestConvertLatest(t *testing.T) {
-	origin := v0testdata.GetInitialV0Config()
-	result, err := chainconfig.ConvertLatest(origin)
-	assert.Nil(t, err)
-	expected := v0testdata.GetConvertedLatestConfig()
+	// Arrange
+	cfgV0 := v0testdata.GetConfigV0()
 
-	cfg, ok := result.(*v1.Config)
-	require.Truef(t, ok, "expected v1 config, got %T", result)
+	// Act
+	cfgLatest, err := chainconfig.ConvertLatest(cfgV0)
 
-	require.Equal(t, config.Version(0), origin.GetVersion())
-	require.Equal(t, config.Version(1), result.GetVersion())
-	require.Equal(t, origin.Faucet, cfg.Faucet)
-	require.Equal(t, origin.Client, cfg.Client)
-	require.Equal(t, origin.Build, cfg.Build)
-	require.Equal(t, origin.Host.RPC, cfg.Validators[0].GetRPC())
-	require.Equal(t, origin.Host.P2P, cfg.Validators[0].GetP2P())
-	require.Equal(t, origin.Host.GRPC, cfg.Validators[0].GetGRPC())
-	require.Equal(t, origin.Host.GRPCWeb, cfg.Validators[0].GetGRPCWeb())
-	require.Equal(t, origin.Host.Prof, cfg.Validators[0].GetProf())
-	require.Equal(t, origin.Host.API, cfg.Validators[0].GetAPI())
-	require.Equal(t, origin.Genesis, cfg.Genesis)
-	require.Equal(t, origin.ListAccounts(), cfg.ListAccounts())
-	require.Equal(t, origin.Init.KeyringBackend, cfg.Validators[0].KeyringBackend)
-	require.Equal(t, origin.Init.Client, cfg.Validators[0].Client)
-	require.Equal(t, origin.Init.Home, cfg.Validators[0].Home)
-	require.Equal(t, expected, result)
+	// Assert
+	require.NoError(t, err)
+	require.Equal(t, chainconfig.LatestVersion, cfgLatest.GetVersion())
 }
