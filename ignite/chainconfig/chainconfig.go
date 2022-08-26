c8dd68722307cfc49331e8fecd750a61846d4640
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-08-26 17:34:52 +0200

    refactor: remove global and rename var `Migration` to `Versions`

--- ignite/chainconfig/chainconfig.go
+++ ignite/chainconfig/chainconfig.go
@@ -1,8 +1,6 @@
 package chainconfig
 
 import (
-	"errors"
-
 	"github.com/ignite-hq/cli/ignite/chainconfig/config"
 	v0 "github.com/ignite-hq/cli/ignite/chainconfig/v0"
 	v1 "github.com/ignite-hq/cli/ignite/chainconfig/v1"
@@ -10,39 +8,23 @@ import (
 )
 
 var (
-	// LatestVersion defines the latest version of the config.
-	LatestVersion config.Version = 1
-
-	// ErrConfigNotFound indicates that the config.yml can't be found.
-	ErrConfigNotFound = errors.New("could not locate a config.yml in your chain")
-
 	// ConfigDirPath returns the path of configuration directory of Ignite.
 	ConfigDirPath = xfilepath.JoinFromHome(xfilepath.Path(".ignite"))
 
 	// ConfigFileNames is a list of recognized names as for Ignite's config file.
 	ConfigFileNames = []string{"config.yml", "config.yaml"}
 
-	// DefaultConfig defines the default config without the validators.
-	DefaultConfig = &v1.Config{
-		BaseConfig: config.BaseConfig{
-			Build: config.Build{
-				Proto: config.Proto{
-					Path: "proto",
-					ThirdPartyPaths: []string{
-						"third_party/proto",
-						"proto_vendor",
-					},
-				},
-			},
-			Faucet: config.Faucet{
-				Host: "0.0.0.0:4500",
-			},
-		},
-	}
+	// LatestVersion defines the latest version of the config.
+	LatestVersion config.Version = 1
 
-	// Migration defines the version as the key and the config instance as the value
-	Migration = map[config.Version]config.Converter{
+	// Versions holds config types for the supported versions.
+	Versions = map[config.Version]config.Converter{
 		0: &v0.Config{},
 		1: &v1.Config{},
 	}
 )
+
+// DefaultConfig returns a config for the latest version initialized with default values.
+func DefaultConfig() *v1.Config {
+	return v1.DefaultConfig()
+}
