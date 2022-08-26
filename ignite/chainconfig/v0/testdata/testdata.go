bf42bc913918f250cc4547e500e99766e47e9551
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-08-26 17:50:05 +0200

    test: change `v0` testdata to only return a v0 config with values

--- ignite/chainconfig/v0/testdata/testdata.go
+++ ignite/chainconfig/v0/testdata/testdata.go
@@ -3,12 +3,12 @@ package testdata
 import (
 	"github.com/ignite-hq/cli/ignite/chainconfig/config"
 	v0 "github.com/ignite-hq/cli/ignite/chainconfig/v0"
-	v1 "github.com/ignite-hq/cli/ignite/chainconfig/v1"
 )
 
-var faucetName = "bob"
+// GetConfigV0 returns a v0 config initialized with random values.
+func GetConfigV0() *v0.Config {
+	faucetName := "bob"
 
-func GetInitialV0Config() *v0.Config {
 	return &v0.Config{
 		BaseConfig: config.BaseConfig{
 			Version: 0,
@@ -61,78 +61,12 @@ func GetInitialV0Config() *v0.Config {
 			Client: map[string]interface{}{"test-client": "test-client"},
 		},
 		Host: config.Host{
-			// when in Docker on MacOS, it only works with 0.0.0.0.
 			RPC:     "localhost:53803",
 			P2P:     "localhost:50198",
 			Prof:    "localhost:53030",
 			GRPC:    "localhost:53831",
 			GRPCWeb: "localhost:46531",
 			API:     "localhost:51028",
-		},
-	}
-}
-
-func GetConvertedLatestConfig() *v1.Config {
-	return &v1.Config{
-		BaseConfig: config.BaseConfig{
-			Version: 1,
-			Build: config.Build{
-				Proto: config.Proto{
-					Path: "proto",
-					ThirdPartyPaths: []string{
-						"third_party/proto",
-						"proto_vendor",
-					},
-				},
-			},
-			Faucet: config.Faucet{
-				Name:     &faucetName,
-				Host:     "0.0.0.0:4500",
-				Coins:    []string{"20000token", "200000000stake"},
-				CoinsMax: []string{"10000token", "100000000stake"},
-				Port:     48772,
-			},
-			Accounts: []config.Account{
-				{
-					Name:  "alice",
-					Coins: []string{"20000token", "200000000stake"},
-				},
-				{
-					Name:  "bob",
-					Coins: []string{"10000token", "100000000stake"},
-				},
-			},
-			Client: config.Client{
-				Vuex: config.Vuex{
-					Path: "vue/src/store",
-				},
-				Dart: config.Dart{
-					Path: "",
-				},
-				OpenAPI: config.OpenAPI{
-					Path: "docs/static/openapi.yml",
-				},
-			},
-			Genesis: map[string]interface{}{},
-		},
-		Validators: []v1.Validator{
-			{
-				Name:   "alice",
-				Bonded: "100000000stake",
-				Client: map[string]interface{}{"test-client": "test-client"},
-				App: map[string]interface{}{
-					"grpc":     map[string]interface{}{"address": "localhost:53831"},
-					"grpc-web": map[string]interface{}{"address": "localhost:46531"},
-					"api":      map[string]interface{}{"address": "localhost:51028"},
-					"test-app": "test-app",
-				},
-				Config: map[string]interface{}{
-					"rpc":         map[string]interface{}{"laddr": "localhost:53803"},
-					"p2p":         map[string]interface{}{"laddr": "localhost:50198"},
-					"pprof_laddr": "localhost:53030",
-					"test-config": "test-config",
-				},
-			},
 		},
 	}
 }
