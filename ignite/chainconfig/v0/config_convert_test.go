5216747edb32941c394b3c46389e257b993d15c3
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-08-26 17:48:57 +0200

    test: add `TestConvertV0ToV1` to `v0` convert tests
    Also removed the `TestConvertNext`.

--- ignite/chainconfig/v0/config_convert_test.go
+++ ignite/chainconfig/v0/config_convert_test.go
@@ -3,7 +3,6 @@ package v0_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ignite-hq/cli/ignite/chainconfig/config"
@@ -11,27 +10,37 @@ import (
 	v1 "github.com/ignite-hq/cli/ignite/chainconfig/v1"
 )
 
-func TestConvertNext(t *testing.T) {
-	origin := testdata.GetInitialV0Config()
-	result, err := origin.ConvertNext()
-	assert.Nil(t, err)
-	expected := testdata.GetConvertedLatestConfig()
+func TestConvertV0ToV1(t *testing.T) {
+	// Arrange
+	cfgV0 := testdata.GetConfigV0()
 
-	cfg, ok := result.(*v1.Config)
-	require.Truef(t, ok, "expected v1 config, got %T", result)
+	// Act
+	c, err := cfgV0.ConvertNext()
+	cfgV1, ok := c.(*v1.Config)
 
-	require.Equal(t, config.Version(0), origin.GetVersion())
-	require.Equal(t, config.Version(1), result.GetVersion())
-	require.Equal(t, origin.Faucet, cfg.Faucet)
-	require.Equal(t, origin.Client, cfg.Client)
-	require.Equal(t, origin.Build, cfg.Build)
-	require.Equal(t, origin.Host.RPC, cfg.Validators[0].GetRPC())
-	require.Equal(t, origin.Host.P2P, cfg.Validators[0].GetP2P())
-	require.Equal(t, origin.Host.GRPC, cfg.Validators[0].GetGRPC())
-	require.Equal(t, origin.Host.GRPCWeb, cfg.Validators[0].GetGRPCWeb())
-	require.Equal(t, origin.Host.Prof, cfg.Validators[0].GetProf())
-	require.Equal(t, origin.Host.API, cfg.Validators[0].GetAPI())
-	require.Equal(t, origin.Genesis, cfg.Genesis)
-	require.Equal(t, origin.ListAccounts(), cfg.ListAccounts())
-	require.Equal(t, expected, result)
+	// Assert
+	require.NoError(t, err)
+	require.Truef(t, ok, "expected *v1.Config, got %T", c)
+	require.Equal(t, config.Version(1), cfgV1.GetVersion())
+
+	// Assert: BaseConfig values
+	require.Equal(t, cfgV0.Build, cfgV1.Build)
+	require.Equal(t, cfgV0.Accounts, cfgV1.Accounts)
+	require.Equal(t, cfgV0.Faucet, cfgV1.Faucet)
+	require.Equal(t, cfgV0.Client, cfgV1.Client)
+	require.Equal(t, cfgV0.Genesis, cfgV1.Genesis)
+
+	// Assert: Values that must be migrated to the first validator
+	require.Len(t, cfgV1.Validators, 1)
+	require.Equal(t, cfgV0.Validator.Name, cfgV1.Validators[0].Name)
+	require.Equal(t, cfgV0.Validator.Staked, cfgV1.Validators[0].Bonded)
+	require.Equal(t, cfgV0.Init.Home, cfgV1.Validators[0].Home)
+	require.Equal(t, cfgV0.Init.KeyringBackend, cfgV1.Validators[0].KeyringBackend)
+	require.Equal(t, cfgV0.Init.Client, cfgV1.Validators[0].Client)
+	require.Equal(t, cfgV0.Host.RPC, cfgV1.Validators[0].GetRPC())
+	require.Equal(t, cfgV0.Host.P2P, cfgV1.Validators[0].GetP2P())
+	require.Equal(t, cfgV0.Host.GRPC, cfgV1.Validators[0].GetGRPC())
+	require.Equal(t, cfgV0.Host.GRPCWeb, cfgV1.Validators[0].GetGRPCWeb())
+	require.Equal(t, cfgV0.Host.Prof, cfgV1.Validators[0].GetProf())
+	require.Equal(t, cfgV0.Host.API, cfgV1.Validators[0].GetAPI())
 }
