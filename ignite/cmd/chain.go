7ad4b3f3ab8fc1930a1aba1138143cd404800712
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-08-28 09:40:32 +0200

    refactor: change migration cmd verifier
    The verifier now stops the command when the config migration is not
    made.

--- ignite/cmd/chain.go
+++ ignite/cmd/chain.go
@@ -2,12 +2,15 @@ package ignitecmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
 
 	"github.com/ignite-hq/cli/ignite/chainconfig"
 )
+
+var migrateMsg = `Your blockchain config version is v%[1]d and the latest is v%[2]d. Would you like to upgrade your config file to v%[2]d?`
 
 // NewChain returns a command that groups sub commands related to compiling, serving
 // blockchains and so on.
@@ -29,52 +32,71 @@ func NewChain() *cobra.Command {
 	return c
 }
 
-// TODO: Refactor config migration verifier
 func addConfigMigrationVerifier(cmd *cobra.Command) *cobra.Command {
 	cmd.Flags().AddFlagSet(flagSetConfig())
 	cmd.Flags().AddFlagSet(flagSetYes())
 
 	preRunFun := cmd.PreRunE
-	cmd.PreRunE = func(cmd *cobra.Command, args []string) error {
+	cmd.PreRunE = func(cmd *cobra.Command, args []string) (err error) {
 		if preRunFun != nil {
-			if err := preRunFun(cmd, args); err != nil {
+			if err = preRunFun(cmd, args); err != nil {
 				return err
 			}
 		}
 
-		appPath := flagGetPath(cmd)
 		configPath := getConfig(cmd)
-
-		var err error
 		if configPath == "" {
-			configPath, err = chainconfig.LocateDefault(appPath)
-			if err != nil {
+			appPath := flagGetPath(cmd)
+
+			if configPath, err = chainconfig.LocateDefault(appPath); err != nil {
 				return err
 			}
 		}
 
-		// Check if the version of the Config File is the latest
-		currentVersion, latest, err := chainconfig.IsConfigLatest(configPath)
+		// Open the file also for writing in case it is migrated to a new version
+		file, err := os.OpenFile(configPath, os.O_RDWR|os.O_CREATE, 0755)
 		if err != nil {
 			return err
 		}
 
-		if !latest {
+		defer file.Close()
+
+		version, err := chainconfig.ReadConfigVersion(file)
+		if err != nil {
+			return err
+		}
+
+		// Config files with older versions must be migrated to the latest before executing the command
+		if version != chainconfig.LatestVersion {
+			// Confirm before overwritting the config file
 			if !getYes(cmd) {
-				var confirmed bool
-				message := fmt.Sprintf("The configuration file of the project is at the version %d. The latest version is %d. Would you like to upgrade your configuration file of your project to the latest version?",
-					currentVersion, chainconfig.LatestVersion)
+				confirmed := false
 				prompt := &survey.Confirm{
-					Message: message,
+					Message: fmt.Sprintf(migrateMsg, version, chainconfig.LatestVersion),
 				}
-				if err := survey.AskOne(prompt, &confirmed); err != nil || !confirmed {
-					return nil
+
+				err := survey.AskOne(prompt, &confirmed)
+				if err != nil {
+					return err
+				} else if !confirmed {
+					return fmt.Errorf(
+						"stopping because config version v%d is required to run the command",
+						chainconfig.LatestVersion,
+					)
 				}
 			}
-			// Convert the current Config Yaml to the latest version
-			return chainconfig.MigrateLatest(configPath)
+
+			// Position at the beginning of the file before starting the migration
+			if _, err := file.Seek(0, 0); err != nil {
+				return err
+			}
+
+			// Convert the current config to the latest version and update the YAML file
+			return chainconfig.MigrateLatest(file)
 		}
+
 		return nil
 	}
+
 	return cmd
 }
