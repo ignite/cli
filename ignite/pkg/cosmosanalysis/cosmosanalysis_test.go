e1343472164f04724a4fc04e45722b85b271be4b
Denis Fadeev <denis@fadeev.org>
2022-08-31 12:45:48 +0400

    remove a placeholder test

--- ignite/pkg/cosmosanalysis/cosmosanalysis_test.go
+++ ignite/pkg/cosmosanalysis/cosmosanalysis_test.go
@@ -5,7 +5,6 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ignite/cli/ignite/pkg/cosmosanalysis"
@@ -202,9 +201,4 @@ func TestFindAppFilePath(t *testing.T) {
 	pathFound, err = cosmosanalysis.FindAppFilePath(tmpDir)
 	require.NoError(t, err)
 	require.Equal(t, filepath.Join(appFolder, "app.go"), pathFound)
-}
-
-func TestSomething(t *testing.T) {
-	err := errors.New("")
-	require.NoError(t, err)
 }
