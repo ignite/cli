0bc483c46ece65e79b0986ea4129f1a8b6bd062b
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-09-02 15:57:45 +0200

    test: fix broken integration test

--- integration/chain/config_test.go
+++ integration/chain/config_test.go
@@ -8,7 +8,6 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/imdario/mergo"
 	"github.com/stretchr/testify/require"
 
 	v1 "github.com/ignite/cli/ignite/chainconfig/v1"
@@ -27,45 +26,43 @@ func TestOverwriteSDKConfigsAndChainID(t *testing.T) {
 		isBackendAliveErr error
 	)
 
-	var c v1.Config
-
+	var cfg v1.Config
 	cf := confile.New(confile.DefaultYAMLEncodingCreator, filepath.Join(app.SourcePath(), "config.yml"))
-	require.NoError(t, cf.Load(&c))
+	require.NoError(t, cf.Load(&cfg))
 
-	c.Genesis = map[string]interface{}{"chain_id": "cosmos"}
-	defaultValidator := v1.Validator{
-		App:    map[string]interface{}{"hello": "cosmos"},
-		Config: map[string]interface{}{"fast_sync": false},
-	}
+	cfg.Genesis = map[string]interface{}{"chain_id": "cosmos"}
+	cfg.Validators[0].App["hello"] = "cosmos"
+	cfg.Validators[0].Config["fast_sync"] = false
 
-	for i := range c.Validators {
-		err := mergo.Merge(&c.Validators[i], defaultValidator)
-		require.NoError(t, err)
-	}
+	require.NoError(t, cf.Save(cfg))
 
-	require.NoError(t, cf.Save(c))
 	go func() {
 		defer cancel()
+
 		isBackendAliveErr = env.IsAppServed(ctx, servers)
 	}()
+
 	env.Must(app.Serve("should serve", envtest.ExecCtx(ctx)))
 	require.NoError(t, isBackendAliveErr, "app cannot get online in time")
 
-	configs := []struct {
-		ec          confile.EncodingCreator
-		relpath     string
-		key         string
-		expectedVal interface{}
+	cases := []struct {
+		ec      confile.EncodingCreator
+		relpath string
+		key     string
+		want    interface{}
 	}{
 		{confile.DefaultJSONEncodingCreator, "config/genesis.json", "chain_id", "cosmos"},
 		{confile.DefaultTOMLEncodingCreator, "config/app.toml", "hello", "cosmos"},
 		{confile.DefaultTOMLEncodingCreator, "config/config.toml", "fast_sync", false},
 	}
 
-	for _, c := range configs {
+	for _, tt := range cases {
 		var conf map[string]interface{}
-		cf := confile.New(c.ec, filepath.Join(env.AppHome(appname), c.relpath))
-		require.NoError(t, cf.Load(&conf))
-		require.Equal(t, c.expectedVal, conf[c.key])
+
+		path := filepath.Join(env.AppHome(appname), tt.relpath)
+		c := confile.New(tt.ec, path)
+
+		require.NoError(t, c.Load(&conf))
+		require.Equalf(t, tt.want, conf[tt.key], "unexpected value for %s", tt.relpath)
 	}
 }
