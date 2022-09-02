cfaae9e1baa50136277ef5ef9792e209cf6ec25e
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2022-09-02 11:11:51 +0200

    use black box testing only

--- ignite/pkg/cosmosclient/cosmosclient_test.go
+++ ignite/pkg/cosmosclient/cosmosclient_test.go
@@ -1,16 +1,10 @@
-package cosmosclient
+package cosmosclient_test
 
 import (
 	"context"
-	"io"
-	"os"
-	"path"
-	"strconv"
 	"testing"
 	"time"
 
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	sdktypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -18,6 +12,7 @@ import (
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 
 	"github.com/ignite/cli/ignite/pkg/cosmosaccount"
+	"github.com/ignite/cli/ignite/pkg/cosmosclient"
 	"github.com/ignite/cli/ignite/pkg/cosmosclient/mocks"
 )
 
@@ -27,136 +22,7 @@ type suite struct {
 	rpcClient *mocks.RPCClient
 }
 
-func newSuite(t *testing.T, setup func(suite)) suite {
-	s := suite{
-		rpcClient: mocks.NewRPCClient(t),
-	}
-	if setup != nil {
-		setup(s)
-	}
-	return s
-}
-
-func TestNew(t *testing.T) {
-	var (
-		ctx     = context.Background()
-		home, _ = os.UserHomeDir()
-	)
-	tests := []struct {
-		name           string
-		opts           []Option
-		expectedClient Client
-		expectedError  string
-		setup          func(suite)
-	}{
-		{
-			name: "ok: default values",
-			setup: func(s suite) {
-				s.rpcClient.EXPECT().Status(ctx).Return(&ctypes.ResultStatus{
-					NodeInfo: p2p.DefaultNodeInfo{Network: "mychain"},
-				}, nil)
-			},
-			expectedClient: Client{
-				chainID:            "mychain",
-				nodeAddress:        defaultNodeAddress,
-				homePath:           path.Join(home, ".mychain"),
-				keyringServiceName: "",
-				keyringDir:         path.Join(home, ".mychain"),
-				keyringBackend:     cosmosaccount.KeyringTest,
-				addressPrefix:      "cosmos",
-				faucetAddress:      defaultFaucetAddress,
-				faucetDenom:        defaultFaucetDenom,
-				faucetMinAmount:    defaultFaucetMinAmount,
-				out:                io.Discard,
-				gas:                strconv.Itoa(defaultGasLimit),
-				broadcastMode:      flags.BroadcastBlock,
-			},
-		},
-		{
-			name: "ok: custom values",
-			setup: func(s suite) {
-				s.rpcClient.EXPECT().Status(ctx).Return(&ctypes.ResultStatus{
-					NodeInfo: p2p.DefaultNodeInfo{Network: "mychain"},
-				}, nil)
-			},
-			opts: []Option{
-				WithHome("home"),
-				WithKeyringServiceName("keyringServiceName"),
-				WithKeyringBackend(cosmosaccount.KeyringOS),
-				WithKeyringDir("keyringDir"),
-				WithNodeAddress("addr"),
-				WithAddressPrefix("prefix"),
-				WithUseFaucet("faucetAddress", "denom", 42),
-				WithGas("gas"),
-				WithGasPrices("gasPrices"),
-				WithFees("fees"),
-				WithBroadcastMode("broadcastMode"),
-				WithGenerateOnly(true),
-			},
-			expectedClient: Client{
-				chainID:            "mychain",
-				nodeAddress:        "addr",
-				homePath:           "home",
-				keyringServiceName: "keyringServiceName",
-				keyringBackend:     cosmosaccount.KeyringOS,
-				keyringDir:         "keyringDir",
-				addressPrefix:      "prefix",
-				useFaucet:          true,
-				faucetAddress:      "faucetAddress",
-				faucetDenom:        "denom",
-				faucetMinAmount:    42,
-				out:                io.Discard,
-				gas:                "gas",
-				gasPrices:          "gasPrices",
-				fees:               "fees",
-				broadcastMode:      "broadcastMode",
-				generateOnly:       true,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			var (
-				require = require.New(t)
-				assert  = assert.New(t)
-				suite   = newSuite(t, tt.setup)
-			)
-			tt.opts = append(tt.opts,
-				WithRPCClient(suite.rpcClient),
-			)
-
-			c, err := New(ctx, tt.opts...)
-
-			if tt.expectedError != "" {
-				require.EqualError(err, tt.expectedError)
-				return
-			}
-			require.NoError(err)
-			assert.Equal(tt.expectedClient.chainID, "mychain")
-			assert.Equal(tt.expectedClient.nodeAddress, c.nodeAddress)
-			assert.Equal(tt.expectedClient.homePath, c.homePath)
-			assert.Equal(tt.expectedClient.keyringServiceName, c.keyringServiceName)
-			assert.Equal(tt.expectedClient.keyringBackend, c.keyringBackend)
-			assert.Equal(tt.expectedClient.keyringDir, c.keyringDir)
-			assert.Equal(tt.expectedClient.addressPrefix, c.addressPrefix)
-			assert.Equal(tt.expectedClient.useFaucet, c.useFaucet)
-			assert.Equal(tt.expectedClient.faucetAddress, c.faucetAddress)
-			assert.Equal(tt.expectedClient.faucetDenom, c.faucetDenom)
-			assert.Equal(tt.expectedClient.faucetMinAmount, c.faucetMinAmount)
-			assert.Equal(tt.expectedClient.out, c.out)
-			assert.Equal(tt.expectedClient.gas, c.gas)
-			assert.Equal(tt.expectedClient.fees, c.fees)
-			assert.Equal(tt.expectedClient.gasPrices, c.gasPrices)
-			assert.Equal(tt.expectedClient.broadcastMode, c.broadcastMode)
-			assert.Equal(tt.expectedClient.generateOnly, c.generateOnly)
-			// assert the sdk config has been updated with the addr prefix
-			config := sdktypes.GetConfig()
-			assert.Equal(tt.expectedClient.addressPrefix, config.GetBech32AccountAddrPrefix())
-		})
-	}
-}
-
-func newClient(t *testing.T, setup func(suite), opts ...Option) Client {
+func newClient(t *testing.T, setup func(suite), opts ...cosmosclient.Option) cosmosclient.Client {
 	s := suite{
 		rpcClient: mocks.NewRPCClient(t),
 	}
@@ -165,11 +31,11 @@ func newClient(t *testing.T, setup func(suite), opts ...Option) Client {
 	if setup != nil {
 		setup(s)
 	}
-	opts = append(opts, []Option{
-		WithRPCClient(s.rpcClient),
-		WithKeyringBackend(cosmosaccount.KeyringMemory),
+	opts = append(opts, []cosmosclient.Option{
+		cosmosclient.WithRPCClient(s.rpcClient),
+		cosmosclient.WithKeyringBackend(cosmosaccount.KeyringMemory),
 	}...)
-	c, err := New(context.Background(), opts...)
+	c, err := cosmosclient.New(context.Background(), opts...)
 	require.NoError(t, err)
 	return c
 }
@@ -324,13 +190,24 @@ func TestClientAddress(t *testing.T) {
 	require.NoError(t, err)
 
 	tests := []struct {
-		name          string
-		accountName   string
-		expectedError string
+		name           string
+		accountName    string
+		opts           []cosmosclient.Option
+		expectedError  string
+		expectedPrefix string
 	}{
 		{
-			name:        "ok: name exists",
-			accountName: expectedAccount.Name,
+			name:           "ok: name exists",
+			accountName:    expectedAccount.Name,
+			expectedPrefix: "cosmos",
+		},
+		{
+			name: "ok: name exists with different prefix",
+			opts: []cosmosclient.Option{
+				cosmosclient.WithAddressPrefix("test"),
+			},
+			accountName:    expectedAccount.Name,
+			expectedPrefix: "test",
 		},
 		{
 			name:          "fail: name not found",
@@ -343,7 +220,7 @@ func TestClientAddress(t *testing.T) {
 			var (
 				require = require.New(t)
 				assert  = assert.New(t)
-				c       = newClient(t, nil)
+				c       = newClient(t, nil, tt.opts...)
 			)
 			_, err := c.AccountRegistry.Import(accountName, key, passphrase)
 			require.NoError(err)
@@ -355,7 +232,7 @@ func TestClientAddress(t *testing.T) {
 				return
 			}
 			require.NoError(err)
-			expectedAddr, err := expectedAccount.Address("cosmos")
+			expectedAddr, err := expectedAccount.Address(tt.expectedPrefix)
 			require.NoError(err)
 			assert.Equal(expectedAddr, address)
 		})
