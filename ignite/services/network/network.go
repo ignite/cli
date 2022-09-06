5426a15b7b722fbec1d63e2f1e76b232834188ee
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-09-06 10:25:12 +0200

    fix: remove unused `monitoringProviderQuery` field from `Network`

--- ignite/services/network/network.go
+++ ignite/services/network/network.go
@@ -12,7 +12,6 @@ import (
 	campaigntypes "github.com/tendermint/spn/x/campaign/types"
 	launchtypes "github.com/tendermint/spn/x/launch/types"
 	monitoringctypes "github.com/tendermint/spn/x/monitoringc/types"
-	monitoringptypes "github.com/tendermint/spn/x/monitoringp/types"
 	profiletypes "github.com/tendermint/spn/x/profile/types"
 	rewardtypes "github.com/tendermint/spn/x/reward/types"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
@@ -43,7 +42,6 @@ type Network struct {
 	stakingQuery            stakingtypes.QueryClient
 	bankQuery               banktypes.QueryClient
 	monitoringConsumerQuery monitoringctypes.QueryClient
-	monitoringProviderQuery monitoringptypes.QueryClient
 }
 
 //go:generate mockery --name Chain --case underscore
