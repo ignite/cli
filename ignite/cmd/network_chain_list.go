6adfeb83e480ee45b319051fe425a6c82ffd51ce
ltacker <lucas.bertrand.22@gmail.com>
2022-09-22 12:56:26 +0200

    advanced flag

--- ignite/cmd/network_chain_list.go
+++ ignite/cmd/network_chain_list.go
@@ -2,6 +2,8 @@ package ignitecmd
 
 import (
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"time"
 
 	"github.com/spf13/cobra"
 
@@ -11,7 +13,18 @@ import (
 	"github.com/ignite/cli/ignite/services/network/networktypes"
 )
 
-var LaunchSummaryHeader = []string{"launch ID", "chain ID", "source", "campaign ID", "network", "reward"}
+var LaunchSummaryHeader = []string{
+	"launch ID",
+	"chain ID",
+	"source",
+	"phase",
+}
+
+var LaunchSummaryAdvancedHeader = []string{
+	"campaign ID",
+	"network",
+	"reward",
+}
 
 // NewNetworkChainList returns a new command to list all published chains on Ignite
 func NewNetworkChainList() *cobra.Command {
@@ -21,10 +34,20 @@ func NewNetworkChainList() *cobra.Command {
 		Args:  cobra.NoArgs,
 		RunE:  networkChainListHandler,
 	}
+	c.Flags().Bool(flagAdvanced, false, "Show advanced information about the chains")
+	c.Flags().Uint64(flagLimit, 100, "Limit of results per page")
+	c.Flags().Uint64(flagPage, 0, "Page for chain list result")
+
 	return c
 }
 
 func networkChainListHandler(cmd *cobra.Command, _ []string) error {
+	var (
+		advanced, _ = cmd.Flags().GetBool(flagAdvanced)
+		limit, _    = cmd.Flags().GetUint64(flagLimit)
+		page, _     = cmd.Flags().GetUint64(flagPage)
+	)
+
 	session := cliui.New()
 	defer session.Cleanup()
 
@@ -36,40 +59,70 @@ func networkChainListHandler(cmd *cobra.Command, _ []string) error {
 	if err != nil {
 		return err
 	}
-	chainLaunches, err := n.ChainLaunchesWithReward(cmd.Context())
+	chainLaunches, err := n.ChainLaunchesWithReward(cmd.Context(), &query.PageRequest{
+		Offset: limit * page,
+		Limit:  limit,
+	})
 	if err != nil {
 		return err
 	}
 
 	session.StopSpinner()
 
-	return renderLaunchSummaries(chainLaunches, session)
+	return renderLaunchSummaries(chainLaunches, session, advanced)
 }
 
 // renderLaunchSummaries writes into the provided out, the list of summarized launches
-func renderLaunchSummaries(chainLaunches []networktypes.ChainLaunch, session cliui.Session) error {
+func renderLaunchSummaries(chainLaunches []networktypes.ChainLaunch, session cliui.Session, advanced bool) error {
+	header := LaunchSummaryHeader
+	if advanced {
+		// advanced information show the campaign ID, type of network and rewards for incentivized testnet
+		header = append(header, LaunchSummaryAdvancedHeader...)
+	}
+
 	var launchEntries [][]string
 
+	// iterate and fetch summary for chains
 	for _, c := range chainLaunches {
-		campaign := "no campaign"
-		if c.CampaignID > 0 {
-			campaign = fmt.Sprintf("%d", c.CampaignID)
+
+		// get the current phase of the chain
+		var phase string
+		switch {
+		case !c.LaunchTriggered:
+			phase = "coordinating"
+		case time.Now().Before(c.LaunchTime):
+			phase = "launching"
+		default:
+			phase = "launched"
 		}
 
-		reward := entrywriter.None
-		if len(c.Reward) > 0 {
-			reward = c.Reward
-		}
-
-		launchEntries = append(launchEntries, []string{
+		entry := []string{
 			fmt.Sprintf("%d", c.ID),
 			c.ChainID,
 			c.SourceURL,
-			campaign,
-			c.Network.String(),
-			reward,
-		})
+			phase,
+		}
+
+		// add advanced information
+		if advanced {
+			campaign := "no campaign"
+			if c.CampaignID > 0 {
+				campaign = fmt.Sprintf("%d", c.CampaignID)
+			}
+
+			reward := entrywriter.None
+			if len(c.Reward) > 0 {
+				reward = c.Reward
+			}
+
+			entry = append(entry,
+				campaign,
+				c.Network.String(),
+				reward)
+		}
+
+		launchEntries = append(launchEntries, entry)
 	}
 
-	return session.PrintTable(LaunchSummaryHeader, launchEntries...)
+	return session.PrintTable(header, launchEntries...)
 }
