a9530843e8a8983dab4d478148215342854e35fd
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-09-29 14:57:57 +0200

    fix: change `pkg/yaml` to convert keys in slice of map
    This is required to make sure the values read from YAML files can be
    merged and serialized to other formats like JSON.

--- ignite/pkg/yaml/map.go
+++ ignite/pkg/yaml/map.go
@@ -17,12 +17,33 @@ func (m *Map) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	return nil
 }
 
+func convertSlice(raw []interface{}) []interface{} {
+	if len(raw) == 0 {
+		return raw
+	}
+
+	if _, ok := raw[0].(map[interface{}]interface{}); !ok {
+		return raw
+	}
+
+	values := make([]interface{}, len(raw))
+	for i, v := range raw {
+		values[i] = convertMapKeys(v.(map[interface{}]interface{}))
+	}
+
+	return values
+}
+
 func convertMapKeys(raw map[interface{}]interface{}) map[string]interface{} {
 	m := make(map[string]interface{})
 
 	for k, v := range raw {
-		if value, ok := v.(map[interface{}]interface{}); ok {
+		if value, _ := v.(map[interface{}]interface{}); value != nil {
+			// Convert map keys to string
 			v = convertMapKeys(value)
+		} else if values, _ := v.([]interface{}); values != nil {
+			// Make sure that maps inside slices also use strings as key
+			v = convertSlice(values)
 		}
 
 		m[k.(string)] = v
