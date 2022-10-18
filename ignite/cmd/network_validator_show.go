7996b0a3ea0ea528e9558542defcd47c4cd6f1b5
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2022-10-18 23:23:43 +0200

    fix(`network`): spinner issue for validator and coordinator commands (#2939)
    * fix 1
    
    * fix 2
    
    * format
    
    * remove unnecessary
    
    * imports
    
    Co-authored-by: Alex Johnson <alex@shmeeload.xyz>

--- ignite/cmd/network_validator_show.go
+++ ignite/cmd/network_validator_show.go
@@ -19,7 +19,7 @@ func NewNetworkValidatorShow() *cobra.Command {
 }
 
 func networkValidatorShowHandler(cmd *cobra.Command, args []string) error {
-	session := cliui.New()
+	session := cliui.New(cliui.StartSpinner())
 	defer session.End()
 
 	nb, err := newNetworkBuilder(cmd, CollectEvents(session.EventBus()))
