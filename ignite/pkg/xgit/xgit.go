ef0bb8be2bdef6b3473e1949c923d9e79e3a61ae
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2022-11-04 14:28:12 +0100

    fix: InitAndCommit doesn't create repo if already exists

--- ignite/pkg/xgit/xgit.go
+++ ignite/pkg/xgit/xgit.go
@@ -1,12 +1,12 @@
 package xgit
 
 import (
+	"fmt"
 	"path/filepath"
 	"time"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/pkg/errors"
 )
 
 var (
@@ -18,23 +18,44 @@ var (
 	}
 )
 
+// InitAndCommit creates a git repo in path if path isn't already inside a git
+// repository, then commits path content.
 func InitAndCommit(path string) error {
-	repo, err := git.PlainInit(path, false)
+	repo, err := git.PlainOpenWithOptions(path, &git.PlainOpenOptions{
+		DetectDotGit: true,
+	})
 	if err != nil {
-		return errors.WithStack(err)
+		if err != git.ErrRepositoryNotExists {
+			return fmt.Errorf("open git repo %s: %w", path, err)
+		}
+		// not a git repo, creates a new one
+		repo, err = git.PlainInit(path, false)
+		if err != nil {
+			return fmt.Errorf("init git repo %s: %w", path, err)
+		}
 	}
 	wt, err := repo.Worktree()
 	if err != nil {
-		return errors.WithStack(err)
+		return fmt.Errorf("worktree %s: %w", path, err)
 	}
-	if _, err := wt.Add("."); err != nil {
-		return errors.WithStack(err)
+	// wt.Add(path) takes only relative path, we need to turn path relative to
+	// repo path.
+	repoPath := wt.Filesystem.Root()
+	path, err = filepath.Rel(repoPath, path)
+	if err != nil {
+		return fmt.Errorf("find relative path %s %s: %w", repoPath, path, err)
+	}
+	if _, err := wt.Add(path); err != nil {
+		return fmt.Errorf("git add %s: %w", path, err)
 	}
 	_, err = wt.Commit(commitMsg, &git.CommitOptions{
 		All:    true,
 		Author: devXAuthor,
 	})
-	return errors.WithStack(err)
+	if err != nil {
+		return fmt.Errorf("git commit %s: %w", path, err)
+	}
+	return nil
 }
 
 func AreChangesCommitted(appPath string) (bool, error) {
