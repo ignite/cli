1a3167e636ebd41b068c78dfbb2d97767e6ea815
Alex Johnson <alex@shmeeload.xyz>
2022-11-28 08:16:18 -0500

    Update ignite/services/network/networkchain/init.go
    Co-authored-by: Jerónimo Albi <jeronimo.albi@tendermint.com>

--- ignite/services/network/networkchain/init.go
+++ ignite/services/network/networkchain/init.go
@@ -105,7 +105,7 @@ func (c *Chain) initGenesis(ctx context.Context) error {
 			return err
 		}
 	case c.genesisConfig != "":
-		c.ev.Send("Fetching custom Genesis from ChainConfig", events.ProgressUpdate())
+		c.ev.Send("Fetching custom genesis from chain config", events.ProgressUpdate())
 
 		// first, initialize with default genesis
 		cmd, err := c.chain.Commands(ctx)
