e2d5f56c5a68d5ba3aeabd6c46cf00e1192797b5
aljo242 <alex@shmeeload.xyz>
2022-11-30 07:59:54 -0500

    testdata

--- ignite/config/testdata/testdata.go
+++ ignite/config/testdata/testdata.go
@@ -3,14 +3,11 @@ package testdata
 import (
 	"testing"
 
-	pluginsconfig "github.com/ignite/cli/ignite/config/plugins"
-
 	"github.com/ignite/cli/ignite/config"
 	chainconfig "github.com/ignite/cli/ignite/config/chain"
 	networkconfigTestdata "github.com/ignite/cli/ignite/config/chain/network/testdata"
 	v0testdata "github.com/ignite/cli/ignite/config/chain/v0/testdata"
 	v1testdata "github.com/ignite/cli/ignite/config/chain/v1/testdata"
-	pluginsconfigTestdata "github.com/ignite/cli/ignite/config/plugins/testdata"
 )
 
 var Versions = map[chainconfig.Version][]byte{
@@ -20,16 +17,10 @@ var Versions = map[chainconfig.Version][]byte{
 
 var NetworkConfig = networkconfigTestdata.ConfigYAML
 
-var PluginsConfig = pluginsconfigTestdata.ConfigYAML
-
 func GetLatestConfig(t *testing.T) *config.ChainConfig {
 	return v1testdata.GetConfig(t)
 }
 
 func GetLatestNetworkConfig(t *testing.T) *config.ChainConfig {
 	return networkconfigTestdata.GetConfig(t)
-}
-
-func GetPluginsConfig(t *testing.T) *pluginsconfig.Config {
-	return pluginsconfigTestdata.GetConfig(t)
 }
