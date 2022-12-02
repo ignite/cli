b3366abf1bc157e7dc3b7677cdece4d40c7130b3
jeronimoalbi <jeronimo.albi@tendermint.com>
2022-12-02 14:43:47 +0100

    feat: add `debugger` package

---
+++ ignite/pkg/debugger/server.go
@@ -0,0 +1,191 @@
+package debugger
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/go-delve/delve/pkg/logflags"
+	"github.com/go-delve/delve/pkg/terminal"
+	"github.com/go-delve/delve/service"
+	"github.com/go-delve/delve/service/debugger"
+	"github.com/go-delve/delve/service/rpc2"
+	"github.com/go-delve/delve/service/rpccommon"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	// DefaultAddress defines the default debugger server address.
+	DefaultAddress = "127.0.0.1:30500"
+
+	// DefaultWorkingDir defines the default directory to use as
+	// working dir when running the app binary that will be debugged.
+	DefaultWorkingDir = "."
+)
+
+// Options configures the debugger server.
+type Option func(*debuggerOptions)
+
+type debuggerOptions struct {
+	disconnectChan      chan struct{}
+	address, workingDir string
+	listener            net.Listener
+	binaryArgs          []string
+	clientRunHook       func()
+	serverStartHook     func(address string)
+}
+
+// Address sets the address for the debugger server.
+func Address(address string) Option {
+	return func(o *debuggerOptions) {
+		o.address = address
+	}
+}
+
+// DisconnectChannel sets the channel used by the server to signal when the client disconnects.
+func DisconnectChannel(c chan struct{}) Option {
+	return func(o *debuggerOptions) {
+		o.disconnectChan = c
+	}
+}
+
+// Listener sets a custom listener to serve requests.
+func Listener(l net.Listener) Option {
+	return func(o *debuggerOptions) {
+		o.listener = l
+	}
+}
+
+// WorkingDir sets the working directory of the new process.
+func WorkingDir(path string) Option {
+	return func(o *debuggerOptions) {
+		o.workingDir = path
+	}
+}
+
+// BinaryArgs sets command line argument for the new process.
+func BinaryArgs(args ...string) Option {
+	return func(o *debuggerOptions) {
+		o.binaryArgs = args
+	}
+}
+
+// ClientRunHook sets a function to be executed right before debugger client is run.
+func ClientRunHook(fn func()) Option {
+	return func(o *debuggerOptions) {
+		o.clientRunHook = fn
+	}
+}
+
+// ServerStartHook sets a function to be executed right before debugger server starts.
+func ServerStartHook(fn func(address string)) Option {
+	return func(o *debuggerOptions) {
+		o.serverStartHook = fn
+	}
+}
+
+// Start starts a debugger server.
+func Start(ctx context.Context, binaryPath string, options ...Option) (err error) {
+	o := applyDebuggerOptions(options...)
+
+	listener := o.listener
+	if listener == nil {
+		var c net.ListenConfig
+
+		listener, err = c.Listen(ctx, "tcp", o.address)
+		if err != nil {
+			return err
+		}
+
+		defer listener.Close()
+	}
+
+	if err = disableDelveLogging(); err != nil {
+		return err
+	}
+
+	dc := o.disconnectChan
+	if dc == nil {
+		dc = make(chan struct{})
+	}
+
+	server := rpccommon.NewServer(&service.Config{
+		Listener:           listener,
+		AcceptMulti:        false,
+		APIVersion:         2,
+		CheckLocalConnUser: true,
+		DisconnectChan:     dc,
+		ProcessArgs:        append([]string{binaryPath}, o.binaryArgs...),
+		Debugger: debugger.Config{
+			WorkingDir: o.workingDir,
+			Backend:    "default",
+		},
+	})
+
+	if o.serverStartHook != nil {
+		o.serverStartHook(o.address)
+	}
+
+	if err = server.Run(); err != nil {
+		return fmt.Errorf("failed to run debugger server: %w", err)
+	}
+
+	defer server.Stop()
+
+	// Wait until the context is done or the connected client disconnects
+	select {
+	case <-ctx.Done():
+	case <-dc:
+	}
+
+	return nil
+}
+
+// Run runs a debugger client.
+func Run(ctx context.Context, binaryPath string, options ...Option) error {
+	listener, conn := service.ListenerPipe()
+	defer listener.Close()
+
+	o := applyDebuggerOptions(options...)
+
+	options = append(options, Listener(listener))
+	g, ctx := errgroup.WithContext(ctx)
+
+	// Start the debbugger server
+	g.Go(func() error {
+		return Start(ctx, binaryPath, options...)
+	})
+
+	// Start the debugger client
+	g.Go(func() error {
+		client := rpc2.NewClientFromConn(conn)
+		term := terminal.New(client, nil)
+
+		if o.clientRunHook != nil {
+			o.clientRunHook()
+		}
+
+		_, err := term.Run()
+		return err
+	})
+
+	return g.Wait()
+}
+
+func applyDebuggerOptions(options ...Option) debuggerOptions {
+	o := debuggerOptions{
+		address:    DefaultAddress,
+		workingDir: DefaultWorkingDir,
+	}
+	for _, apply := range options {
+		apply(&o)
+	}
+	return o
+}
+
+func disableDelveLogging() error {
+	if err := logflags.Setup(false, "", ""); err != nil {
+		return err
+	}
+	return nil
+}
