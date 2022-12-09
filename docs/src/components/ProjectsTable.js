f6297f744c9fc0d939f68f74d2618f12b10baac1
Denis Fadeev <denis@fadeev.org>
2022-12-09 17:59:54 +0400

    fix: inverted logos for dark theme

--- docs/src/components/ProjectsTable.js
+++ docs/src/components/ProjectsTable.js
@@ -6,7 +6,7 @@ const GridItem = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  box-shadow: 1px 1px #efefef;
+  box-shadow: 1px 1px var(--ifm-color-emphasis-200);
   font-weight: 500;
   padding: 1rem;
   text-align: center;
@@ -16,25 +16,28 @@ const Grid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   border-collapse: collapse;
-  ${GridItem}:nth-child(3n) {
-    box-shadow: 0 1px #efefef;
+  position: relative;
+  &:after {
+    width: 100%;
+    height: 1px;
+    content: "";
+    bottom: -1px;
+    background: var(--ifm-background-color);
+    position: absolute;
   }
-  ${GridItem}:nth-last-child(1) {
-    box-shadow: 1px 0px #efefef;
+  ${GridItem}:nth-child(3n) {
+    box-shadow: 0 1px var(--ifm-color-emphasis-200);
+  }
+  html[data-theme="dark"] & img {
+    filter: invert(1);
   }
   @media (max-width: 500px) {
     grid-template-columns: 1fr 1fr;
     ${GridItem}:nth-child(3n) {
-      box-shadow: 1px 1px #efefef;
+      box-shadow: 1px 1px var(--ifm-color-emphasis-200);
     }
     ${GridItem}:nth-child(2n) {
-      box-shadow: 0 1px #efefef;
-    }
-    ${GridItem}:nth-last-child(1) {
-      box-shadow: 0px 0px #efefef;
-    }
-    ${GridItem}:nth-last-child(2) {
-      box-shadow: 1px 0px #efefef;
+      box-shadow: 0 1px var(--ifm-color-emphasis-200);
     }
   }
 `;
