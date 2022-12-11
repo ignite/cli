66bea2bebaf310382e3405d4505d7448200c5f0e
joshLong145 <jlong2@ramapo.edu>
2022-12-11 12:25:54 -0500

    additional plugin cache tests

--- ignite/services/plugin/cache_test.go
+++ ignite/services/plugin/cache_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/ignite/cli/ignite/services/plugin"
 )
 
-func TestNewPlugin(t *testing.T) {
+func TestPluginCacheAdd(t *testing.T) {
 	const path = "/path/to/awesome/plugin"
 	unixFD, _ := net.ResolveUnixAddr("unix", "/var/folders/5k/sv4bxrs102n_6rr7430jc7j80000gn/T/plugin193424090")
 
@@ -27,11 +27,50 @@ func TestNewPlugin(t *testing.T) {
 	c := hplugin.ReattachConfig{}
 	err = plugin.ReadPluginConfig(path, &c)
 	require.NoError(t, err)
+}
+
+func TestPluginCacheDelete(t *testing.T) {
+	const path = "/path/to/awesome/plugin"
+	unixFD, _ := net.ResolveUnixAddr("unix", "/var/folders/5k/sv4bxrs102n_6rr7430jc7j80000gn/T/plugin193424090")
+
+	rc := hplugin.ReattachConfig{
+		Protocol:        hplugin.ProtocolNetRPC,
+		ProtocolVersion: hplugin.CoreProtocolVersion,
+		Addr:            unixFD,
+		Pid:             24464,
+	}
+
+	err := plugin.WritePluginConfig(path, rc)
+	require.NoError(t, err)
 
 	err = plugin.DeletePluginConf(path)
 	require.NoError(t, err)
 
+	c := hplugin.ReattachConfig{}
 	// there should be an error after deleting the config from the cache
 	err = plugin.ReadPluginConfig(path, &c)
 	require.Error(t, err)
+}
+
+func TestPluginCacheCheck(t *testing.T) {
+	const path = "/path/to/awesome/plugin"
+	unixFD, _ := net.ResolveUnixAddr("unix", "/var/folders/5k/sv4bxrs102n_6rr7430jc7j80000gn/T/plugin193424090")
+
+	rc := hplugin.ReattachConfig{
+		Protocol:        hplugin.ProtocolNetRPC,
+		ProtocolVersion: hplugin.CoreProtocolVersion,
+		Addr:            unixFD,
+		Pid:             24464,
+	}
+
+	t.Run("Cache should be hydrated", func(t *testing.T) {
+		err := plugin.WritePluginConfig(path, rc)
+		require.NoError(t, err)
+		require.Equal(t, true, plugin.CheckPluginConf(path))
+	})
+
+	t.Run("Cache should be empty", func(t *testing.T) {
+		_ = plugin.DeletePluginConf(path)
+		require.Equal(t, false, plugin.CheckPluginConf(path))
+	})
 }
