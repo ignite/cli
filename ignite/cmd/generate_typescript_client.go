7fca5ad27af331e33534de516216242153e91125
Jerónimo Albi <jeronimo.albi@tendermint.com>
2023-01-04 16:31:43 +0100

    refactor: change `generate ts-client` to ignore the cache by default (#3331)
    Co-authored-by: Alex Johnson <alex@shmeeload.xyz>

--- ignite/cmd/generate_typescript_client.go
+++ ignite/cmd/generate_typescript_client.go
@@ -8,16 +8,15 @@ import (
 	"github.com/ignite/cli/ignite/services/chain"
 )
 
+const (
+	flagUseCache = "use-cache"
+)
+
 func NewGenerateTSClient() *cobra.Command {
 	c := &cobra.Command{
 		Use:   "ts-client",
 		Short: "TypeScript frontend client",
 		Long: `Generate a framework agnostic TypeScript client for your blockchain project.
-
-To generate all clients for custom and standard Cosmos SDK modules, run this
-command:
-
-	ignite generate ts-client --clear-cache
 
 By default the TypeScript client is generated in the "ts-client/" directory. You
 can customize the output directory in config.yml:
@@ -25,6 +24,10 @@ can customize the output directory in config.yml:
 	client:
 	  typescript:
 	    path: new-path
+
+Output can also be customized by using a flag:
+
+	ignite generate ts-client --output new-path
 
 TypeScript client code can be automatically regenerated on reset or source code
 changes when the blockchain is started with a flag:
@@ -37,6 +40,7 @@ changes when the blockchain is started with a flag:
 
 	c.Flags().AddFlagSet(flagSetYes())
 	c.Flags().StringP(flagOutput, "o", "", "TypeScript client output path")
+	c.Flags().Bool(flagUseCache, false, "use build cache to speed-up generation")
 
 	return c
 }
@@ -65,7 +69,12 @@ func generateTSClientHandler(cmd *cobra.Command, _ []string) error {
 		return err
 	}
 
-	err = c.Generate(cmd.Context(), cacheStorage, chain.GenerateTSClient(output))
+	useCache, err := cmd.Flags().GetBool(flagUseCache)
+	if err != nil {
+		return err
+	}
+
+	err = c.Generate(cmd.Context(), cacheStorage, chain.GenerateTSClient(output, useCache))
 	if err != nil {
 		return err
 	}
