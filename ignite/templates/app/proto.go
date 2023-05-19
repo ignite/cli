ba565015669e425ee51031a8b510dfa66c257736
Danilo Pantani <danpantani@gmail.com>
2023-05-19 18:29:00 +0200

    remove unused ctx

--- ignite/templates/app/proto.go
+++ ignite/templates/app/proto.go
@@ -4,10 +4,8 @@ import (
 	"embed"
 
 	"github.com/gobuffalo/genny/v2"
-	"github.com/gobuffalo/plush/v4"
 
 	"github.com/ignite/cli/ignite/pkg/xgenny"
-	"github.com/ignite/cli/ignite/templates/field/plushhelpers"
 )
 
 //go:embed files/proto/* files/buf.work.yaml
@@ -15,14 +13,13 @@ var fsProto embed.FS
 
 // NewBufGenerator returns the generator to buf build files.
 func NewBufGenerator(appPath string) (*genny.Generator, error) {
-	g := genny.New()
-	ctx := plush.NewContext()
-	plushhelpers.ExtendPlushContext(ctx)
-	g.Transformer(xgenny.Transformer(ctx))
-	protoTemplate := xgenny.NewEmbedWalker(
-		fsProto,
-		"files",
-		appPath,
+	var (
+		g        = genny.New()
+		template = xgenny.NewEmbedWalker(
+			fsProto,
+			"files",
+			appPath,
+		)
 	)
-	return g, xgenny.Box(g, protoTemplate)
+	return g, xgenny.Box(g, template)
 }
