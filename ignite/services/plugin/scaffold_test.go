b56d445992a704bd6e7095947df407e26762a446
jeronimoalbi <jeronimo.albi@gmail.com>
2023-06-08 12:54:29 +0200

    test: improve plugin scaffold test

--- ignite/services/plugin/scaffold_test.go
+++ ignite/services/plugin/scaffold_test.go
@@ -2,16 +2,23 @@ package plugin
 
 import (
 	"context"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestScaffold(t *testing.T) {
+	// Arrange
 	tmp := t.TempDir()
+	ctx := context.Background()
 
-	path, err := Scaffold(context.Background(), tmp, "github.com/foo/bar", false)
+	// Act
+	path, err := Scaffold(ctx, tmp, "github.com/foo/bar", false)
 
+	// Assert
 	require.NoError(t, err)
 	require.DirExists(t, path)
+	require.FileExists(t, filepath.Join(path, "go.mod"))
+	require.FileExists(t, filepath.Join(path, "main.go"))
 }
