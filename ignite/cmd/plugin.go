68c65ba87361a582767f9e3bc1fe5ea9e9588d2c
jeronimoalbi <jeronimo.albi@gmail.com>
2023-06-13 13:32:03 +0200

    refactor: change plugin command to add analizer to calls

--- ignite/cmd/plugin.go
+++ ignite/cmd/plugin.go
@@ -206,7 +206,8 @@ func linkPluginHook(rootCmd *cobra.Command, p *plugin.Plugin, hook *plugin.Hook)
 			}
 		}
 
-		err := p.Interface.ExecuteHookPre(ctx, newExecutedHook(hook, cmd, args))
+		execHook := newExecutedHook(hook, cmd, args)
+		err := p.Interface.ExecuteHookPre(ctx, execHook, plugin.NewAnalizer())
 		if err != nil {
 			return fmt.Errorf("plugin %q ExecuteHookPre() error: %w", p.Path, err)
 		}
@@ -221,7 +222,8 @@ func linkPluginHook(rootCmd *cobra.Command, p *plugin.Plugin, hook *plugin.Hook)
 			// if the command has failed the `PostRun` will not execute. here we execute the cleanup step before returnning.
 			if err != nil {
 				ctx := cmd.Context()
-				err := p.Interface.ExecuteHookCleanUp(ctx, newExecutedHook(hook, cmd, args))
+				execHook := newExecutedHook(hook, cmd, args)
+				err := p.Interface.ExecuteHookCleanUp(ctx, execHook, plugin.NewAnalizer())
 				if err != nil {
 					fmt.Printf("plugin %q ExecuteHookCleanUp() error: %v", p.Path, err)
 				}
@@ -239,7 +241,7 @@ func linkPluginHook(rootCmd *cobra.Command, p *plugin.Plugin, hook *plugin.Hook)
 		execHook := newExecutedHook(hook, cmd, args)
 
 		defer func() {
-			err := p.Interface.ExecuteHookCleanUp(ctx, execHook)
+			err := p.Interface.ExecuteHookCleanUp(ctx, execHook, plugin.NewAnalizer())
 			if err != nil {
 				fmt.Printf("plugin %q ExecuteHookCleanUp() error: %v", p.Path, err)
 			}
@@ -253,7 +255,7 @@ func linkPluginHook(rootCmd *cobra.Command, p *plugin.Plugin, hook *plugin.Hook)
 			}
 		}
 
-		err := p.Interface.ExecuteHookPost(ctx, execHook)
+		err := p.Interface.ExecuteHookPost(ctx, execHook, plugin.NewAnalizer())
 		if err != nil {
 			return fmt.Errorf("plugin %q ExecuteHookPost() error : %w", p.Path, err)
 		}
@@ -325,7 +327,7 @@ func linkPluginCmd(rootCmd *cobra.Command, p *plugin.Plugin, pluginCmd *plugin.C
 				}
 				execCmd.ImportFlags(cmd)
 				// Call the plugin Execute
-				err := p.Interface.Execute(ctx, execCmd)
+				err := p.Interface.Execute(ctx, execCmd, plugin.NewAnalizer())
 				// NOTE(tb): This pause gives enough time for go-plugin to sync the
 				// output from stdout/stderr of the plugin. Without that pause, this
 				// output can be discarded and not printed in the user console.
