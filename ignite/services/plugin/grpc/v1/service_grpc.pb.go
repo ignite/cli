51efa6d9248903899ac4a441c203aa8c06a696cd
Clockwork <alex@clockwork.gr>
2023-06-23 14:13:32 +0300

    refactor: rename proto files and rebuild

--- ignite/services/plugin/grpc/v1/service_grpc.pb.go
+++ ignite/services/plugin/grpc/v1/service_grpc.pb.go
@@ -8,7 +8,6 @@ package v1
 
 import (
 	context "context"
-
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
@@ -138,24 +137,21 @@ type InterfaceServiceServer interface {
 }
 
 // UnimplementedInterfaceServiceServer must be embedded to have forward compatible implementations.
-type UnimplementedInterfaceServiceServer struct{}
+type UnimplementedInterfaceServiceServer struct {
+}
 
 func (UnimplementedInterfaceServiceServer) Manifest(context.Context, *ManifestRequest) (*ManifestResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Manifest not implemented")
 }
-
 func (UnimplementedInterfaceServiceServer) Execute(context.Context, *ExecuteRequest) (*ExecuteResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Execute not implemented")
 }
-
 func (UnimplementedInterfaceServiceServer) ExecuteHookPre(context.Context, *ExecuteHookPreRequest) (*ExecuteHookPreResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ExecuteHookPre not implemented")
 }
-
 func (UnimplementedInterfaceServiceServer) ExecuteHookPost(context.Context, *ExecuteHookPostRequest) (*ExecuteHookPostResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ExecuteHookPost not implemented")
 }
-
 func (UnimplementedInterfaceServiceServer) ExecuteHookCleanUp(context.Context, *ExecuteHookCleanUpRequest) (*ExecuteHookCleanUpResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ExecuteHookCleanUp not implemented")
 }
@@ -333,7 +329,8 @@ type ClientAPIServiceServer interface {
 }
 
 // UnimplementedClientAPIServiceServer must be embedded to have forward compatible implementations.
-type UnimplementedClientAPIServiceServer struct{}
+type UnimplementedClientAPIServiceServer struct {
+}
 
 func (UnimplementedClientAPIServiceServer) Dependencies(context.Context, *DependenciesRequest) (*DependenciesResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Dependencies not implemented")
