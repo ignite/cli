0891ffec63fb9ced4abde75e6824057e88c9df41
codelover <62229912+DedicatedDev@users.noreply.github.com>
2023-08-10 13:55:44 +0300

    chore: uin64 overflow fix (#3610)
    * chore: uin64 overflow fix
    
    * chore: uin64 overflow fix
    
    * chore: fix memory nil point problem
    
    * chore: migrate with sdkmath
    
    * chore: replae sdk.Int to sdkmath.Int
    
    * chore: add change log
    
    * chore: remove deprecated sdk.Int
    
    ---------
    
    Co-authored-by: Free Bird <freebird@Frees-iMac-Pro.local>
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>

--- ignite/pkg/cosmosfaucet/transfer.go
+++ ignite/pkg/cosmosfaucet/transfer.go
@@ -7,8 +7,8 @@ import (
 	"sync"
 	"time"
 
+	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
 	chaincmdrunner "github.com/ignite/cli/ignite/pkg/chaincmd/runner"
 )
 
@@ -16,32 +16,32 @@ import (
 var transferMutex = &sync.Mutex{}
 
 // TotalTransferredAmount returns the total transferred amount from faucet account to toAccountAddress.
-func (f Faucet) TotalTransferredAmount(ctx context.Context, toAccountAddress, denom string) (totalAmount uint64, err error) {
+func (f Faucet) TotalTransferredAmount(ctx context.Context, toAccountAddress, denom string) (totalAmount sdkmath.Int, err error) {
 	fromAccount, err := f.runner.ShowAccount(ctx, f.accountName)
 	if err != nil {
-		return 0, err
+		return sdkmath.NewInt(0), err
 	}
 
 	events, err := f.runner.QueryTxEvents(ctx,
 		chaincmdrunner.NewEventSelector("message", "sender", fromAccount.Address),
 		chaincmdrunner.NewEventSelector("transfer", "recipient", toAccountAddress))
 	if err != nil {
-		return 0, err
+		return sdkmath.NewInt(0), err
 	}
 
+	totalAmount = sdkmath.NewInt(0)
 	for _, event := range events {
 		if event.Type == "transfer" {
 			for _, attr := range event.Attributes {
 				if attr.Key == "amount" {
 					coins, err := sdk.ParseCoinsNormalized(attr.Value)
 					if err != nil {
-						return 0, err
+						return sdkmath.NewInt(0), err
 					}
 
-					amount := coins.AmountOf(denom).Uint64()
-
-					if amount > 0 && time.Since(event.Time) < f.limitRefreshWindow {
-						totalAmount += amount
+					amount := coins.AmountOf(denom)
+					if amount.GT(sdkmath.NewInt(0)) && time.Since(event.Time) < f.limitRefreshWindow {
+						totalAmount = totalAmount.Add(amount)
 					}
 				}
 			}
@@ -64,21 +64,21 @@ func (f *Faucet) Transfer(ctx context.Context, toAccountAddress string, coins sd
 		if err != nil {
 			return err
 		}
-
-		if f.coinsMax[c.Denom] != 0 {
-			if totalSent >= f.coinsMax[c.Denom] {
+		coinMax, found := f.coinsMax[c.Denom]
+		if found && !coinMax.IsNil() && !coinMax.Equal(sdkmath.NewInt(0)) {
+			if totalSent.GTE(coinMax) {
 				return fmt.Errorf(
 					"account has reached to the max. allowed amount (%d) for %q denom",
-					f.coinsMax[c.Denom],
+					coinMax,
 					c.Denom,
 				)
 			}
 
-			if (totalSent + c.Amount.Uint64()) > f.coinsMax[c.Denom] {
+			if (totalSent.Add(c.Amount)).GT(coinMax) {
 				return fmt.Errorf(
 					`ask less amount for %q denom. account is reaching to the limit (%d) that faucet can tolerate`,
 					c.Denom,
-					f.coinsMax[c.Denom],
+					coinMax,
 				)
 			}
 		}
