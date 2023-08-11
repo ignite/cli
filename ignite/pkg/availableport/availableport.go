fb3c06f307c4f40cfa7c618996adb1ac61ddd569
iamagf <agf0710@gmail.com>
2023-08-10 20:15:39 -0600

    Removing excessive conditions

--- ignite/pkg/availableport/availableport.go
+++ ignite/pkg/availableport/availableport.go
@@ -7,98 +7,64 @@ import (
 	"time"
 )
 
-type optionalParameters struct {
-	WithRandomizer *rand.Rand
-	WithMinPort    int
-	WithMaxPort    int
+type availablePortOptions struct {
+	randomizer *rand.Rand
+	minPort    uint
+	maxPort    uint
 }
 
-type OptionalParameters func(o *optionalParameters)
+type Options func(o *availablePortOptions)
 
-func WithRandomizer(r *rand.Rand) OptionalParameters {
-	return func(o *optionalParameters) {
-		o.WithRandomizer = r
+func WithRandomizer(r *rand.Rand) Options {
+	return func(o *availablePortOptions) {
+		o.randomizer = r
 	}
 }
 
-func WithMaxPort(maxPort int) OptionalParameters {
-	return func(o *optionalParameters) {
-		o.WithMaxPort = maxPort
+func WithMaxPort(maxPort uint) Options {
+	return func(o *availablePortOptions) {
+		o.maxPort = maxPort
 	}
 }
 
-func WithMinPort(minPort int) OptionalParameters {
-	return func(o *optionalParameters) {
-		o.WithMinPort = minPort
+func WithMinPort(minPort uint) Options {
+	return func(o *availablePortOptions) {
+		o.minPort = minPort
 	}
 }
 
 // Find finds n number of unused ports.
 // it is not guaranteed that these ports will not be allocated to
 // another program in the time of calling Find().
-func Find(n int, moreParameters ...OptionalParameters) (ports []int, err error) {
-	// Defining them before so we can set a value depending on the OptionalParameters
-	var min int
-	var max int
-	var r *rand.Rand
-
-	options := &optionalParameters{}
-	if len(moreParameters) != 0 {
-		opt := moreParameters[0]
-		opt(options)
-	} else {
-		// If we don't require special conditions, we can
-		// return to the original parameters
-		min = 44000
-		max = 55000
-		r = rand.New(rand.NewSource(time.Now().UnixNano()))
+func Find(n uint, options ...Options) (ports []uint, err error) {
+	// Defining them before so we can set a value depending on the AvailablePortOptions
+	opts := availablePortOptions{
+		minPort:    44000,
+		maxPort:    55000,
+		randomizer: rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
 
-	if options.WithMinPort != 0 {
-		if options.WithMinPort > -1 {
-			min = options.WithMinPort
-		} else {
-			// This is not required since the port would become 0
-			// but the user could not notice that sent a negative port
-			return nil, fmt.Errorf("ports can't be negative (negative min port given)")
-		}
-	} else {
-		min = 44000
+	for _, apply := range options {
+		apply(&opts)
 	}
-
-	if options.WithMaxPort != 0 {
-		if options.WithMaxPort > -1 {
-			max = options.WithMaxPort
-		} else {
-			// This is not required since the port would become 0
-			// but the user could not notice that sent a negative port
-			return nil, fmt.Errorf("ports can't be negative (negative max port given)")
-		}
-	} else {
-		max = 55000
-	}
-	if options.WithRandomizer != nil {
-		r = options.WithRandomizer
-	} else {
-		r = rand.New(rand.NewSource(time.Now().UnixNano()))
+	// If the number of ports required is bigger than the range, this stops it
+	if opts.maxPort < opts.minPort {
+		return nil, fmt.Errorf("invalid ports range: max < min (%d < %d)", opts.maxPort, opts.minPort)
 	}
 
 	// If the number of ports required is bigger than the range, this stops it
-	if max < min {
-		return nil, fmt.Errorf("invalid ports range: max < min (%d < %d)", max, min)
-	}
-
-	// If the number of ports required is bigger than the range, this stops it
-	if n > (max - min) {
-		return nil, fmt.Errorf("invalid amount of ports requested: limit is %d", max-min)
+	if n > (opts.maxPort - opts.minPort) {
+		return nil, fmt.Errorf("invalid amount of ports requested: limit is %d", opts.maxPort-opts.minPort)
 	}
 
 	// Marker to point if a port is already added in the list
-	registered := make(map[int]bool)
-	for i := 0; i < n; i++ {
+	registered := make(map[uint]bool)
+	for i := uint(0); i < n; i++ {
 		for {
 			// Greater or equal to min and lower than max
-			port := r.Intn(max-min+1) + min
+			totalPorts := opts.maxPort - opts.minPort + 1
+			randomPort := opts.randomizer.Intn(int(totalPorts))
+			port := uint(randomPort) + opts.minPort
 
 			conn, err := net.Dial("tcp", fmt.Sprintf(":%d", port))
 			// if there is an error, this might mean that no one is listening from this port
@@ -107,6 +73,8 @@ func Find(n int, moreParameters ...OptionalParameters) (ports []int, err error) 
 				conn.Close()
 				continue
 			}
+			defer conn.Close()
+
 			// if the port is already registered we skip it to the next one
 			// otherwise it's added to the ports list and pointed in our map
 			if registered[port] {
