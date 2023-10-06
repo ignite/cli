da17f3d35bdade5e9f0ca527b361d6eb8c4ffd6e
Pantani <Pantani>
2023-10-06 02:51:08 +0200

    fix wrong tx swagger generation

--- ignite/pkg/cosmosanalysis/module/message.go
+++ ignite/pkg/cosmosanalysis/module/message.go
@@ -3,9 +3,7 @@ package module
 // messageImplementation is the list of methods needed for a sdk.Msg implementation
 // TODO(low priority): dynamically get these from the source code of underlying version of the sdk.
 var messageImplementation = []string{
-	"Route",
-	"Type",
-	"GetSigners",
-	"GetSignBytes",
-	"ValidateBasic",
+	"Reset",
+	"Descriptor",
+	"ProtoMessage",
 }
