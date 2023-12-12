f26e49417d10769e76c76f3157fc4950b188d24a
Pantani <Pantani>
2023-12-12 18:30:48 -0300

    fix some lint issues

--- ignite/version/version.go
+++ ignite/version/version.go
@@ -191,7 +191,7 @@ func Long(ctx context.Context) string {
 // AssertSupportedCosmosSDKVersion asserts that a Cosmos SDK version is supported by Ignite CLI.
 func AssertSupportedCosmosSDKVersion(v cosmosver.Version) error {
 	if v.LT(cosmosver.StargateFortySevenTwoVersion) {
-		return errors.Errorf(errOldCosmosSDKVersion, v)
+		return errors.Errorf(errOldCosmosSDKVersionStr, v)
 	}
 	return nil
 }
