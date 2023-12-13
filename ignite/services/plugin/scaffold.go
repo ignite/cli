668677d5e3b2dbf0e63dca010a0a49f7251cffd4
Ehsan-saradar <ehsan.saradar@gmail.com>
2023-12-13 14:41:47 +0330

    Add go mod tidy and fmt to scaffolding apps

--- ignite/services/plugin/scaffold.go
+++ ignite/services/plugin/scaffold.go
@@ -14,6 +14,7 @@ import (
 	"golang.org/x/text/language"
 
 	"github.com/ignite/cli/v28/ignite/pkg/errors"
+	"github.com/ignite/cli/v28/ignite/pkg/gocmd"
 	"github.com/ignite/cli/v28/ignite/pkg/xgenny"
 )
 
@@ -24,7 +25,7 @@ var fsPluginSource embed.FS
 func Scaffold(ctx context.Context, dir, moduleName string, sharedHost bool) (string, error) {
 	var (
 		name     = filepath.Base(moduleName)
-		title    = strings.ReplaceAll(strings.ReplaceAll(cases.Title(language.English).String(name), "_", ""), "-", "")
+		title    = toTitle(name)
 		finalDir = path.Join(dir, name)
 		g        = genny.New()
 		template = xgenny.NewEmbedWalker(
@@ -60,5 +61,16 @@ func Scaffold(ctx context.Context, dir, moduleName string, sharedHost bool) (str
 		return "", errors.WithStack(err)
 	}
 
+	if err := gocmd.ModTidy(ctx, finalDir); err != nil {
+		return "", errors.WithStack(err)
+	}
+	if err := gocmd.Fmt(ctx, finalDir); err != nil {
+		return "", errors.WithStack(err)
+	}
+
 	return finalDir, nil
+}
+
+func toTitle(s string) string {
+	return strings.ReplaceAll(strings.ReplaceAll(cases.Title(language.English).String(s), "_", ""), "-", "")
 }
