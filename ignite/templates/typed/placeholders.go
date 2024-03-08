c8a677bab9a24eea2494cbe8ca13a18a9836024d
Pantani <Pantani>
2024-03-07 23:46:57 -0300

    remove unused placeholders

--- ignite/templates/typed/placeholders.go
+++ ignite/templates/typed/placeholders.go
@@ -3,7 +3,6 @@ package typed
 //nolint:godot
 const (
 	Placeholder  = "// this line is used by starport scaffolding # 1"
-	Placeholder2 = "// this line is used by starport scaffolding # 2"
 	Placeholder3 = "// this line is used by starport scaffolding # 3"
 	Placeholder4 = "<!-- this line is used by starport scaffolding # 4 -->"
 
