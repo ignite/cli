59400a520fb3fc4a67799a54df39705c91e4374a
Danilo Pantani <danpantani@gmail.com>
2024-03-08 16:55:03 -0300

    feat: remove all import placeholders using the `xast` pkg (#4004)
    * remove all import placeholders
    
    * add changelog
    
    * remove unused placeholders
    
    * fix wrong import path
    
    * improve readbility
    
    * remove keeper definition placeholder
    
    ---------
    
    Co-authored-by: Pantani <Pantani>

--- ignite/templates/ibc/placeholders.go
+++ ignite/templates/ibc/placeholders.go
@@ -2,21 +2,11 @@ package ibc
 
 //nolint:godot
 const (
-	Placeholder  = "// this line is used by starport scaffolding # 1"
-	Placeholder2 = "// this line is used by starport scaffolding # 2"
-	Placeholder3 = "// this line is used by starport scaffolding # 3"
+	Placeholder = "// this line is used by starport scaffolding # 1"
 
 	// Placeholders IBC packets
 	PlaceholderIBCPacketEvent         = "// this line is used by starport scaffolding # ibc/packet/event"
 	PlaceholderIBCPacketModuleRecv    = "// this line is used by starport scaffolding # ibc/packet/module/recv"
 	PlaceholderIBCPacketModuleAck     = "// this line is used by starport scaffolding # ibc/packet/module/ack"
 	PlaceholderIBCPacketModuleTimeout = "// this line is used by starport scaffolding # ibc/packet/module/timeout"
-
-	// Placeholders for messages
-	PlaceholderProtoTxRPC     = "// this line is used by starport scaffolding # proto/tx/rpc"
-	PlaceholderProtoTxMessage = "// this line is used by starport scaffolding # proto/tx/message"
-
-	// Placeholders AutoCLI
-	PlaceholderAutoCLIQuery = "// this line is used by ignite scaffolding # autocli/query"
-	PlaceholderAutoCLITx    = "// this line is used by ignite scaffolding # autocli/tx"
 )
