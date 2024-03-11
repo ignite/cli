d1b89436c36d49d009e39bde40a149b76fc996e5
Jerónimo Albi <jeronimo.albi@tendermint.com>
2024-03-11 21:49:56 +0100

    chore: rename `pkg/yaml` to `pkg/xyaml` (#4008)
    * chore: rename `pkg/yaml` to `pkg/xyaml`
    
    * chore: update changelog

--- ignite/config/chain/v1/validator.go
+++ ignite/config/chain/v1/validator.go
@@ -1,7 +1,7 @@
 package v1
 
 import (
-	xyaml "github.com/ignite/cli/v28/ignite/pkg/yaml"
+	"github.com/ignite/cli/v28/ignite/pkg/xyaml"
 )
 
 // Validator holds info related to validator settings.
