5ed963208767ab398cbc40e80897ebefd6ca4034
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2024-03-13 14:50:58 +0100

    feat: scaffold consumer chain (#3660)
    * feat: add validation kind in config
    
    Disable gentx generation when validation is consumer.
    
    * Add consumer chain plush scaffolding
    
    * update config.yml when scaffold consumer chain
    
    * Add hard-coded interchain-security require
    
    * remove comment
    
    * fix bad merge go.sum
    
    * update ibc to v8
    
    * fix changelog
    
    * chore: update interchain-security dependency
    
    Use the latest compatible with sdk50
    
    * update ccvconsumertypes -> ccvtypes
    
    * templates: ibc-go/v7 -> ibc-go/v8
    
    plus other dep updates
    
    * fix imports
    
    * fix imports paths
    
    * do not pass CapabilityKeeper in dep.Inject
    
    * Fix lint
    
    * fix: add missing ibcconsumer.AppModule (#3848)
    
    Co-authored-by: Pantani <Pantani>
    
    * remove ICS dep
    
    * wip exec plugin!
    
    * use plugin repo
    
    * restore templates/app/files w/o IsConsumerChain condition
    
    * create files-consumer alternate template folder
    
    * mark minimal and consumer flags as exclusive
    
    * fix wrong location for consumer_*.go files
    
    * fix error handling for IsInitialized
    
    * revert commit wip plugin exec
    
    * use plugin to read & write consumer module genesis
    
    * fix linter
    
    * backport NFT module #3924 in files-consumer
    
    * update app-consumer url
    
    * move app-consumer address to ignite org
    
    * update CL
    
    * use new plugin location
    
    * use merged version of consumer app
    
    * changelog
    
    * sync fixes
    
    * updates
    
    * fixes
    
    * changelog
    
    * feedback
    
    * fix linter
    
    * update ante handlers
    
    * import
    
    * updates
    
    ---------
    
    Co-authored-by: Ehsan-saradar <ehsan.saradar@gmail.com>
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    Co-authored-by: Julien Robert <julien@rbrt.fr>

--- ignite/services/scaffolder/module.go
+++ ignite/services/scaffolder/module.go
@@ -56,6 +56,7 @@ var (
 		"account":                    {},
 		"block":                      {},
 		"broadcast":                  {},
+		"consumer":                   {}, // ICS consumer module
 		"encode":                     {},
 		"multisign":                  {},
 		"sign":                       {},
