b647f095aa3fe08f8253c649698abb2116b492d3
Pantani <Pantani>
2024-05-15 01:31:58 +0200

    improve Test_extractRootModulePath test cases

--- ignite/pkg/cosmosgen/generate_openapi_test.go
+++ ignite/pkg/cosmosgen/generate_openapi_test.go
@@ -14,33 +14,38 @@ func Test_extractRootModulePath(t *testing.T) {
 	}{
 		{
 			name: "test cosmos-sdk path",
-			path: "/Users/danilopantani/Desktop/go/pkg/mod/github.com/cosmos/cosmos-sdk@v0.50.6/proto/cosmos/distribution/v1beta1",
-			want: "/Users/danilopantani/Desktop/go/pkg/mod/github.com/cosmos/cosmos-sdk@v0.50.6",
+			path: "/Users/ignite/Desktop/go/pkg/mod/github.com/cosmos/cosmos-sdk@v0.50.6/proto/cosmos/distribution/v1beta1",
+			want: "/Users/ignite/Desktop/go/pkg/mod/github.com/cosmos/cosmos-sdk@v0.50.6",
+		},
+		{
+			name: "test cosmos-sdk module proto path",
+			path: "/Users/ignite/Desktop/go/pkg/mod/github.com/cosmos/cosmos-sdk@v0.50.6/x/bank",
+			want: "/Users/ignite/Desktop/go/pkg/mod/github.com/cosmos/cosmos-sdk@v0.50.6",
 		},
 		{
 			name: "test ibc path",
-			path: "/Users/danilopantani/Desktop/go/pkg/mod/github.com/cosmos/ibc-go/v8@v8.2.0/proto/ibc/applications/interchain_accounts/controller/v1",
-			want: "/Users/danilopantani/Desktop/go/pkg/mod/github.com/cosmos/ibc-go/v8@v8.2.0",
+			path: "/Users/ignite/Desktop/go/pkg/mod/github.com/cosmos/ibc-go/v8@v8.2.0/proto/ibc/applications/interchain_accounts/controller/v1",
+			want: "/Users/ignite/Desktop/go/pkg/mod/github.com/cosmos/ibc-go/v8@v8.2.0",
 		},
 		{
 			name: "test chain path",
-			path: "/Users/danilopantani/Desktop/go/src/github.com/ignite/venus",
-			want: "/Users/danilopantani/Desktop/go/src/github.com/ignite/venus",
+			path: "/Users/ignite/Desktop/go/src/github.com/ignite/venus",
+			want: "/Users/ignite/Desktop/go/src/github.com/ignite/venus",
 		},
 		{
 			name: "test module path without version",
-			path: "/Users/danilopantani/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/proto/applications",
-			want: "/Users/danilopantani/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/proto/applications",
+			path: "/Users/ignite/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/proto/applications",
+			want: "/Users/ignite/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/proto/applications",
 		},
 		{
 			name: "test module path with broken version",
-			path: "/Users/danilopantani/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.$/controller",
-			want: "/Users/danilopantani/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.$/controller",
+			path: "/Users/ignite/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.$/controller",
+			want: "/Users/ignite/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.$/controller",
 		},
 		{
 			name: "test module path with v2 version",
-			path: "/Users/danilopantani/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/v2@v2.19.1/proto/files",
-			want: "/Users/danilopantani/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/v2@v2.19.1",
+			path: "/Users/ignite/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/v2@v2.19.1/proto/files",
+			want: "/Users/ignite/Desktop/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/v2@v2.19.1",
 		},
 	}
 	for _, tt := range tests {
