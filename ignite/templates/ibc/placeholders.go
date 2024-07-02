d7f71dda18a7f731bff7aa22dc1085f559a00b4f
Danilo Pantani <danpantani@gmail.com>
2024-07-02 16:17:04 +0200

    fix merge conflicts

--- ignite/templates/ibc/placeholders.go
+++ ignite/templates/ibc/placeholders.go
@@ -9,22 +9,4 @@ const (
 	PlaceholderIBCPacketModuleRecv    = "// this line is used by starport scaffolding # ibc/packet/module/recv"
 	PlaceholderIBCPacketModuleAck     = "// this line is used by starport scaffolding # ibc/packet/module/ack"
 	PlaceholderIBCPacketModuleTimeout = "// this line is used by starport scaffolding # ibc/packet/module/timeout"
-<<<<<<< HEAD
-
-	// Placeholders for messages
-	PlaceholderProtoTxRPC     = "// this line is used by starport scaffolding # proto/tx/rpc"
-	PlaceholderProtoTxMessage = "// this line is used by starport scaffolding # proto/tx/message"
-
-	// Placeholders for Oracle
-	PlaceholderProtoTxImport          = "// this line is used by starport scaffolding # proto/tx/import"
-	PlaceholderOraclePacketModuleRecv = "// this line is used by starport scaffolding # oracle/packet/module/recv"
-	PlaceholderOraclePacketModuleAck  = "// this line is used by starport scaffolding # oracle/packet/module/ack"
-	PlaceholderOracleModuleRecv       = "// this line is used by starport scaffolding # oracle/module/recv"
-	PlaceholderOracleModuleAck        = "// this line is used by starport scaffolding # oracle/module/ack"
-
-	// Placeholders AutoCLI
-	PlaceholderAutoCLIQuery = "// this line is used by ignite scaffolding # autocli/query"
-	PlaceholderAutoCLITx    = "// this line is used by ignite scaffolding # autocli/tx"
-=======
->>>>>>> 59400a52 (feat: remove all import placeholders using the `xast` pkg (#4004))
 )
