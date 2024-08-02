3152cf5dcddf8062af2c21f70601e63d623bb597
Danilo Pantani <danpantani@gmail.com>
2024-08-02 14:22:05 +0200

    fix: add missing verbose mode flags (#4286)
    * add missing verbose mode flags
    
    * add changelog
    
    * remove unused debug flag

--- ignite/cmd/cmd.go
+++ ignite/cmd/cmd.go
@@ -109,8 +109,14 @@ To get started, create a blockchain:
 	}, nil
 }
 
+func flagSetVerbose() *flag.FlagSet {
+	fs := flag.NewFlagSet("", flag.ContinueOnError)
+	fs.BoolP(flagVerbose, "v", false, "verbose output")
+	return fs
+}
+
 func getVerbosity(cmd *cobra.Command) uilog.Verbosity {
-	if verbose, _ := cmd.Flags().GetBool("verbose"); verbose {
+	if verbose, _ := cmd.Flags().GetBool(flagVerbose); verbose {
 		return uilog.VerbosityVerbose
 	}
 
