bf2539d6aa92f18171434dd43493a4053e04b03e
Danilo Pantani <danpantani@gmail.com>
2024-11-11 17:44:02 -0300

    fix: fix gentx parser into the cosmosutil package (#4402)
    * Fix gentx parser into the cosmosutil package because the use of Delegator Address in MsgCreateValidator is deprecated
    
    * add changelog

--- ignite/pkg/cosmosutil/gentx.go
+++ ignite/pkg/cosmosutil/gentx.go
@@ -7,9 +7,8 @@ import (
 
 	sdkmath "cosmossdk.io/math"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-
 	"github.com/cometbft/cometbft/crypto/ed25519"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/ignite/cli/v29/ignite/pkg/errors"
 )
@@ -17,7 +16,11 @@ import (
 type (
 	// GentxInfo represents the basic info about gentx file.
 	GentxInfo struct {
+		// Deprecated: Use of Delegator Address in MsgCreateValidator is deprecated.
+		// The validator address bytes and delegator address bytes refer to the same account while creating validator (defer
+		// only in bech32 notation).
 		DelegatorAddress string
+		ValidatorAddress string
 		PubKey           ed25519.PubKey
 		SelfDelegation   sdk.Coin
 		Memo             string
@@ -75,6 +78,7 @@ func ParseGentx(gentxBz []byte) (info GentxInfo, err error) {
 
 	info.Memo = gentx.Body.Memo
 	info.DelegatorAddress = gentx.Body.Messages[0].DelegatorAddress
+	info.ValidatorAddress = gentx.Body.Messages[0].ValidatorAddress
 
 	pb := gentx.Body.Messages[0].PubKey.Key
 	info.PubKey, err = base64.StdEncoding.DecodeString(pb)
