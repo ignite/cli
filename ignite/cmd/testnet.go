fbf722125d903d23c8e3c3033d5cd18548d40fbf
Julien Robert <julien@rbrt.fr>
2025-02-10 23:20:47 +0100

    docs: update long description `testnet` (#4502)
    (cherry picked from commit 7aa6b4a55c31c730caf42200fab7ccd14f602a72)
    
    # Conflicts:
    #	ignite/cmd/testnet.go

--- ignite/cmd/testnet.go
+++ ignite/cmd/testnet.go
@@ -8,7 +8,12 @@ import (
 func NewTestnet() *cobra.Command {
 	c := &cobra.Command{
 		Use:     "testnet [command]",
+<<<<<<< HEAD
 		Short:   "Start a testnet local",
+=======
+		Short:   "Simulate and manage test networks",
+		Long:    `Comprehensive toolset for managing and simulating blockchain test networks. It allows users to either run a test network in place using mainnet data or set up a multi-node environment for more complex testing scenarios. Additionally, it includes a subcommand for simulating the chain, which is useful for fuzz testing and other testing-related tasks.`,
+>>>>>>> 7aa6b4a5 (docs: update long description `testnet` (#4502))
 		Aliases: []string{"t"},
 		Args:    cobra.ExactArgs(1),
 	}
