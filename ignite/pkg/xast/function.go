659e2f5b2a44bf2f6203ee92ee7ce5e3ca7386ac
Pantani <danpantani@gmail.com>
2025-05-08 01:15:19 -0300

    remove unused error return

--- ignite/pkg/xast/function.go
+++ ignite/pkg/xast/function.go
@@ -429,7 +429,7 @@ func addFunctionCall(expr *ast.CallExpr, calls functionCalls) error {
 }
 
 // addStructs modifies struct literal fields.
-func addStructs(fileSet *token.FileSet, expr *ast.CompositeLit, structs functionStructs) error {
+func addStructs(fileSet *token.FileSet, expr *ast.CompositeLit, structs functionStructs) {
 	// Find the current max offset to avoid reused positions
 	file := fileSet.File(expr.Pos())
 	maxOffset := file.Offset(expr.Rbrace)
@@ -474,8 +474,6 @@ func addStructs(fileSet *token.FileSet, expr *ast.CompositeLit, structs function
 			file.AddLine(file.Offset(expr.Rbrace))
 		}
 	}
-
-	return nil
 }
 
 // codeToBlockStmt parses code string into AST block statement.
@@ -600,10 +598,7 @@ func applyFunctionOptions(fileSet *token.FileSet, f *ast.FuncDecl, opts *functio
 					return true
 				}
 
-				if err := addStructs(fileSet, expr, structs); err != nil {
-					errInspect = err
-					return false
-				}
+				addStructs(fileSet, expr, structs)
 				delete(structMapCheck, name)
 
 			default:
