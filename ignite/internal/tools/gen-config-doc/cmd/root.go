94257e5c94ceaccdcdce64396b7fc928650623ad
Julien Robert <julien@rbrt.fr>
2025-05-21 23:06:53 +0200

    change file location

--- ignite/internal/tools/gen-config-doc/cmd/root.go
+++ ignite/internal/tools/gen-config-doc/cmd/root.go
@@ -21,8 +21,8 @@ const (
 	flagOutput   = "output"
 	flagFilename = "filename"
 
-	defaultFilename = "03-config.md"
-	defaultDocPath  = "docs/docs/08-references"
+	defaultFilename = "01-config.md"
+	defaultDocPath  = "docs/docs/08-configuration"
 )
 
 // NewRootCmd creates a new root command.
