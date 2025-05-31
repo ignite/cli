b4c551cd4cdd0d610be9045cff75e9eead9bbd1d
Julien Robert <julien@rbrt.fr>
2025-05-31 08:34:20 +0200

    Revert "continue revert"
    This reverts commit 96939bde7a68bb64c0833dd321bcce7d79f99c24.

--- ignite/templates/field/field.go
+++ ignite/templates/field/field.go
@@ -105,9 +105,6 @@ func (f Field) ValueLoop() string {
 	if !ok {
 		panic(fmt.Sprintf("unknown type %s", f.DatatypeName))
 	}
-	if dt.NonIndex {
-		panic(fmt.Sprintf("non index type %s", f.DatatypeName))
-	}
 	return dt.ValueLoop
 }
 
@@ -160,9 +157,6 @@ func (f Field) ToBytes(name string) string {
 	if !ok {
 		panic(fmt.Sprintf("unknown type %s", f.DatatypeName))
 	}
-	if dt.NonIndex {
-		panic(fmt.Sprintf("non index type %s", f.DatatypeName))
-	}
 	return dt.ToBytes(name)
 }
 
@@ -171,9 +165,6 @@ func (f Field) ToString(name string) string {
 	dt, ok := datatype.IsSupportedType(f.DatatypeName)
 	if !ok {
 		panic(fmt.Sprintf("unknown type %s", f.DatatypeName))
-	}
-	if dt.NonIndex {
-		panic(fmt.Sprintf("non index type %s", f.DatatypeName))
 	}
 	return dt.ToString(name)
 }
