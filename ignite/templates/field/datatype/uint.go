8fd169cf9170ac1170412ebc87a48c6eb68fd3af
Danilo Pantani <danpantani@gmail.com>
2025-05-31 04:31:49 -0300

    refactor: remove the `nullify` pkg from the chain `testutil` (#4710)

--- ignite/templates/field/datatype/uint.go
+++ ignite/templates/field/datatype/uint.go
@@ -48,7 +48,8 @@ var (
 	DataUintSlice = DataType{
 		DataType:                func(string) string { return "[]uint64" },
 		CollectionsKeyValueName: func(string) string { return collectionValueComment },
-		DefaultTestValue:        "1,2,3,4,5",
+		DefaultTestValue:        "[]uint64{1, 2, 3, 4, 5}",
+		ValueLoop:               "[]uint64{uint64(i+i%1), uint64(i+i%2), uint64(i+i%3)}",
 		ProtoType: func(_, name string, index int) string {
 			return fmt.Sprintf("repeated uint64 %s = %d", name, index)
 		},
