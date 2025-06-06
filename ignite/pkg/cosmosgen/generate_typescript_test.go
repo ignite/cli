e3a7e73c5ba92571c2ed0d57921807c5820cab61
Julien Robert <julien@rbrt.fr>
2025-06-06 09:56:22 +0200

    add tests

--- ignite/pkg/cosmosgen/generate_typescript_test.go
+++ ignite/pkg/cosmosgen/generate_typescript_test.go
@@ -1,1 +1,67 @@
-package cosmosgen_test
+package cosmosgen
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/ettle/strcase"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ignite/cli/v29/ignite/pkg/cache"
+	"github.com/ignite/cli/v29/ignite/pkg/cosmosanalysis/module"
+	"github.com/ignite/cli/v29/ignite/pkg/cosmosbuf"
+)
+
+func TestGenerateTypeScript(t *testing.T) {
+	require := require.New(t)
+	testdataDir := "testdata"
+	appDir := filepath.Join(testdataDir, "testchain")
+	tsClientDir := filepath.Join(appDir, "ts-client")
+
+	cacheStorage, err := cache.NewStorage(filepath.Join(t.TempDir(), "cache.db"))
+	require.NoError(err)
+
+	buf, err := cosmosbuf.New(cacheStorage, t.Name())
+	require.NoError(err)
+
+	g := newTSGenerator(&generator{
+		appPath:      appDir,
+		protoDir:     "proto",
+		goModPath:    "go.mod",
+		cacheStorage: cacheStorage,
+		buf:          buf,
+		opts: &generateOptions{
+			useCache: false,
+			jsOut: func(m module.Module) string {
+				return filepath.Join(tsClientDir, strcase.ToKebab(m.Name))
+			},
+		},
+	})
+
+	// Use module discovery to collect test module proto.
+	module, err := module.Discover(t.Context(), appDir, appDir, module.WithProtoDir(g.g.protoDir))
+	require.NoError(err, "failed to discover module")
+	require.Len(module, 1, "expected exactly one module to be discovered")
+
+	err = g.generateModuleTemplate(t.Context(), appDir, module[0])
+	require.NoError(err, "failed to generate TypeScript files")
+
+	// compare all generated files to golden files
+	goldenDir := filepath.Join(testdataDir, "expected_files", "ts-client")
+	_ = filepath.Walk(goldenDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return err
+		}
+		rel, _ := filepath.Rel(goldenDir, path)
+		got := filepath.Join(tsClientDir, rel)
+		gold, err := os.ReadFile(path)
+		require.NoError(err, "failed to read golden file: %s", path)
+
+		gotBytes, err := os.ReadFile(got)
+		require.NoError(err, "failed to read generated file: %s", got)
+		require.Equal(string(gold), string(gotBytes), "file %s does not match golden file", rel)
+
+		return nil
+	})
+}
---
+++ ignite/pkg/cosmosgen/testdata/testchain/go.mod
@@ -0,0 +1,3 @@
+module github.com/ignite/planet
+
+go 1.16
