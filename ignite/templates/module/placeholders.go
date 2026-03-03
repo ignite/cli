527f39918981e0b4e6641804187db5eab538243b
Danilo Pantani <danpantani@gmail.com>
2026-03-02 22:52:34 -0300

    refactor: remove ibc keys placeholders (#4883)
    * refactor: remove ibc keys placeholders
    
    * add changelog
    
    * trigger CI
    
    * trigger ci

--- ignite/templates/module/placeholders.go
+++
@@ -1,7 +0,0 @@
-package module
-
-const (
-	// Placeholders in IBC module files.
-	PlaceholderIBCKeysName = "// this line is used by starport scaffolding # ibc/keys/name"
-	PlaceholderIBCKeysPort = "// this line is used by starport scaffolding # ibc/keys/port"
-)
