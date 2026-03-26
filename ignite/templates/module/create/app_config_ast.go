b728fd663f74b47608ed68bbd2e9f2e2ffed7482
Danilo Pantani <danpantani@gmail.com>
2026-03-26 06:01:49 -0300

    fix: ignore `context.Canceled` errors in Sentry reporting (#4909)
    * fix: ignore `context.Canceled` errors in Sentry reporting
    
    * fix lint and add changelog
    
    * fix missing comment
    
    * rollback comments changes

--- ignite/templates/module/create/app_config_ast.go
+++ ignite/templates/module/create/app_config_ast.go
@@ -26,7 +26,7 @@ func SkipConfigEntry() AddModuleAppConfigOption {
 }
 
 // SpecifyModuleEntry allows to define to which field the module should be added in the app config.
-// E.g. "PreBlockers", "InitGenesis", "BeginBlockers", "EndBlockers"
+// E.g. "PreBlockers", "InitGenesis", "BeginBlockers", "EndBlockers".
 func SpecifyModuleEntry(fields ...string) AddModuleAppConfigOption {
 	return func(opts *addModuleAppConfigOptions) {
 		opts.runtimeFields = fields
